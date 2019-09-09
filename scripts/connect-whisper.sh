7ad75903bf86e0ec604b941ae5236fc87bb8b273
Samuel Stokes <sam.stokes@consensys.net>
2019-09-09 08:44:06 -0400

    Add third geth node with whisper client to test env

--- scripts/connect-whisper.sh
+++ scripts/connect-whisper.sh
@@ -2,12 +2,11 @@
 
 if [ $# -eq 0 ]
   then
-    echo "Number of whisper connections not supplied. Defaulting to two."
-    count=2
+    echo "Number of whisper connections not supplied. Defaulting to three."
+    count=3
 fi
 
 for (( node_num=1; node_num<=$count; node_num++))
 do
   ttab -G -t "Whisper Node $node_num" exec node ../whisper/src/index.js $node_num
-  #newtab node ../whisper/src/index.js $node_num
 done
