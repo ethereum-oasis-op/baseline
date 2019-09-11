b2cc5bc13f90b4428ddaf985b0953b9b9e69f8f2
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-09-11 15:08:52 -0400

    App docker container working w/o config

--- entangled_poc/app/src/App.js
+++ entangled_poc/app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import config from './config.js';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { config.name }
         </p>
         <a
           className="App-link"
