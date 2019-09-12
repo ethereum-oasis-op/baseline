0ed267964e9bc4e3cb0a92a76afa26bde59819c9
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-09-12 17:44:04 -0400

    Added material UI and typography

---
+++ entangled_poc/app/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+
+// Custom config for each user type
+import config from './config.js';
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          {config.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © '}
+          <Link color="inherit" href="https://material-ui.com/">
+            Your Website
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
--- entangled_poc/app/src/index.js
+++ entangled_poc/app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
