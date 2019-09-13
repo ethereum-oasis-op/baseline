ca64f72be1894d41c30e3aae5ccd43c554ba2644
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-09-13 17:51:20 -0400

    Added in mongo db and seeded collection

--- entangled_poc/app/src/config.js
+++ entangled_poc/app/src/config.js
@@ -1,5 +1,8 @@
 const config = {
-  name: "Hello"
+  name: 'Buyer1 Demo',
+  mongoHost: 'localhost:27017',
+  databaseName: 'buyer1db',
+  entangledField: 'rfq-quantity'
 };
 
 module.exports = config;
