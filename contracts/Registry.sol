f2d6da216d126d04525752f3a58ea6efdcfac834
Kartheek Solipuram <kartheek.s@gmail.com>
2019-09-27 13:38:31 +0200

    addressed comments on pr 147 and fixed Dockerfile

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.8;
+pragma experimental ABIEncoderV2;
 import "./ERC165Compatible.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/access/Roles.sol";
@@ -17,7 +18,7 @@ contract Registry is Ownable, ERC165Compatible {
         address orgAddress;
         bytes32 name;
         uint role;
-        bytes32 messagingKey;
+        bytes messagingKey;
     }
 
     mapping (address => Org) orgMap;
@@ -29,18 +30,27 @@ contract Registry is Ownable, ERC165Compatible {
         setInterfaces();
     }
 
+    /// @notice This is an implementation of setting interfaces for the organization
+    /// registry contract
+    /// @dev the character '^' corresponds to bit wise xor of individual interface id's
+    /// which are the parsed 4 bytes of the function signature of each of the functions
+    /// in the org registry contract
     function setInterfaces() public onlyOwner returns (bool) {
         supportedInterfaces[this.registerOrg.selector ^
                             this.getOrgCount.selector ^
                             this.getOrgs.selector] = true;
     }
 
-    function getInterfaces() external view returns (bytes4) {
+    /// @notice This function is a helper function to be able to get the
+    /// set interface id by the setInterfaces()
+    function getInterfaces() external pure returns (bytes4) {
         return this.registerOrg.selector ^
                             this.getOrgCount.selector ^
                             this.getOrgs.selector;
     }
 
+    /// @dev Since this is an inherited method from ERC165 Compatible, it returns the value of the interface id
+    /// set during the deployment of this contract
     function supportsInterface(bytes4 interfaceId) external view returns (bool) {
         return supportedInterfaces[interfaceId];
     }
@@ -52,11 +62,12 @@ contract Registry is Ownable, ERC165Compatible {
     /// @param _key public keys required for message communication
     /// @dev Function to register an organization
     /// @return `true` upon successful registration of the organization
-    function registerOrg(address _address, bytes32 _name, uint _role, bytes32 _key) external onlyOwner returns (bool) {
+    function registerOrg(address _address, bytes32 _name, uint _role, bytes calldata _key) external onlyOwner returns (bool) {
         Org memory org = Org(_address, _name, _role, _key);
         roleMap[_role].add(_address);
         orgMap[_address] = org;
         parties.push(_address);
+        return true;    
     }
 
     /// @dev Function to get the count of number of organizations to help with extraction
@@ -65,12 +76,13 @@ contract Registry is Ownable, ERC165Compatible {
         return parties.length;
     }
 
-    /// @notice Function to retrieve the details of the registered organization
+    /// @notice Function to retrieve a page of registered organizations along with details
+    /// @notice start and end indices here are a convenience for pagination
     /// @param start starting index of the array where organization addresses are stored
     /// @param count ending index of the array where organization addresses are stored
     /// @dev Getter to retrieve details of the organization enabled for pagination
     /// @return array form of the details of the organization as stored in the struct
-    function getOrgs(uint start, uint count) external view returns (address[] memory, bytes32[] memory , uint[] memory, bytes32[] memory) {
+    function getOrgs(uint start, uint count) external view returns (address[] memory, bytes32[] memory , uint[] memory, bytes[] memory) {
         uint end = start + count - 1;
         if (end >= parties.length) end = parties.length - 1;
 
@@ -78,7 +90,7 @@ contract Registry is Ownable, ERC165Compatible {
         address[] memory addresses = new address[](size);
         bytes32[] memory names = new bytes32[](size);
         uint[] memory roles = new uint[](size);
-        bytes32[] memory keys = new bytes32[](size);
+        bytes[] memory keys = new bytes[](size);
 
         for (uint i = 0; i < size; i++) {
             addresses[i] = parties[i + start];
@@ -87,6 +99,6 @@ contract Registry is Ownable, ERC165Compatible {
             keys[i] = orgMap[parties[i + start]].messagingKey;
         }
 
-        return (addresses, names, roles, keys);
+        return (addresses, names, roles, keys); 
     }
 }
--- contracts/__tests__/Registry.test.js
+++ contracts/__tests__/Registry.test.js
@@ -1,9 +1,9 @@
 import { ethers, utils } from 'ethers';
 import { getWallet, getAccounts } from '../wallet';
-
 import RegistryArtifact from '../../artifacts/Registry';
 
 const wallet = getWallet();
+
 const { compilerOutput: { abi , evm: { bytecode } } } = RegistryArtifact;
 const Registry = new ethers.ContractFactory(abi, bytecode, wallet);
 
@@ -18,9 +18,9 @@ test('Should be able to set interfaces for compatibility checks', async () => {
 });
 
 test('Should be able to correctly set the interface id', async () => {
-    /* const c1 = utils.hexDataSlice(utils.keccak256(utils.toUtf8Bytes('registerOrg(address,bytes32,uint,bytes32)')), 0, 4);
-    const c2 = utils.hexDataSlice(utils.keccak256(utils.toUtf8Bytes('getOrgCount()')), 0, 4);
-    const c3 = utils.hexDataSlice(utils.keccak256(utils.toUtf8Bytes('getOrgs(uint,uint)')), 0, 4); */
+    // const c1 = utils.hexDataSlice(utils.keccak256(utils.toUtf8Bytes('registerOrg(address,bytes32,uint,bytes32)')), 0, 4);
+    // const c2 = utils.hexDataSlice(utils.keccak256(utils.toUtf8Bytes('getOrgCount()')), 0, 4);
+    // const c3 = utils.hexDataSlice(utils.keccak256(utils.toUtf8Bytes('getOrgs(uint,uint)')), 0, 4);
     const x = await registry.getInterfaces();
     const y = await registry.supportsInterface(x);
     expect(y).toBe(true);
@@ -33,7 +33,7 @@ test('Should be able to get 0 count at the beginning', async () => {
 
 test('Should be able to register an org and retrieve the event', async () => {
     const accounts = await getAccounts();
-    const txReceipt = await registry.registerOrg(accounts[1], utils.formatBytes32String("Name"), 2, utils.formatBytes32String("randomHex"));
+    const txReceipt = await registry.registerOrg(accounts[1], utils.formatBytes32String("Name"), 2, utils.hexlify('0x0471099dd873dacf9570f147b9e07ebd671e05bfa63912ee623a800ede8a294f7f60a13fadf1b53d681294cc9b9ff0a4abdf47338ff72d3c34c95cdc9328bd0128'));
 });
 
 test('Should be able to retrieve the incremented org count', async () => {
@@ -50,7 +50,7 @@ test('Should be able to retrieve registered org details', async () => {
         3: key
     } = await registry.getOrgs(0, 1);
     expect(address[0]).toBe(accounts[1]);
-    expect(name[0]).toBe(utils.formatBytes32String("Name"));
+    expect(name[0]).toBe(utils.formatBytes32String('Name'));
     expect(role[0].toNumber()).toBe(2);
-    expect(key[0]).toBe(utils.formatBytes32String("randomHex"));
+    expect(key[0]).toBe(utils.hexlify('0x0471099dd873dacf9570f147b9e07ebd671e05bfa63912ee623a800ede8a294f7f60a13fadf1b53d681294cc9b9ff0a4abdf47338ff72d3c34c95cdc9328bd0128'));
 });
