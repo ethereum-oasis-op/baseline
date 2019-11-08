ab3ffd6bf5183c4d1375b8281fa1089a5782ba39
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-11-08 12:22:12 -0500

    WIP graphql

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,43 @@ global.customRequire = function (file) {
 }
 
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+    identities: [Identity]
+  }
+
+  type Identity {
+    _id: ID
+    created: Int
+    keyId: String
+    privateKey: String
+    publicKey: String
+  }
+
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
+
 const bodyParser = require('body-parser');
 const apiRouter = require('./api/rest-express');
 
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use('/api/v1', apiRouter.router);
+
+// Adding the REST endpoints as middleware to the GraphQL endpoints
+server.applyMiddleware({ app });
 
 module.exports = {
   app,
