43402afa0dd1a8d9ed9be6ef2cb40e9fb8e86173
Patrick Macom <patrickmacom@gmail.com>
2019-11-15 13:15:13 -0500

    feat: adds graphql subscriptions

--- radish-api/src/app.js
+++ radish-api/src/app.js
@@ -1,4 +1,6 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { createServer } from 'http';
 import coreQuery from './schemas/core';
 import OrganizationSchema from './schemas/organization';
 import OrganizationResolver from './resolvers/organization';
@@ -17,12 +19,25 @@ export default async function startServer() {
   const app = express();
   await healthcheck();
 
+const PORT = process.env.PORT || 8001;
+
+const typeDefs = [PartnerSchema, OrganizationSchema, ScalarsSchema];
+const resolvers = [PartnerResolver, OrganizationResolver, ScalarsResolver];
+
+export default async function startServer() {
+  const app = express();
+
   const server = new ApolloServer({
     typeDefs: [coreQuery, ...typeDefs],
     resolvers,
   });
+  server.applyMiddleware({ app, path: '/graphql' });
 
-  server.listen(80).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+  const httpServer = createServer(app);
+  server.installSubscriptionHandlers(httpServer);
+
+  httpServer.listen({ port: PORT }, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
   });
 }
