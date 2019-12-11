bc6bb7c934a65615e18b5d604d8ad086254e247b
Kartheek Solipuram <kartheek.s@gmail.com>
2019-12-10 22:29:26 -0500

    updated functionalities to get managers in org service js file

--- radish-api/src/init-buyer.js
+++ radish-api/src/init-buyer.js
@@ -12,6 +12,7 @@ import {
   setInterfaceImplementer,
   getInterfaceAddress,
   getRegisteredOrganization,
+  getManager,
 } from './services/organization';
 import { getServerSettings } from './utils/serverSettings';
 
@@ -22,11 +23,14 @@ const main = async () => {
   const walletAddress = await getAddress();
   console.log('the account used in this container is: ', walletAddress);
   console.log('the balance of the account used in this container is: ', await getBalance());
-  const erc1820Address = await deployERC1820Registry('ERC1820Registry');
-  console.log('data stored in db for ERC1820Registry Smartcontract: ', erc1820Address);
-  const orgRegistryRecord = await deployOrgRegistry(erc1820Address.contract.contractAddress);
+  const erc1820Record = await deployERC1820Registry('ERC1820Registry');
+  console.log('data stored in db for ERC1820Registry Smartcontract: ', erc1820Record);
+  const orgRegistryRecord = await deployOrgRegistry(erc1820Record.contract.contractAddress);
   console.log('data stored in db for OrgRegistry Smartcontract: ', orgRegistryRecord);
-  const txHashAssignManager = await assignManager(orgRegistryRecord.contract.contractAddress);
+  const txHashAssignManager = await assignManager(
+    orgRegistryRecord.contract.contractAddress,
+    walletAddress,
+  );
   console.log('transactionHash for assigning new manager of OrgRegistry: ', txHashAssignManager);
   const txHashSetInterfaceImplmenter = await setInterfaceImplementer(
     walletAddress,
@@ -39,19 +43,20 @@ const main = async () => {
   );
   console.log('OrgRegistry Address in db is: ', await getOrganizationRegistryAddress());
   const orgRegistryAddress = await getInterfaceAddress(
-    erc1820Address.contract.contractAddress,
+    erc1820Record.contract.contractAddress,
     walletAddress,
     utils.id('IOrgRegistry'),
   );
   console.log('OrgRegistry Address retrieved from Registrar is: ', orgRegistryAddress);
   const registerOrgTxHash = await registerToOrgRegistry(
+    orgRegistryAddress,
     walletAddress,
     config.myOrganizationName,
     config.myOrganizationRole,
     config.myOrganizationKey,
   );
   console.log('transactionHash for registering the buyer is: ', registerOrgTxHash);
-  const orgRecord = await getRegisteredOrganization(walletAddress);
+  const orgRecord = await getRegisteredOrganization(orgRegistryAddress, walletAddress);
   console.log('registered organization for this container is: ', orgRecord);
   process.exit(0);
 };
