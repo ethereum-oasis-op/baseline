48006104df5bc406ec4189270619ebe97523ffba
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-12-12 16:52:51 -0500

    Removed graphql from service API

--- messenger/app.js
+++ messenger/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const apiRouter = require('./api/rest-express');
 
 app.use(bodyParser.json({ limit: '2mb' }));
+
 app.use('/api/v1', apiRouter.router);
 
 module.exports = {
