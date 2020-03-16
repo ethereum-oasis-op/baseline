fadcea7682390380e0472aac3861d9530c6b610e
Samuel Stokes <sam.stokes@consensys.net>
2020-03-16 18:25:27 -0400

    Move files into radish34/

--- radish34/contracts/src/wallet.js
+++ radish34/contracts/src/wallet.js
@@ -12,17 +12,17 @@ let walletWithProvider;
 ethers.errors.setLogLevel('error');
 
 const wallet = new ethers.Wallet('0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
-const artifactAdapter = new SolCompilerArtifactAdapter(path.resolve(__dirname, 'artifacts'), path.resolve(__dirname, 'contracts'));
+const artifactAdapter = new SolCompilerArtifactAdapter(path.resolve(__dirname, '../artifacts'), path.resolve(__dirname, '../contracts'));
 
 const revertTraceSubprovider = new RevertTraceSubprovider(artifactAdapter, wallet.address, true);
 const profilerSubprovider = new ProfilerSubprovider(artifactAdapter, wallet.address);
 const coverageSubprovider = new CoverageSubprovider(artifactAdapter, wallet.address);
 
 const providerEngine = new Web3ProviderEngine();
-providerEngine.addProvider(new FakeGasEstimateSubprovider(4 * (10 ** 6))); 
+providerEngine.addProvider(new FakeGasEstimateSubprovider(4 * (10 ** 6)));
 // Ganache does a poor job of estimating gas, so just crank it up for testing.
 providerEngine.addProvider(revertTraceSubprovider);
-providerEngine.addProvider(new RpcSubprovider({ rpcUrl: 'http://ganache:8545' }));
+providerEngine.addProvider(new RpcSubprovider({ rpcUrl: 'http://0.0.0.0:8545' }));
 providerEngine.addProvider(profilerSubprovider);
 providerEngine.addProvider(coverageSubprovider);
 providerUtils.startProviderEngine(providerEngine);
