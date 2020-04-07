9b2088ff7621ff0826207ee9911c71148ff17d0b
Samuel Stokes <sam.stokes@consensys.net>
2020-04-07 15:08:12 -0400

    fix(bug): parseLog for createMSA was failing. Added Shield.test.js file

--- radish34/contracts/__tests__/Shield.test.js
+++ radish34/contracts/__tests__/Shield.test.js
@@ -1,18 +1,14 @@
-import { getWallet, getAccounts, getSigner, getProvider, link } from '../src/utils';
+import { getAccounts, getSigner, getProvider, link, formatProof } from '../src/utils';
 import Doppelganger from 'ethereum-doppelganger';
 import { ethers, utils } from 'ethers';
 import testdata from './mockVerifierData';
 
 import VerifierArtifact from '../artifacts/Verifier.json';
-import BN256G2Artifact from '../artifacts/BN256G2.json'; 
+import BN256G2Artifact from '../artifacts/BN256G2.json';
 import ShieldArtifact from '../artifacts/Shield.json';
-import MerkleTreeArtifact from '../artifacts/MerkleTree.json'
 import ERC1820RegistryArtifact from '../artifacts/ERC1820Registry.json';
 
-const wallet = getWallet();
-const SNARK_SCALAR_BOUND = "21888242871839275222246405745257275088548364400416034343698204186575808495617";
-const PRIME_BOUND = "21888242871839275222246405745257275088696311157297823662689037894645226208583";
-let verifier, shield, merkleTree;
+let verifier, shield;
 let accounts, signer;
 
 test('Should be able to deploy Shield contract', async () => {
@@ -33,3 +29,69 @@ test('Should be able to deploy Shield contract', async () => {
   expect(verifier.address).toMatch(new RegExp('^0x[a-fA-F0-9]{40}$'));
   expect(shield.address).toMatch(new RegExp('^0x[a-fA-F0-9]{40}$'));
 });
+
+test('Successfully execute registerVerificationKey transaction for createMSA', async () => {
+  // Note: Alternatively, hex inputs from interaction with the zkp service during set up and proof generation
+  // can be used. To be able to convert the proof and vk arrays to BigInt, `formatProof` a util in 
+  // api/src/utils/crypto/conversions.js can be used to handle this conversion
+  let topic = utils.id("VkChanged(uint8)");
+  const tx = await shield.registerVerificationKey(testdata.msa.vks_int, 0);
+  await tx.wait();
+  const txReceipt = await getProvider().getTransactionReceipt(tx.hash);
+  const txLogs = shield.interface.parseLog(txReceipt.logs[0]);
+  expect(tx.confirmations > 0).toBeTruthy();
+  expect(txReceipt.status).toEqual(1);
+  expect(tx.hash).toMatch(new RegExp('^0x[a-fA-F0-9]{64}$'));
+  expect(txLogs.name).toEqual('VkChanged');
+  expect(txLogs.topic).toEqual(topic);
+}, 20000);
+
+test('Successfully execute createMSA transaction', async () => {
+  // Note: Alternatively, hex inputs from interaction with the zkp service during set up and proof generation
+  // can be used. To be able to convert the proof and vk arrays to BigInt, `formatProof` a util in 
+  // api/src/utils/crypto/conversions.js can be used to handle this conversion
+  let topic = utils.id("NewLeaf(uint256,bytes32,bytes32)");
+  const overrides = { gasLimit: 10000000 }; // The maximum units of gas for the transaction to use
+  const tx = await shield.createMSA(testdata.msa.proofs_int, testdata.msa.inputs_hash, ...testdata.msa.inputs_hex, overrides);
+  await tx.wait(); // The operation is NOT complete yet; we must wait until it is mined
+  const txReceipt = await getProvider().getTransactionReceipt(tx.hash);
+  const txLogs = shield.interface.parseLog(txReceipt.logs[1]);
+  expect(tx.confirmations > 0).toBeTruthy();
+  expect(txReceipt.status).toEqual(1);
+  expect(tx.hash).toMatch(new RegExp('^0x[a-fA-F0-9]{64}$'));
+  expect(txLogs.name).toEqual('NewLeaf');
+  expect(txLogs.topic).toEqual(topic);
+}, 20000);
+
+test('Successfully execute registerVerificationKey transaction for createPO', async () => {
+  // Note: Alternatively, hex inputs from interaction with the zkp service during set up and proof generation
+  // can be used. To be able to convert the proof and vk arrays to BigInt, `formatProof` a util in 
+  // api/src/utils/crypto/conversions.js can be used to handle this conversion
+  let topic = utils.id("VkChanged(uint8)");
+  const tx = await shield.registerVerificationKey(testdata.po.vks_int, 1);
+  await tx.wait();
+  const txReceipt = await getProvider().getTransactionReceipt(tx.hash);
+  const txLogs = shield.interface.parseLog(txReceipt.logs[0]);
+  expect(tx.confirmations > 0).toBeTruthy();
+  expect(txReceipt.status).toEqual(1);
+  expect(tx.hash).toMatch(new RegExp('^0x[a-fA-F0-9]{64}$'));
+  expect(txLogs.name).toEqual('VkChanged');
+  expect(txLogs.topic).toEqual(topic);
+}, 20000);
+
+test('Successfully execute createPO transaction', async () => {
+  // Note: Alternatively, hex inputs from interaction with the zkp service during set up and proof generation
+  // can be used. To be able to convert the proof and vk arrays to BigInt, `formatProof` a util in 
+  // api/src/utils/crypto/conversions.js can be used to handle this conversion
+  let topic = utils.id("NewLeaf(uint256,bytes32,bytes32)");
+  const overrides = { gasLimit: 10000000 }; // The maximum units of gas for the transaction to use
+  const tx = await shield.createPO(testdata.po.proofs_int, testdata.po.inputs_hash, ...testdata.po.inputs_hex, overrides);
+  await tx.wait(); // The operation is NOT complete yet; we must wait until it is mined
+  const txReceipt = await getProvider().getTransactionReceipt(tx.hash);
+  const txLogs = shield.interface.parseLog(txReceipt.logs[1]);
+  expect(tx.confirmations > 0).toBeTruthy();
+  expect(txReceipt.status).toEqual(1);
+  expect(tx.hash).toMatch(new RegExp('^0x[a-fA-F0-9]{64}$'));
+  expect(txLogs.name).toEqual('NewLeaf');
+  expect(txLogs.topic).toEqual(topic);
+}, 20000);
