bece9bedd618880d46a005ac2882d2f1faef17f1
Kartheek Solipuram <kartheek.s@gmail.com>
2020-04-15 15:32:19 -0700

    minor fixes to get commitment generation working

--- radish34/api/src/services/agreement.js
+++ radish34/api/src/services/agreement.js
@@ -16,6 +16,8 @@ import { saveNotice } from './notice';
 
 const pycryptojs = require('zokrates-pycryptojs');
 
+let _commitment;
+
 /**
 An msaDoc object (from the mongodb) contains an array of commitments.
 But we only want to 'pop' the latest commitment from the array, to add it to our Commitment class.
@@ -35,10 +37,6 @@ export const extractAgreementFromDoc = (agreementDoc, index = 'latest') => {
   return agreementObject;
 };
 
-export const simpleGetter = () => {
-    return "done";
-}
-
 /**
 Structure of the database from which the class can be created:
 db schema Agreement: {
@@ -57,3 +55,76 @@ db schema Agreement: {
   ]
 }
 **/
+
+const validate = (object, objectType) => {
+  // validate objects' keys:
+  const requiredKeys = (type => {
+    switch (type) {
+      case 'constants':
+        return [
+          'zkpPublicKeyOfBuyer',
+          'zkpPublicKeyOfSupplier',
+          'name',
+          'description',
+          'erc20ContractAddress',
+        ];
+      default:
+        throw new Error('object type not recognised.');
+    }
+  })(objectType);
+
+  const missingKeys = requiredKeys.filter(key => !(key in object));
+
+  if (missingKeys.length > 0)
+    throw new Error(`missing required keys for PO object ${objectType}: ${missingKeys}`);
+};
+
+
+export const generateCommitment = (data) => {
+  const { _id, constants, variables = {}, commitment = {}, initType } = data;
+
+  const {
+    zkpPublicKeyOfSender,
+    zkpPublicKeyOfRecipient,
+    name,
+    description,
+    erc20ContractAddress,
+  }  = constants;
+
+  if (Object.keys(commitment).length !== 0) {
+    // Case 1:
+    const preimage = {
+      zkpPublicKeyOfSender: elementify(zkpPublicKeyOfSender).hex(64),
+      zkpPublicKeyOfRecipient: elementify(zkpPublicKeyOfRecipient).hex(64),
+      name: elementify(name).hex(32),
+      description: elementify(description).hex(32),
+      erc20ContractAddress: elementify(erc20ContractAddress).hex(64),
+    };
+    _commitment = new Commitment(preimage, {}, initType);
+  } else {
+    // Case 2:
+    _commitment = new Commitment({}, commitment, initType);
+  }
+  return _commitment;
+};
+
+export const getAgreementById = async id => {
+  try {
+    const agreement = await AgreementModel.findOne({ _id: id }).lean();
+    return agreement;
+  } catch (e) {
+    console.log('\nError getting Agreement from DB: ', e);
+    return false;
+  }
+};
+
+export const getAgreementsByName = async name => {
+  try {
+    const msas = await AgreementModel.find({ 'constants.name': name }).lean();
+    return msas;
+  } catch (e) {
+    console.log('\nError getting Agremement from DB: ', e);
+    return false;
+  }
+};
+
