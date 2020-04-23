ab61b79b9d1a64cf647e28f9a0d824746fd5a14d
Kyle Thomas <kyle@provide.services>
2020-04-23 07:18:21 -0400

    Refactor zkp service for generic compile & proxy consumption

--- radish34/zkp/dev.Dockerfile
+++ radish34/zkp/dev.Dockerfile
@@ -1,4 +1,4 @@
-FROM zokrates/zokrates:0.5.1 as builder
+FROM zokrates/zokrates:0.5.2 as builder
 
 FROM node:11.15
 
@@ -8,9 +8,9 @@ WORKDIR /app
 COPY --from=builder /home/zokrates/zokrates /app/zokrates
 COPY --from=builder /home/zokrates/.zokrates* /app/stdlib
 
-COPY ./package.json ./package-lock.json ./.babelrc ./
+COPY . .
 RUN npm ci
 RUN curl https://sh.rustup.rs -sSf -y | sh
 
 EXPOSE 80
-CMD npm start
+CMD npm run dev
--- radish34/zkp/src/app.js
+++ radish34/zkp/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import compile from './routes/compile';
 import generateProof from './routes/generateProof';
 import generateKeys from './routes/generateKeys';
 import vk from './routes/vk';
@@ -12,6 +13,8 @@ app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ limit: '2mb', extended: false }));
 
 app.get('/healthcheck', (req, res) => res.sendStatus(200));
+
+app.use('/compile', compile);
 app.use('/generate-keys', generateKeys);
 app.use('/generate-proof', generateProof);
 app.use('/vk', vk);
