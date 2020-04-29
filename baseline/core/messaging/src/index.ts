7b37fbaac475ec6a364bd28658182fc4dc7d52e2
Samuel Stokes <sam.stokes@consensys.net>
2020-04-29 14:17:13 -0400

    Fix messenger typescript errors

--- baseline/core/messaging/src/index.ts
+++ baseline/core/messaging/src/index.ts
@@ -12,9 +12,9 @@ export interface IMessagingService {
   disconnect(): Promise<void>;
   getSubscribedSubjects(): string[];
   isConnected(): boolean;
-  publish(subject: string, payload: any, reply?: string): Promise<void>;
+  publish(subject: string, payload: any, reply?: string, senderId?: string, recipientId?: string): Promise<void>;
   request(subject: string, timeout: number, data?: any): Promise<any | void>;
-  subscribe(subject: string, callback: (msg: any, err?: any) => void): Promise<any>;
+  subscribe(subject: string, keyId?: string, callback?: (msg: any, err?: any) => void): Promise<any>;
   unsubscribe(subject: string);
   flush(): Promise<void>;
 }
