ccc09a2d099b6966cc43d2dd7523f3b45f3648b7
Myron Rotter <m.rotter@campus.tu-berlin.de>
2020-04-29 13:20:39 +0200

    Add radish34 logger

---
+++ radish34/logger/src/format.js
@@ -0,0 +1,141 @@
+const winston = require("winston");
+const {
+  error,
+  warning,
+  deprecated,
+  http,
+  info,
+  verbose,
+  debug,
+  information,
+  success,
+  redirection,
+  client,
+  server,
+} = require("./static");
+
+const LEVEL = Symbol.for("level");
+
+const levelFormat = (level) => {
+  switch (level) {
+    case "error":
+      return error(level.toUpperCase());
+    case "warning":
+      return warning(level.toUpperCase());
+    case "deprecated":
+      return deprecated(level.toUpperCase());
+    case "http":
+      return http(level.toUpperCase());
+    case "info":
+      return info(level.toUpperCase());
+    case "verbose":
+      return verbose(level.toUpperCase());
+    case "debug":
+      return debug(level.toUpperCase());
+    default:
+      return level;
+  }
+};
+
+const statusFormat = (status) => {
+  if (100 <= status && status <= 199) {
+    return information(status.toString());
+  } else if (200 <= status && status <= 299) {
+    return success(status.toString());
+  } else if (300 <= status && status <= 399) {
+    return redirection(status.toString());
+  } else if (400 <= status && status <= 499) {
+    return client(status.toString());
+  } else if (500 <= status && status <= 599) {
+    return server(status.toString());
+  } else {
+    return status.toString();
+  }
+};
+
+module.exports = {
+  // Formats
+  defaultConsoleFormat: winston.format.printf(
+    ({ timestamp, level, service, message, ...args }) => {
+      const timestampFormated = timestamp.slice(0, 19).replace("T", " ");
+
+      return `[${timestampFormated}] [${levelFormat(
+        level
+      )}] [${service}]: ${message} ${
+        Object.keys(args).length ? JSON.stringify(args) : ""
+      }`;
+    }
+  ),
+  simpleHttpConsoleFormat: winston.format.printf(
+    ({ timestamp, level, service, req, res, message, ...args }) => {
+      const timestampFormated = timestamp.slice(0, 19).replace("T", " ");
+
+      return `[${timestampFormated}] [${levelFormat(level)}] [${service}]: ${
+        req.method
+      } ${req.originalUrl} ${message} ${
+        Object.keys(args).length ? JSON.stringify(args) : ""
+      }`;
+    }
+  ),
+  reqHttpConsoleFormat: winston.format.printf(
+    ({ timestamp, level, message, meta }) => {
+      const timestampFormated = timestamp.slice(0, 19).replace("T", " ");
+      level = "http";
+      const service = message;
+
+      return `[${timestampFormated}] [${levelFormat(
+        level
+      )}] [${service}]: ${statusFormat(meta.res.statusCode)} ${
+        meta.responseTime
+      }ms ${meta.req.method} ${meta.req.originalUrl}`;
+    }
+  ),
+  defaultFileFormat: winston.format.printf(
+    ({ timestamp, level, service, message, ...args }) => {
+      const timestampFormated = timestamp.slice(0, 19).replace("T", " ");
+
+      return `[${timestampFormated}] [${level.toUpperCase()}] [${service}]: ${message} ${
+        Object.keys(args).length ? JSON.stringify(args) : ""
+      }`;
+    }
+  ),
+  simpleHttpFileFormat: winston.format.printf(
+    ({ timestamp, level, service, req, message, ...args }) => {
+      const timestampFormated = timestamp.slice(0, 19).replace("T", " ");
+
+      return `[${timestampFormated}] [${level.toUpperCase()}] [${service}]: ${
+        req.method
+      } ${req.originalUrl} ${message} ${
+        Object.keys(args).length ? JSON.stringify(args) : ""
+      }`;
+    }
+  ),
+  reqHttpFileFormat: winston.format.printf(
+    ({ timestamp, level, message, meta }) => {
+      const timestampFormated = timestamp.slice(0, 19).replace("T", " ");
+      level = "http";
+      const service = message;
+
+      return `[${timestampFormated}] [${level.toUpperCase()}] [${service}]: ${
+        meta.res.statusCode
+      } ${meta.responseTime}ms ${meta.req.method} ${
+        meta.req.originalUrl
+      } ${JSON.stringify(meta)}`;
+    }
+  ),
+  // Filter
+  filterOnly: (level) => {
+    return winston.format((info) => {
+      if (info[LEVEL] === level) {
+        return info;
+      }
+    })();
+  },
+  filterWithout: (level) => {
+    return winston.format((info) => {
+      if (info[LEVEL] !== level) {
+        return info;
+      }
+    })();
+  },
+};
---
+++ radish34/logger/src/logger.js
@@ -0,0 +1,152 @@
+const winston = require("winston");
+const expressWinston = require("express-winston");
+const path = require("path");
+
+const { levels } = require("./static");
+const {
+  defaultConsoleFormat,
+  simpleHttpConsoleFormat,
+  reqHttpConsoleFormat,
+  defaultFileFormat,
+  simpleHttpFileFormat,
+  reqHttpFileFormat,
+  filterOnly,
+  filterWithout,
+} = require("./format");
+
+const rootPath = path.dirname(
+  require.main.filename || process.mainModule.filename
+);
+const logsPath = path.join(rootPath, "./logs");
+
+// Logging level for environment level
+const env = process.env.NODE_ENV || "development";
+const __level__ = env === "production" ? "info" : "debug";
+
+// Date for log filename
+const getDateString = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const day = `${date.getDate()}`.padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
+// Transports
+const transports = {
+  defaultConsole: new winston.transports.Console({
+    level: __level__,
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.simple(),
+      filterWithout("http"),
+      defaultConsoleFormat
+    ),
+  }),
+  simpleHttpConsole: new winston.transports.Console({
+    level: "http",
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.simple(),
+      filterOnly("http"),
+      simpleHttpConsoleFormat
+    ),
+  }),
+  reqHttpConsole: new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.simple(),
+      reqHttpConsoleFormat
+    ),
+  }),
+  defaultFile: new winston.transports.File({
+    filename: logsPath + `/${getDateString()}_debug.log`,
+    level: __level__,
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.simple(),
+      filterWithout("http"),
+      defaultFileFormat
+    ),
+  }),
+  simpleHttpFile: new winston.transports.File({
+    filename: logsPath + `/${getDateString()}_debug.log`,
+    level: "http",
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.simple(),
+      filterOnly("http"),
+      simpleHttpFileFormat
+    ),
+  }),
+  reqHttpFile: new winston.transports.File({
+    filename: logsPath + `/${getDateString()}_debug.log`,
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.simple(),
+      reqHttpFileFormat
+    ),
+  }),
+};
+
+// Logger
+const logger = winston.createLogger({
+  transports: [
+    transports.defaultConsole,
+    transports.simpleHttpConsole,
+    transports.defaultFile,
+    transports.simpleHttpFile,
+  ],
+  levels: levels,
+  // Do not exit on handled exceptions
+  exitOnError: false,
+});
+
+const reqLogger = (service) =>
+  expressWinston.logger({
+    transports: [transports.reqHttpConsole, transports.reqHttpFile],
+    meta: true,
+    statusLevels: false,
+    colorize: false,
+    // Pass service string via msg
+    msg: service,
+  });
+
+const reqErrorLogger = (message) =>
+  expressWinston.errorLogger({
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: logsPath + "/full.log" }),
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    msg: message,
+  });
+
+module.exports = { logger, reqLogger, reqErrorLogger };
