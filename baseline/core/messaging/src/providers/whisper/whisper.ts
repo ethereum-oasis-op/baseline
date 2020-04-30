34c163096e237368af2ea344804b1322946d9f6e
Samuel Stokes <sam.stokes@consensys.net>
2020-04-29 22:19:56 -0400

    WIP: adhere to interface

--- baseline/core/messaging/src/providers/whisper/whisper.ts
+++ baseline/core/messaging/src/providers/whisper/whisper.ts
@@ -13,118 +13,27 @@ const TTL = process.env.WHISPER_TTL || 20;
 const POW_TARGET = process.env.WHISPER_POW_TARGET || 2;
 
 export class WhisperService {
+  private keyId: string;
+  private recipientId: string;
 
-  constructor() { }
-
-  // If the Identities collection is empty, create a new Identity
-  async createFirstIdentity() {
-    const identities = await Identity.find({});
-    if (identities.length === 0) {
-      await this.createIdentity();
-    }
+  constructor(config) {
+    this.keyId = config.keyId;
+    this.recipientId = config.recipientId;
   }
 
-  // Create a new whisper identity
-  async createIdentity(): Promise<any> {
-    const web3 = await getWeb3();
-    const keyId = await web3.shh.newKeyPair();
-    const pubKey = await web3.shh.getPublicKey(keyId);
-    this.subscribe(DEFAULT_TOPIC, pubKey, this.checkMessageContent);
-
-    return {
-      keyId: keyId,
-      publicKey: await web3.shh.getPublicKey(keyId),
-    };
+  async connect() {
+    return getWeb3();
   }
 
   async isConnected(): Promise<any> {
     return isWeb3Connected();
   }
 
-  // Load previously created Whisper IDs from database into Whisper node
-  async loadIdentities() {
-    const identities = await Identity.find({});
-    identities.forEach(async (id) => {
-      try {
-        const web3 = await getWeb3();
-        const keyId = await web3.shh.addPrivateKey(id.privateKey);
-        const pubKey = await web3.shh.getPublicKey(keyId);
-        // keyId will change so need to update that in Mongo
-        await Identity.findOneAndUpdate(
-          { _id: pubKey },
-          { keyId },
-          { new: true },
-        );
-        await this.subscribe(DEFAULT_TOPIC, pubKey, this.checkMessageContent);
-      } catch (err) {
-        logger.error(
-          `Error adding public key ${id.publicKey} to Whisper node: ${err}`,
-        );
-      }
-    });
-  }
-
-  async checkMessageContent(data) {
-    const web3 = await getWeb3();
-    const content = await web3.utils.toAscii(data.payload);
-    // Check if this is a JSON structured message
-    const [isJSON, messageObj] = await hasJsonStructure(content);
-    // Store raw message
-    let doc = await storeNewMessage(data, content);
-    if (isJSON) {
-      if (messageObj.type === 'delivery_receipt') {
-        // Check if receipt came from original recipient
-        const originalMessage = await Message.findOne({
-          _id: messageObj.messageId,
-        });
-        if (!originalMessage) {
-          throw new Error(`Original message id (${messageObj.messageId}) not found. Cannot add delivery receipt.`);
-        } else if (originalMessage.recipientId === data.sig) {
-          // Update original message to indicate successful delivery
-          doc = await Message.findOneAndUpdate(
-            { _id: messageObj.messageId },
-            { deliveredDate: messageObj.deliveredDate },
-            { upsert: false, new: true },
-          );
-        }
-      } else {
-        this.sendDeliveryReceipt(data);
-      }
-      // Append source message ID to the object for tracking inbound
-      // messages from partners via the messenger API
-      messageObj.messageId = data.hash;
-      // Adding sender Id to message to know who sent the message
-      messageObj.senderId = data.sig;
-      // Send all JSON messages to processing service
-      forwardMessage(messageObj);
-    } else { // Text message
-      await this.sendDeliveryReceipt(data);
-    }
-    return doc;
-  }
-
-  async sendDeliveryReceipt(data) {
-    // Send delivery receipt back to sender
-    const time = await Math.floor(Date.now() / 1000);
-    const receiptObject = {
-      type: 'delivery_receipt',
-      deliveredDate: time,
-      messageId: data.hash,
-    };
-    const receiptString = JSON.stringify(receiptObject);
-    await this.publish(
-      undefined,
-      receiptString,
-      data.recipientPublicKey,
-      data.sig,
-    );
-  }
-
   async publish(
     subject = DEFAULT_TOPIC,
     payload: any,
-    senderId: any,
-    recipientId: any,
+    reply,
+    recipientId: string
   ): Promise<any> {
     let messageString = payload;
     if (typeof payload === 'object') {
@@ -133,13 +42,11 @@ export class WhisperService {
 
     const web3 = await getWeb3();
     const content = web3.utils.fromAscii(messageString);
-    const whisperId = await Identity.findOne({ _id: senderId });
-    const { keyId } = whisperId;
 
     try {
       const messageObj = {
-        pubKey: recipientId,
-        sig: keyId,
+        pubKey: this.recipientId,
+        sig: this.keyId,
         ttl: TTL,
         topic: subject,
         payload: content,
@@ -155,7 +62,6 @@ export class WhisperService {
 
   async subscribe(
     subject: string = DEFAULT_TOPIC,
-    keyId: string,
     callback: (msg: any) => void,
   ) {
     // Subscribe to private messages
@@ -163,7 +69,7 @@ export class WhisperService {
     web3.shh
       .subscribe('messages', {
         minPow: POW_TARGET,
-        privateKeyID: keyId,
+        privateKeyID: this.keyId,
         topics: [subject],
       })
       .on('data', async (data) => {
@@ -177,4 +83,40 @@ export class WhisperService {
         logger.error(err);
       });
   }
+
+  // Load previously created Whisper IDs from database into Whisper node
+  async loadIdentities(identities, topic, callback) {
+    let newIdentities = [];
+    if (identities.length === 0) {
+      this.createIdentity(topic, callback);
+    } else {
+      identities.forEach(async (id) => {
+        try {
+          const web3 = await getWeb3();
+          const keyId = await web3.shh.addPrivateKey(id.privateKey);
+          const publicKey = await web3.shh.getPublicKey(keyId);
+          newIdentities.push({ publicKey, keyId });
+          await this.subscribe(topic = DEFAULT_TOPIC, callback);
+        } catch (err) {
+          logger.error(
+            `Error adding public key ${id.publicKey} to Whisper node: ${err}`,
+          );
+        }
+      });
+    }
+    return newIdentities;
+  }
+
+  async createIdentity(topic = DEFAULT_TOPIC, callback) {
+    // Create new public/private key pair
+    const web3 = await getWeb3();
+    const keyId = await web3.shh.newKeyPair();
+    const publicKey = await web3.shh.getPublicKey(keyId);
+    const privateKey = await web3.shh.getPrivateKey(keyId);
+    const createdDate = await Math.floor(Date.now() / 1000);
+
+    this.subscribe(topic, callback);
+    return { publicKey, privateKey, keyId, createdDate };
+  }
+
 }
