9b2afa4998bd23cbb454a22e8fbdb6383b851c15
MCLDG <54769515+MCLDG@users.noreply.github.com>
2020-05-05 01:21:17 +0800

    radish on Amazon Linux (#134)

--- radish34/ui/src/components/UserSelection.js
+++ radish34/ui/src/components/UserSelection.js
@@ -3,6 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import { ServerSettingsContext } from '../contexts/server-settings-context';
+import { Config } from '../config-radish.json';
+
+const uriBuyer = `${Config.radishBuyerAPIURL}/graphql`;
+const uriSupplier1 = `${Config.radishSupplier1APIURL}/graphql`;
+const uriSupplier2 = `${Config.radishSupplier2APIURL}/graphql`;
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -29,7 +34,7 @@ const useStyles = makeStyles(() => ({
 const UserSelection = () => {
   const classes = useStyles();
   const [data, setData] = useState({ status: 504 });
-  const apiURL = window.localStorage.getItem('api') || 'localhost:8001/graphql';
+  const apiURL = window.localStorage.getItem('api') || `http://${uriBuyer}`;
   const user = window.localStorage.getItem('username') || 'Org1';
   const { settings } = useContext(ServerSettingsContext);
   const { organizationAddress } = settings || {};
@@ -52,11 +57,10 @@ const UserSelection = () => {
 
   const handleChange = event => {
     const users = {
-      Org1: { url: 'localhost:8001/graphql', role: 1 },
-      Supplier1: { url: 'localhost:8002/graphql', role: 2 },
-      Supplier2: { url: 'localhost:8003/graphql', role: 2 },
+      Org1: { url: uriBuyer, role: 1 },
+      Supplier1: { url: uriSupplier1, role: 2 },
+      Supplier2: { url: uriSupplier2, role: 2 },
     };
-
     window.location.reload(false);
     window.localStorage.setItem('api', users[event.target.value].url);
     window.localStorage.setItem('userRole', users[event.target.value].role);
