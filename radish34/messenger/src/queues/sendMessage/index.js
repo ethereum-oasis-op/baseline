39473264ee0587e32a23e1fe04081be8d673f4d8
Myron Rotter <myronrotter01@t-online.de>
2020-05-15 14:11:34 +0200

    Add logger; replace console.log/old logger

--- radish34/messenger/src/queues/sendMessage/index.js
+++ radish34/messenger/src/queues/sendMessage/index.js
@@ -1,7 +1,7 @@
 const Queue = require('bull');
-const logger = require('winston');
 const { setQueues } = require('bull-board');
 const Config = require('../../../config');
+const { logger } = require('radish34-logger');
 
 const requestNamespace = `baseline:messenger:sendMessage:req`;
 const responseNamespace = `baseline:messenger:sendMessage:res`;
@@ -11,11 +11,11 @@ let sendMessageResQueue;
 
 try {
   sendMessageReqQueue = new Queue(requestNamespace, Config.users[0].redisUrl);
-  logger.debug(`SUCCESS: connected to bull queue "${requestNamespace}" at ${Config.users[0].redisUrl}`);
+  logger.debug(`Successfully connected to bull queue "${requestNamespace}" at ${Config.users[0].redisUrl}.`, { service: 'MESSENGER' });
   sendMessageResQueue = new Queue(responseNamespace, Config.users[0].redisUrl);
-  logger.debug(`SUCCESS: connected to bull queue "${responseNamespace}" at ${Config.users[0].redisUrl}`);
+  logger.debug(`Successfully connected to bull queue "${responseNamespace}" at ${Config.users[0].redisUrl}.`, { service: 'MESSENGER' });
 } catch (error) {
-  logger.error(`ERROR: could not connect to bull queue "baseline:messenger:sendMessage" at ${Config.users[0].redisUrl}`);
+  logger.error(`Could not connect to bull queue "baseline:messenger:sendMessage" at ${Config.users[0].redisUrl}.`, { service: 'MESSENGER' });
 }
 
 // Adding Queue to BullBoard Admin UI
