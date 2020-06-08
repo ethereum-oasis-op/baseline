569cc24b15c40ce2b785ce87513a662b54006a36
George Spasov <spasov05203@gmail.com>
2020-06-08 14:32:07 +0300

    D365 to Google Sheets Demo Example

---
+++ examples/google-sheets-connector/d365/BaselineAPI/Controllers/Proposal/ProposalController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using AuthenticationUtility;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace BaselineApi.Controllers.Proposal
+{
+    public class Proposal
+    {
+        public string rfqCaseId { get; set; }
+        public string purchQty { get; set; }
+        public string purchUnit { get; set; }
+        public string purchPrice { get; set; }
+        public string lineNum { get; set; }
+
+    }
+
+    public class ProposalController : ApiController
+    {
+        public static string RFQStatusCheck = ClientConfiguration.Default.UriString + "data" + "/RequestForQuotationReplyHeaders?$select=RFQCaseNumber,HighestRFQStatus&$filter=RFQCaseNumber%20eq%20";
+        public static string ODataEntityPath = ClientConfiguration.Default.UriString + "data";
+        public static string ManageProposalActionPath = "/HandleRFQReplies/Microsoft.Dynamics.DataEntities.manageProposal";
+        public static string MediaType = "application/json";
+
+        // GET: api/Proposal/000385
+        public async Task<bool> Get(int id)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaType));
+
+            var authenticationHeader = OAuthHelper.GetAuthenticationHeader(useWebAppAuthentication: true);
+            string[] splittedToken = authenticationHeader.Split(' ');
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", splittedToken[1]);
+
+            string rfqUrl = RFQStatusCheck + "%27" + id.ToString("D6") + "%27";
+            var response = await client.GetAsync(rfqUrl);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            dynamic json = JsonConvert.DeserializeObject(contents);
+            dynamic rfqValue = json["value"];
+            
+            if (rfqValue.Count > 0)
+            {
+                string status = rfqValue[0]["HighestRFQStatus"];
+
+                if (status == "Sent")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // POST: api/Proposal
+        public async Task<string> Post(Proposal value)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaType));
+
+            var authenticationHeader = OAuthHelper.GetAuthenticationHeader(useWebAppAuthentication: true);
+            string[] splittedToken = authenticationHeader.Split(' ');
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", splittedToken[1]);
+
+            PropertyInfo[] proposalInfos = value.GetType().GetProperties();
+            Dictionary<string, string> proposalDictionary = new Dictionary<string, string>();
+
+            foreach (PropertyInfo info in proposalInfos)
+            {
+                proposalDictionary.Add(info.Name, info.GetValue(value, null).ToString());
+            }
+
+            var json = JsonConvert.SerializeObject(proposalDictionary, Formatting.Indented);
+
+            var stringContent = new StringContent(json, Encoding.UTF8, MediaType);
+
+            var response = await client.PostAsync(ODataEntityPath + ManageProposalActionPath, stringContent);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            return contents;
+        }
+    }
+}
---
+++ examples/google-sheets-connector/d365/README.md
@@ -0,0 +1,15 @@
+# baseline-integration
+
+## BaselineConnectorDLL
+- connecting the D365 environment with the baseline proxy;
+
+## BaselineDynamicsExtensions
+- event handler extensions related to: 
+  - sending a RFQ;
+  - posting a price disc agreement;
+  - confirming a purchase order;
+- preparation of data sent to the dll connector;
+
+## BaselineAPI
+- checking the status of RFQ by id;
+- managing the proposal;
