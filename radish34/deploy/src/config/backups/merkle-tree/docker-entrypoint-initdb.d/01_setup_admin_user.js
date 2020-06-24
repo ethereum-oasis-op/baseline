596d18ddd6b120b33217933d84d645d512f53426
Kyle Thomas <kyle@provide.services>
2020-06-24 15:06:44 -0400

    Move radish34/ -> examples/radish34/

--- radish34/deploy/src/config/backups/merkle-tree/docker-entrypoint-initdb.d/01_setup_admin_user.js
+++
@@ -1,9 +0,0 @@
-this.db.createUser({
-  user: 'admin',
-  pwd: 'admin',
-  roles: [
-    { role: 'userAdmin', db: 'merkle_tree' },
-    { role: 'dbAdmin', db: 'merkle_tree' },
-    { role: 'readWrite', db: 'merkle_tree' },
-  ],
-});
