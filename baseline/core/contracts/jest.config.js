4c65979278f3b251fc4b2aa5102521917d41f1a3
Kyle Thomas <kyle@provide.services>
2020-06-30 04:42:54 -0400

    Fix core contracts test harness

--- baseline/core/contracts/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    verbose: true,
-    setupTestFrameworkScriptFile: "./jest.setup.js",
-    runner: "truffle-jest/lib/runner",
-    globalSetup: "truffle-jest/lib/setup",
-    globalTeardown: "truffle-jest/lib/teardown",
-    testEnvironment: "truffle-jest/lib/environment"
-  };
-  
