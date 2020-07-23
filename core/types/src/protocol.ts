2e42e1306f1104603fc8c7bfde0a2c929abd9add
Kyle Thomas <kyle@provide.services>
2020-07-23 03:17:27 -0400

    Cleanup inline comments describing protocol message type

--- core/types/src/protocol.ts
+++ core/types/src/protocol.ts
@@ -13,8 +13,8 @@ export type Message = {
   opcode: Opcode; // up to 40 bits
   recipient: string; // up to 336 bits
   shield: string; // up to 336 bits
-  identifier: string; // 128 bits (i.e., uuidv4 circuit/workflow identifier)
-  signature: string; // 64 bits
+  identifier: string; // up to 288 bits (i.e., UUIDv4 circuit/workflow identifier)
+  signature: string; // 512 bits
   type: PayloadType; // 1 bit
   payload: Buffer; // arbitrary length
 }
