1fb4e9ffc9752afd2bf846ed59e6d756ef1c1e68
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-08-23 22:35:08 +0100

    add utils

--- core/api/test/baseline-rpc.spec.ts
+++ core/api/test/baseline-rpc.spec.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers';
 import { baselineProviderRpc, baselineServiceFactory } from '../src/index';
+import { promisedTimeout } from './utils';
 
 const defaultAccountAddress = '0x7e5f4552091a69125d5dfcb7b8c2659029395bdf';
 
