5773284f26b9c22ebea4069d0e02a94e69921544
Kyle Thomas <kyle@provide.services>
2020-09-29 03:10:12 -0400

    Run createAgreement circuit lifecycle successfully

--- core/privacy/test/zokrates.spec.ts
+++ core/privacy/test/zokrates.spec.ts
@@ -3,8 +3,8 @@ import { zkSnarkCircuitProviderServiceFactory, zkSnarkCircuitProviderServiceZokr
 
 const noopAgreementCircuitPath = '../../lib/circuits/noopAgreement.zok';
 const createAgreementPath = '../../lib/circuits/createAgreement.zok';
-// const signAgreementPath = '../../lib/circuits/signAgreement.zok';
-          
+const signAgreementPath = '../../lib/circuits/signAgreement.zok';
+
 let provider;
 
 describe('when the underlying zokrates provider is available', () => {
@@ -14,12 +14,25 @@ describe('when the underlying zokrates provider is available', () => {
 
   it('runs the noopAgreement circuit lifecycle successfully', () => {
     shouldBehaveLikeZKSnarkCircuit(provider, noopAgreementCircuitPath, ['2']); // Todo: Update witness arguments
-    // shouldBehaveLikeZKSnarkCircuit(provider, createAgreementPath, ['2']); // Todo: Update witness arguments
-    shouldBehaveLikeZKSnarkCircuit(provider, createAgreementPath, 
-                                    [1, 
-                                     [[2, 3], [4, 5]],
-                                     [[6, 7], 8, 9]]
-                                  ); // Todo: Update witness arguments
+  });
+
+  it('runs the createAgreement circuit lifecycle successfully', () => {
+    const args = [
+      '1', 
+      {
+        value: ['3', '1'],
+        salt: ['3', '5'],
+      },
+      {
+        senderPublicKey: ['6', '7'],
+        agreementName: '8',
+        agreementUrl: '9',
+      }
+    ];
+    shouldBehaveLikeZKSnarkCircuit(provider, createAgreementPath, args);
+  });
+
+  it('runs the signAgreement circuit lifecycle successfully', () => {
     // shouldBehaveLikeZKSnarkCircuit(provider, signAgreementPath, ['2']); // Todo: Update witness arguments
   });
 });
