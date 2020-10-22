13a75a964d25f778f63f9ba4b156ea0689f3a513
Samuel Stokes <sam.stokes@consensys.net>
2020-10-22 18:10:17 -0400

    Add IVerifier.sol contract to core

---
+++ core/contracts/contracts/privacy/IVerifier.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: CC0
+pragma solidity ^0.6.9;
+
+/**
+@title IVerifier
+@dev Example Verifier Implementation
+@notice Do not use this example in any production code!
+*/
+interface IVerifier {
+
+    function verify(
+        uint256[] calldata _proof,
+        uint256[] calldata _inputs
+    ) external returns (bool result);
+}
