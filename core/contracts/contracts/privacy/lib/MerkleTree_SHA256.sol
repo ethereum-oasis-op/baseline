8ae6962e3fecb1663042baa3c942eedae039e491
Samuel Stokes <sam.stokes@consensys.net>
2020-10-23 17:04:38 -0400

    Initial version of Shield test suite

--- core/contracts/contracts/privacy/lib/MerkleTree_SHA256.sol
+++ core/contracts/contracts/privacy/lib/MerkleTree_SHA256.sol
@@ -13,40 +13,37 @@ pragma solidity ^0.6.9;
 
 contract MerkleTree_sha256 {
 
-    /*
-    @notice Explanation of the Merkle Tree in this contract:
-    This is an append-only merkle tree; populated from left to right.
-    We do not store all of the merkle tree's nodes. We only store the right-most 'frontier' 
-    of nodes required to calculate the new root when the next new leaf value is added.
-
-                      TREE (not stored)                       FRONTIER (stored)
-
-                                 0                                     ?
-                          /             \
-                   1                             2                     ?
-               /       \                     /       \
-           3             4               5               6             ?
-         /   \         /   \           /   \           /    \
-       7       8      9      10      11      12      13      14        ?
-     /  \    /  \   /  \    /  \    /  \    /  \    /  \    /  \
-    15  16  17 18  19  20  21  22  23  24  25  26  27  28  29  30      ?
-
-    level  row  width  start#     end#
-      4     0   2^0=1   w=0     2^1-1=0
-      3     1   2^1=2   w=1     2^2-1=2
-      2     2   2^2=4   w=3     2^3-1=6
-      1     3   2^3=8   w=7     2^4-1=14
-      0     4   2^4=16  w=15    2^5-1=30
-
-    height = 4
-    w = width = 2 ** height = 2^4 = 16
-    #nodes = (2 ** (height + 1)) - 1 = 2^5-1 = 31
-
-    */
-
     /**
-    These events are what the merkle-tree microservice's filters will listen for.
+      @notice Explanation of the Merkle Tree in this contract:
+      This is an append-only merkle tree; populated from left to right.
+      We do not store all of the merkle tree's nodes. We only store the right-most 'frontier' 
+      of nodes required to calculate the new root when the next new leaf value is added.
+
+                        TREE (not stored)                       FRONTIER (stored)
+
+                                   0                                     ?
+                            /             \
+                     1                             2                     ?
+                 /       \                     /       \
+             3             4               5               6             ?
+           /   \         /   \           /   \           /    \
+         7       8      9      10      11      12      13      14        ?
+       /  \    /  \   /  \    /  \    /  \    /  \    /  \    /  \
+      15  16  17 18  19  20  21  22  23  24  25  26  27  28  29  30      ?
+
+      level  row  width  start#     end#
+        4     0   2^0=1   w=0     2^1-1=0
+        3     1   2^1=2   w=1     2^2-1=2
+        2     2   2^2=4   w=3     2^3-1=6
+        1     3   2^3=8   w=7     2^4-1=14
+        0     4   2^4=16  w=15    2^5-1=30
+
+      height = 4
+      w = width = 2 ** height = 2^4 = 16
+      #nodes = (2 ** (height + 1)) - 1 = 2^5-1 = 31
     */
+
+    // These events are what the merkle-tree microservice's filters will listen for.
     event NewLeaf(uint leafIndex, bytes32 leafValue, bytes32 root);
     event NewLeaves(uint minLeafIndex, bytes32[] leafValues, bytes32 root);
  
