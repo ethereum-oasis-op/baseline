084c71a8336681ad9789c5dadb0c5078ebfdad61
Samuel Stokes <sam.stokes@consensys.net>
2020-10-26 15:56:26 -0400

    Conform to solidity naming convention. Name params in contract interfaces

--- core/contracts/contracts/privacy/lib/VerifierNoop.sol
+++ core/contracts/contracts/privacy/lib/VerifierNoop.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.6.9;
 
 // Verifies all calls containing proper parameters
 // Do not use this example in any production code!
-contract Verifier_noop {
+contract VerifierNoop {
 
     function verify(
         uint256[] calldata _proof,
-        uint256[] calldata _inputs
+        uint256[] calldata _publicInputs
     ) external pure returns (bool result) {
         require(_proof[0] != 0, "Proof is undefined");
-        require(_inputs[0] != 0, "Input is undefined");
+        require(_publicInputs[0] != 0, "Input is undefined");
         return true;
     }
     
