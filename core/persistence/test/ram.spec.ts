1c64c6326e08c6736756c916269986749d1716f6
Myron Rotter <myronrotter01@t-online.de>
2021-01-04 15:37:59 +0100

    Adjust publish and alert

--- core/persistence/test/ram.spec.ts
+++ core/persistence/test/ram.spec.ts
@@ -32,6 +32,26 @@ describe('RAM persistence provider', () => {
       let subscribed = await provider.subscribe(params);
       assert.deepEqual(subscribed.identifiers, params.fields);
     });
+
+    it('existing fields via RegExp', async () => {
+      const params = {
+        fields: new RegExp('0', 'g')
+      }
+      let subscribed = await provider.subscribe(params);
+      assert.deepEqual(subscribed.expressions[0], params.fields);
+      subscribed = await provider.subscribe(params);
+      assert.lengthOf(subscribed.expressions, 1);
+    });
+
+    it('existing fields via string', async () => {
+      const params = {
+        fields: '3'
+      }
+      let subscribed = await provider.subscribe(params);
+      assert.deepEqual(subscribed.identifiers[0], params.fields);
+      subscribed = await provider.subscribe(params);
+      assert.lengthOf(subscribed.identifiers, 1);
+    });
   });
 
   describe('unsubscribe to', () => {
@@ -68,6 +88,27 @@ describe('RAM persistence provider', () => {
       subscribed = await provider.unsubscribe({ fields: ['0', '1'] });
       assert.deepEqual(subscribed.identifiers, ['2']);
     });
+
+    it('not existing fields', async () => {
+      const paramsRegExp = {
+        fields: new RegExp('0', 'g')
+      }
+      const paramsString = {
+        fields: '0'
+      }
+      const paramsStringArray = {
+        fields: ['0', '1', '2']
+      }
+      let subscribed = await provider.unsubscribe(paramsRegExp);
+      assert.deepEqual(subscribed.identifiers, []);
+      assert.deepEqual(subscribed.expressions, []);
+      subscribed = await provider.unsubscribe(paramsString);
+      assert.deepEqual(subscribed.identifiers, []);
+      assert.deepEqual(subscribed.expressions, []);
+      subscribed = await provider.unsubscribe(paramsStringArray);
+      assert.deepEqual(subscribed.identifiers, []);
+      assert.deepEqual(subscribed.expressions, []);
+    });
   });
 
   describe('subscribe, publish and alert', () => {
@@ -85,23 +126,18 @@ describe('RAM persistence provider', () => {
 
     it('publish records', async () => {
       const params = {
-        records: [{ identifier: '0', hash: '0' }, { identifier: '4', hash: '4' }]
-      }
-      let published = await provider.publish(params);
-      assert.deepEqual(published, params.records);
-    });
-
-    it('publish same record with altered hash', async () => {
-      const params = {
-        records: [{ identifier: '0', hash: '00' }]
+        records: [{ identifier: '0' }, { identifier: '4' }]
       }
       let published = await provider.publish(params);
       assert.deepEqual(published, params.records);
     });
 
     it('alert', async () => {
-      let alerts = await provider.alert();
-      assert.deepEqual(alerts, [{ identifier: '0', hash: '00' }]);
+      const params = {
+        alerts: [{identifier: '4', message: 'Hello World!'}, {identifier: '0', message: 'This record needs to be baselined!'}]
+      }
+      let alerts = await provider.alert(params);
+      assert.deepEqual(alerts, params.alerts);
     });
   });
 });
