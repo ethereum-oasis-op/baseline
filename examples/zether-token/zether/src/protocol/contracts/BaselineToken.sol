a945216902b7befa5b765bdd3ff470151a487265
Gokul Alex <gokulsan@users.noreply.github.com>
2021-01-28 22:03:48 +0530

    Update BaselineToken.sol

--- examples/zether-token/zether/src/protocol/contracts/BaselineToken.sol
+++ examples/zether-token/zether/src/protocol/contracts/BaselineToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.11;
 
-import "./../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract BaselineToken is ERC20Mintable {
     // hack...
