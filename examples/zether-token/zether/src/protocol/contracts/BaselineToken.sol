1cd2b406875e3bb9697b82ab55f71cce60a1ed28
Gokul Alex <gokulsan@users.noreply.github.com>
2021-01-28 22:01:58 +0530

    Update and rename CashToken.sol to BaselineToken.sol

--- examples/zether-token/zether/src/protocol/contracts/BaselineToken.sol
+++ examples/zether-token/zether/src/protocol/contracts/BaselineToken.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.5.11;
 
 import "./../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract CashToken is ERC20Mintable {
+contract BaselineToken is ERC20Mintable {
     // hack...
 }
