e2fde34faa31f985af5fd6e61ddc4a6a71c99e6a
Samuel Stokes <sam.stokes@mesh.xyz>
2021-06-23 13:33:58 -0400

    bri-2: add single prettierrc for consistent formatting

--- examples/bri-2/commit-mgr/src/blockchain/utils.js
+++ examples/bri-2/commit-mgr/src/blockchain/utils.js
@@ -13,130 +13,130 @@ let ws_provider;
 
 export const http_provider = new ethers.providers.JsonRpcProvider(process.env.ETH_CLIENT_HTTP);
 export const get_ws_provider = () => {
-	if (!ws_provider) {
-		try {
-			ws_provider = new ethers.providers.WebSocketProvider(process.env.ETH_CLIENT_WS);
-			ws_provider._websocket.on('error', (error) => {
-				logger.error(`[WEBSOCKET] "error" event: ${error.stack}`);
-				ws_provider = undefined;
-			});
-			ws_provider._websocket.on('close', (event) => {
-				logger.error(`[WEBSOCKET] "close" event: ${event}`);
-				ws_provider = undefined;
-			});
-			logger.info(`Established websocket connection to Ethereum client: ${process.env.ETH_CLIENT_WS}`);
-		} catch (err) {
-			logger.error(`[WEBSOCKET] Cannot establish connection: ${process.env.ETH_CLIENT_WS}`);
-		}
-	}
-	return ws_provider;
+  if (!ws_provider) {
+    try {
+      ws_provider = new ethers.providers.WebSocketProvider(process.env.ETH_CLIENT_WS);
+      ws_provider._websocket.on('error', (error) => {
+        logger.error(`[WEBSOCKET] "error" event: ${error.stack}`);
+        ws_provider = undefined;
+      });
+      ws_provider._websocket.on('close', (event) => {
+        logger.error(`[WEBSOCKET] "close" event: ${event}`);
+        ws_provider = undefined;
+      });
+      logger.info(`Established websocket connection to Ethereum client: ${process.env.ETH_CLIENT_WS}`);
+    } catch (err) {
+      logger.error(`[WEBSOCKET] Cannot establish connection: ${process.env.ETH_CLIENT_WS}`);
+    }
+  }
+  return ws_provider;
 };
 
 // Meant to be called everytime this commit-mgr service is restarted
 export const restartSubscriptions = async () => {
-	const activeTrees = await merkleTrees.find({
-		_id: { $regex: /_0$/ },
-		active: true
-	});
+  const activeTrees = await merkleTrees.find({
+    _id: { $regex: /_0$/ },
+    active: true
+  });
 
-	const provider = get_ws_provider();
-	provider.on('block', async (result) => {
-		logger.debug(`NEW BLOCK: %o`, result);
-	});
+  const provider = get_ws_provider();
+  provider.on('block', async (result) => {
+    logger.debug(`NEW BLOCK: %o`, result);
+  });
 
-	// For all 'active' MerkleTrees, search through old logs for any
-	// newLeaf events we missed while service was offline. Then resubscribe
-	// to the events.
-	for (let i = 0; i < activeTrees.length; i++) {
-		const contractAddress = activeTrees[i]._id.slice(0, -2);
-		const fromBlock = activeTrees[i].latestLeaf ? activeTrees[i].latestLeaf.blockNumber : 0;
-		await checkChainLogs(contractAddress, fromBlock);
-		subscribeMerkleEvents(contractAddress);
-	}
+  // For all 'active' MerkleTrees, search through old logs for any
+  // newLeaf events we missed while service was offline. Then resubscribe
+  // to the events.
+  for (let i = 0; i < activeTrees.length; i++) {
+    const contractAddress = activeTrees[i]._id.slice(0, -2);
+    const fromBlock = activeTrees[i].latestLeaf ? activeTrees[i].latestLeaf.blockNumber : 0;
+    await checkChainLogs(contractAddress, fromBlock);
+    subscribeMerkleEvents(contractAddress);
+  }
 };
 
 export const checkChainLogs = async (contractAddress, fromBlock) => {
-	// If fromBlock is provided, check next block so we don't add a leaf that was already captured
-	const blockNum = fromBlock ? fromBlock + 1 : 0;
-	logger.info(
-		`Checking chain logs for missed newLeaf events starting at block ${fromBlock} for contract: ${contractAddress}`
-	);
-	// besu has a bug where 'eth_getLogs' expects 'fromBlock' to be a string instead of integer
-	let convertedBlockNum = blockNum;
-	switch (process.env.ETH_CLIENT_TYPE) {
-		case 'besu':
-			convertedBlockNum = `${blockNum}`;
-			break;
-		case 'infura-gas':
-			convertedBlockNum = '0x' + blockNum.toString(16);
-			break;
-		case 'infura':
-			convertedBlockNum = '0x' + blockNum.toString(16);
-			break;
-	}
+  // If fromBlock is provided, check next block so we don't add a leaf that was already captured
+  const blockNum = fromBlock ? fromBlock + 1 : 0;
+  logger.info(
+    `Checking chain logs for missed newLeaf events starting at block ${fromBlock} for contract: ${contractAddress}`
+  );
+  // besu has a bug where 'eth_getLogs' expects 'fromBlock' to be a string instead of integer
+  let convertedBlockNum = blockNum;
+  switch (process.env.ETH_CLIENT_TYPE) {
+    case 'besu':
+      convertedBlockNum = `${blockNum}`;
+      break;
+    case 'infura-gas':
+      convertedBlockNum = '0x' + blockNum.toString(16);
+      break;
+    case 'infura':
+      convertedBlockNum = '0x' + blockNum.toString(16);
+      break;
+  }
 
-	const params = {
-		fromBlock: convertedBlockNum,
-		toBlock: 'latest',
-		address: contractAddress,
-		topics: [newLeafEvent]
-	};
-	const res = await jsonrpc('eth_getLogs', [params]);
-	const logs = res.result;
+  const params = {
+    fromBlock: convertedBlockNum,
+    toBlock: 'latest',
+    address: contractAddress,
+    topics: [newLeafEvent]
+  };
+  const res = await jsonrpc('eth_getLogs', [params]);
+  const logs = res.result;
 
-	const contractInterface = new ethers.utils.Interface(shieldContract.abi);
+  const contractInterface = new ethers.utils.Interface(shieldContract.abi);
 
-	for (let i = 0; i < logs.length; i++) {
-		const txLogs = contractInterface.parseLog(logs[i]);
-		const leafIndex = txLogs.args[0].toNumber();
-		const leafValue = txLogs.args[1];
-		logger.info(`Found previously missed leaf index ${leafIndex} of value ${leafValue}`);
+  for (let i = 0; i < logs.length; i++) {
+    const txLogs = contractInterface.parseLog(logs[i]);
+    const leafIndex = txLogs.args[0].toNumber();
+    const leafValue = txLogs.args[1];
+    logger.info(`Found previously missed leaf index ${leafIndex} of value ${leafValue}`);
 
-		const leaf = {
-			hash: leafValue,
-			leafIndex: leafIndex,
-			txHash: logs[i].transactionHash,
-			blockNumber: logs[i].blockNumber
-		};
-		await insertLeaf(contractAddress, leaf);
-	}
+    const leaf = {
+      hash: leafValue,
+      leafIndex: leafIndex,
+      txHash: logs[i].transactionHash,
+      blockNumber: logs[i].blockNumber
+    };
+    await insertLeaf(contractAddress, leaf);
+  }
 };
 
 export const jsonrpc = async (method, params, id) => {
-	const response = await axios.post(process.env.ETH_CLIENT_HTTP, {
-		jsonrpc: '2.0',
-		id: id || 1,
-		method: method,
-		params: params
-	});
-	logger.debug(`${method} result:`, response.data);
-	return response.data;
+  const response = await axios.post(process.env.ETH_CLIENT_HTTP, {
+    jsonrpc: '2.0',
+    id: id || 1,
+    method: method,
+    params: params
+  });
+  logger.debug(`${method} result:`, response.data);
+  return response.data;
 };
 
 export const chainName = () => {
-	let name;
-	switch (parseInt(process.env.CHAIN_ID, 10)) {
-		case 1:
-			name = 'MAINNET';
-			break;
-		case 3:
-			name = 'ROPSTEN';
-			break;
-		case 4:
-			name = 'RINKEBY';
-			break;
-		case 5:
-			name = 'GOERLI';
-			break;
-		case 42:
-			name = 'KOVAN';
-			break;
-		case 101010:
-			name = 'LOCAL';
-			break;
-		default:
-			name = 'LOCAL';
-			break;
-	}
-	return name;
+  let name;
+  switch (parseInt(process.env.CHAIN_ID, 10)) {
+    case 1:
+      name = 'MAINNET';
+      break;
+    case 3:
+      name = 'ROPSTEN';
+      break;
+    case 4:
+      name = 'RINKEBY';
+      break;
+    case 5:
+      name = 'GOERLI';
+      break;
+    case 42:
+      name = 'KOVAN';
+      break;
+    case 101010:
+      name = 'LOCAL';
+      break;
+    default:
+      name = 'LOCAL';
+      break;
+  }
+  return name;
 };
--- examples/bri-2/commit-mgr/src/index.ts
+++ examples/bri-2/commit-mgr/src/index.ts
@@ -8,61 +8,61 @@ import { connectNATS } from './nats';
 import { get_ws_provider, restartSubscriptions } from './blockchain';
 import { jsonRpcHandler } from './route-handlers/rpc-methods';
 import {
-	getStatus,
-	getSettings,
-	getCommits,
-	getCommit,
-	createCommit,
-	sendCommitToPartners,
-	sendCommitOnChain,
-	getMerkleTrees,
-	getMerkleTree,
-	createMerkleTree,
-	deleteMerkleTree,
-	deleteAllMerkleTrees,
-	getContracts,
-	deleteAllContracts
+  getStatus,
+  getSettings,
+  getCommits,
+  getCommit,
+  createCommit,
+  sendCommitToPartners,
+  sendCommitOnChain,
+  getMerkleTrees,
+  getMerkleTree,
+  createMerkleTree,
+  deleteMerkleTree,
+  deleteAllMerkleTrees,
+  getContracts,
+  deleteAllContracts
 } from './route-handlers/rest-methods';
 
 const main = async () => {
-	logger.info('Starting commitment manager server...');
-	dotenv.config();
-	const port = process.env.SERVER_PORT;
-	const dbUrl = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
+  logger.info('Starting commitment manager server...');
+  dotenv.config();
+  const port = process.env.SERVER_PORT;
+  const dbUrl = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
 
-	logger.debug(`Attempting to connect to db: ${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`);
-	await dbConnect(dbUrl);
-	await connectNATS();
-	await get_ws_provider(); // Establish websocket connection
-	await restartSubscriptions(); // Enable event listeners for active MerkleTrees
+  logger.debug(`Attempting to connect to db: ${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`);
+  await dbConnect(dbUrl);
+  await connectNATS();
+  await get_ws_provider(); // Establish websocket connection
+  await restartSubscriptions(); // Enable event listeners for active MerkleTrees
 
-	const app = express();
-	app.use(reqLogger('COMMIT-MGR')); // Log requests
-	app.use(reqErrorLogger('COMMIT-MGR')); // Log errors
-	app.use(bodyParser.json({ limit: '2mb' })); // Pre-parse body content
-	app.use(cors());
+  const app = express();
+  app.use(reqLogger('COMMIT-MGR')); // Log requests
+  app.use(reqErrorLogger('COMMIT-MGR')); // Log errors
+  app.use(bodyParser.json({ limit: '2mb' })); // Pre-parse body content
+  app.use(cors());
 
-	app.get('/status', getStatus);
-	app.get('/settings', getSettings);
-	app.get('/commits', getCommits);
-	app.get('/commits/:commitId', getCommit);
-	app.post('/commits', createCommit);
-	app.post('/commits/:commitId/send-partners', sendCommitToPartners);
-	app.post('/commits/:commitId/send-mainnet', sendCommitOnChain);
-	app.get('/merkle-trees', getMerkleTrees);
-	app.get('/merkle-trees/:treeId', getMerkleTree);
-	app.post('/merkle-trees', createMerkleTree);
-	app.post('/merkle-trees/:treeId/delete', deleteMerkleTree);
-	app.post('/delete/merkle-trees', deleteAllMerkleTrees);
-	app.post('/delete/contracts', deleteAllContracts);
-	app.get('/contracts', getContracts);
+  app.get('/status', getStatus);
+  app.get('/settings', getSettings);
+  app.get('/commits', getCommits);
+  app.get('/commits/:commitId', getCommit);
+  app.post('/commits', createCommit);
+  app.post('/commits/:commitId/send-partners', sendCommitToPartners);
+  app.post('/commits/:commitId/send-mainnet', sendCommitOnChain);
+  app.get('/merkle-trees', getMerkleTrees);
+  app.get('/merkle-trees/:treeId', getMerkleTree);
+  app.post('/merkle-trees', createMerkleTree);
+  app.post('/merkle-trees/:treeId/delete', deleteMerkleTree);
+  app.post('/delete/merkle-trees', deleteAllMerkleTrees);
+  app.post('/delete/contracts', deleteAllContracts);
+  app.get('/contracts', getContracts);
 
-	// Single endpoint to handle all JSON-RPC requests
-	// app.use(rpcServer.middleware());
-	app.post('/jsonrpc', jsonRpcHandler);
+  // Single endpoint to handle all JSON-RPC requests
+  // app.use(rpcServer.middleware());
+  app.post('/jsonrpc', jsonRpcHandler);
 
-	app.listen(port, () => {
-		logger.info(`REST server listening on port ${port}.`);
-	});
+  app.listen(port, () => {
+    logger.info(`REST server listening on port ${port}.`);
+  });
 };
 main();
