660a009cbfebc330ab173f74e7b0ffd360553ea7
ognjenkurtic <kurticognjen@gmail.com>
2021-11-10 13:29:59 +0100

    Add tsconfig with mocha types and an example test

---
+++ examples/bpi-monolith-test-suite/tests/example.ts
@@ -0,0 +1,9 @@
+
+import {expect} from 'chai';
+
+describe('Example tests', () => {
+    it('Should run a simple example test and assert success', () => {
+        const testVar = 1;
+        expect(testVar).to.be.equal(1);
+    });
+});
