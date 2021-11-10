d72be2640b2af73f9762e92965b2c02ccc9de4e8
ognjenkurtic <kurticognjen@gmail.com>
2021-11-10 13:35:54 +0100

    Add dummy BPI class and add it to the example test

--- examples/bpi-monolith-test-suite/tests/example.ts
+++ examples/bpi-monolith-test-suite/tests/example.ts
@@ -1,9 +1,16 @@
+import { BPI }  from '../bpi/bpi';
+import { expect } from 'chai';
 
-import {expect} from 'chai';
 
 describe('Example tests', () => {
     it('Should run a simple example test and assert success', () => {
-        const testVar = 1;
-        expect(testVar).to.be.equal(1);
+        const aliceBpi = new BPI();
+        aliceBpi.TestProperty = 1;
+
+        const bobBpi = new BPI();
+        bobBpi.TestProperty = 2; 
+
+        expect(aliceBpi.TestProperty).to.be.equal(1);
+        expect(bobBpi.TestProperty).to.be.equal(2);
     });
 });
