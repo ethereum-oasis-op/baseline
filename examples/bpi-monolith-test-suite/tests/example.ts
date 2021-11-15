c0d3d04d7cb57c252cd02501e1fabcf446ee29f1
dewi-tim <dewi-tim@nethermind.io>
2021-11-15 20:07:13 +0000

    adds workgroup tests

--- examples/bpi-monolith-test-suite/tests/example.ts
+++ examples/bpi-monolith-test-suite/tests/example.ts
@@ -22,20 +22,19 @@ describe('Org and Workgroup setup', () => {
         const aliceBpi = new BPI();
 
         const workgroup = aliceBpi.addWorkgroup('123','MyWorkgroup');
-
+        expect(aliceBpi.getWorkgroupById(workgroup.id).name).is.equal(workgroup.name);
 
 
 
 
         
         // TODO: We assume there exists a external source of organization id's and addresses for contact
-        // aliceBpi.InviteOrganization(bobId)
-        // aliceBpi.TestProperty = 1;
 
-        // const bobBpi = new BPI();
-        // bobBpi.TestProperty = 2; 
-
-        // expect(aliceBpi.TestProperty).to.be.equal(1);
-        // expect(bobBpi.TestProperty).to.be.equal(2);
     });
+    it('Alice searches for a new workgrop that does not exist', () => {
+        const aliceBpi = new BPI();
+        const workgroup = aliceBpi.addWorkgroup('123','MyWorkgroup');
+
+        expect(aliceBpi.getWorkgroupById("nonsense")).to.be.undefined;
+    })
 });
