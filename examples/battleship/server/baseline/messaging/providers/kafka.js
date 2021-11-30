0740cf731280c8ca7d857f5bd9eeb2a1e91fb8e8
Jack Proudfoot <jsp50@duke.edu>
2021-11-29 22:35:56 -0500

    Added id to kafka messenger

--- examples/battleship/server/baseline/messaging/providers/kafka.js
+++ examples/battleship/server/baseline/messaging/providers/kafka.js
@@ -2,15 +2,15 @@
  * A kafka messaging provider.
  */
 
-const publish = (subject, message) => {
+const publish = (id, subject, message) => {
     error('Not implemented')
 }
 
-const subscribe = (subject) => {
+const subscribe = (id, subject) => {
     error('Not implemented')
 }
 
-const unsubscribe = (subject) => {
+const unsubscribe = (id, subject) => {
     error('Not implemented')
 }
 
