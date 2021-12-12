121d6197fad429678bd38eb7c648e3a53c6ed0b6
Stefan Kostic <skostic9242@gmail.com>
2021-12-12 01:17:23 +0100

    Remove not needed test endpoints

--- examples/battleship/server/server.js
+++ examples/battleship/server/server.js
@@ -8,20 +8,12 @@ const truffle_connect = require('./connection/truffle_connect.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const { hash } = require('./baseline/utils/hash.js')
-
-const KafkaProducer = require('./baseline/messaging/producer.js');
-
-
 const { socketConnection } = require('./baseline/utils/socket')
 socketConnection(server)
-
-const proofVerify = require('./baseline/privacy/proof-verify.js')
 
 const { organizationRouter } = require('./baseline/organization')
 const { workgroupRouter } = require('./baseline/workgroup')
 const { battleshipRouter } = require('./baseline/battleship')
-
 
 const KafkaConsumer = require('./baseline/messaging/consumer.js')
 KafkaConsumer.consume(console.log).then(() => {
@@ -40,40 +32,6 @@ app.use('/', express.static('public_static'));
 app.use('/api/organization', organizationRouter)
 app.use('/api/workgroup', workgroupRouter)
 app.use('/api/battleship', battleshipRouter)
-
-app.get('/accounts', (req, res) => {
-  truffle_connect.start(function (accounts) {
-    res.send(accounts);
-  })
-});
-
-app.get('/deploy', async (req, res) => {
-  await truffle_connect.deploy(function (address) {
-    res.send(address)
-  })
-});
-
-app.post('/proof', async (req, res) => {
-  fullProof = await proofVerify.fullProve(req.body);
-  res.send(fullProof);
-});
-
-app.post('/verifyInputs', async(req, res) => {
-  verifyInputs = await proofVerify.getVerifyProofInputs(req.body.proof, req.body.publicSignals);
-  res.send(verifyInputs);
-});
-
-app.post('/testMsg', async(req, res) => {
-  const producer = new KafkaProducer();
-  await producer.queue(req.body.message)
-  res.send('msg queued')
-})
-
-
-app.post('/hash', async(req, res) => {
-  const boardHash = await hash(req.body);
-  res.send(boardHash);
-})
 
 server.listen(port, async () => {
   truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://ganache-cli:8545"));
