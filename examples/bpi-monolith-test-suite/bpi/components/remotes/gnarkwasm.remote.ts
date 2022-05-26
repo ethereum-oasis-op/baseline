523f21c7e3ba41dda919f5336fbf1985ccde9d95
Kasshern <keith.salzman@protonmail.com>
2022-05-26 07:28:03 -0500

    Removes unused remote

--- examples/bpi-monolith-test-suite/bpi/components/remotes/gnarkwasm.remote.ts
+++
@@ -1,7 +0,0 @@
-import { internalAxios } from './internal.axios';
-import { privateInput } from './privateInput';
-
-export const checkValidity = async (input: privateInput) => {
-    const response = await internalAxios.post('/', input)
-    return response;
-}
--- examples/bpi-monolith-test-suite/bpi/components/remotes/privateInput.ts
+++
@@ -1,4 +0,0 @@
-export interface privateInput {
-    agreementStateCommitment: string,
-    stateObjectCommitment: string,
-}
