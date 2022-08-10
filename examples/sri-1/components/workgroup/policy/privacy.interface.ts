ccc6baebc7517fe45e0ea2c0df6cd6d369991936
Kasshern <keith.salzman@protonmail.com>
2022-08-10 15:37:35 -0500

    Added privacy rules

--- examples/sri-1/components/workgroup/policy/privacy.interface.ts
+++ examples/sri-1/components/workgroup/policy/privacy.interface.ts
@@ -1,8 +1,9 @@
 
 export interface IPrivacy {
-    dataVisibilityRoles: string[];
-    addDataVisibilityRole(role: string, visibilityLevel: string);
-    removeDataVisibilityRole(role: string);
+    id: string;
+    name: string;
+    dataVisibilityRoles: Map<string, string>;
+    addDataVisibilityRole(role: string, roleName: string, visibilityLevel: string);
+    removeDataVisibilityRole(requestorId: string, roleName: string);
     updateDatavisibilityRole(role: string, ...updates: string[]);
-    
 }
--- examples/sri-1/components/workgroup/policy/privacy.ts
+++ examples/sri-1/components/workgroup/policy/privacy.ts
@@ -3,23 +3,50 @@ import { IPrivacy } from "./privacy.interface";
 export class Privacy implements IPrivacy {
     id: string
     name: string;
-    dataVisibilityRoles: string[] = [];
+    dataVisibilityRoles: Map<string, string> = new Map(); //Stores roles and Access in form <Role, Access> ex. <"admin", "CRUD">
+    roleRoster: Map<string, string> = new Map(); //Stores existing users and their role in form <userId, role> ex. <"Alice", "Admin">
     
-    constructor(id: string, name: string, dataVisibilityRoles: string[]) {
+    constructor(id: string, name: string, adminId: string) {
         this.id = id;
         this.name = name;
-        dataVisibilityRoles.forEach(role => {
-            this.dataVisibilityRoles.push(role)
+        Object.keys(Roles).forEach((role, index) => {
+            this.dataVisibilityRoles.set(role, Object.values(Roles)[index])
         });
+        this.roleRoster.set(adminId, "admin");
     }
 
-    addDataVisibilityRole(role: string, visibilityLevel: string) {
-        return null;
+    addDataVisibilityRole(requestorId: string, roleName: string, visibilityLevel: string) {
+        //!Loookup subject role by Id both stored on subject OR lookup subject role per workgroup here.
+        const roleAccess = this.dataVisibilityRoles.get(this.roleRoster.get(requestorId) ?? '');
+        if (roleAccess && roleAccess.charAt(0) == 'C') {
+        return this.dataVisibilityRoles.set(roleName, visibilityLevel);
+        }
+        return 'Access Denied';
     }
-    removeDataVisibilityRole(role: string){
-        return null;
+    removeDataVisibilityRole(requestorId: string, roleName: string){
+        const roleAccess = this.dataVisibilityRoles.get(this.roleRoster.get(requestorId) ?? '');
+        if (roleAccess && roleAccess.charAt(3) == 'D') {
+          return  this.dataVisibilityRoles.delete(roleName);
+        } 
+        return 'Access Denied';
     }
-    updateDatavisibilityRole(role: string, ...updates: string[]){
-        return null;
+    updateDatavisibilityRole(requestorId: string, roleName: string, ...updates: string[]){
+        const roleAccess = this.dataVisibilityRoles.get(this.roleRoster.get(requestorId) ?? '');
+        if (roleAccess && roleAccess.charAt(2) == 'U' ) {
+            return this.dataVisibilityRoles.set(roleName, updates)
+        }
+        return 'Access Denied';
     }
+
+    //TODO implement checks for doing security actions to ones self - check for lowercase 
+    //TODO Change updates to be a single string 
+
+}
+
+//Uppercase: can perform anywhere. Lowercase: can only perform on own items
+enum Roles {
+    operator = 'CRUD',
+    admin = 'CRUD',
+    subject = 'cRud',
+    delegate = 'cRud',
 }
