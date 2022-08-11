23e2a323de6652662f68fb166d2837b5e9ceb9c7
Kasshern <keith.salzman@protonmail.com>
2022-08-11 14:57:31 -0500

    reviewed workflow components, notes and TODOs

--- examples/sri-1/components/workgroup/workflow/workflow.interface.ts
+++ examples/sri-1/components/workgroup/workflow/workflow.interface.ts
@@ -1,19 +1,6 @@
 import { Workstep } from '../workstep/workstep';
 
 export interface IWorkflow {
-    id: string; //[R234] A workflow with more than one workstep MUST have a unique identifier within a BPI.
-    Worksteps: Workstep[]; //Worksteps connected casually by order in workflow? or alternative casual connection [R233] 
+    id: string; 
+    Worksteps: Workstep[]; 
 } 
-
-/**[R232]
-All requirements for a workstep MUST also be applied to a workflow.
-This means that requirements such as determinism, ability to be updated, versioning, characteristics of zero-knowledge proofs, etc. also apply to a workflow.
-
-[R233] 
-If there is more than one workstep in a workflow, the worksteps in a workflow MUST be causally connected.
-This means that the output of a workstep in a workflow is a required input into one or more subsequent worksteps.
-
-[R235]
-A workflow with more than one workstep and a given set of inputs MUST be sequentially executed by a BPI.
-This simply means that for a given set of inputs there is only one process path through a given workflow. 
-*/
--- examples/sri-1/components/workgroup/workflow/workflow.ts
+++ examples/sri-1/components/workgroup/workflow/workflow.ts
@@ -2,8 +2,8 @@ import { IWorkflow } from './workflow.interface';
 import { Workstep } from '../workstep/workstep';
 
 export class Workflow implements IWorkflow {
-  id: string;
-  Worksteps: Workstep[] = []; //Worksteps connected casually by order in workflow? or alternative casual connection [R233]
+  id: string; //[R234] A workflow with more than one workstep MUST have a unique identifier within a BPI.
+  Worksteps: Workstep[] = []; //[R233] If there is more than one workstep in a workflow, the worksteps in a workflow MUST be causally connected.This means that the output of a workstep in a workflow is a required input into one or more subsequent worksteps. //TODO Worksteps connected causally by order in workflow? or alternative causal connection [R233]
 
   constructors(id: string, worksteps: Workstep[]) {
     this.id = id;
@@ -11,15 +11,11 @@ export class Workflow implements IWorkflow {
   };
 }
 
-/**[R232]
+/**[R232] //TODO Implement all applicable workstep requirements here
 All requirements for a workstep MUST also be applied to a workflow.
 This means that requirements such as determinism, ability to be updated, versioning, characteristics of zero-knowledge proofs, etc. also apply to a workflow.
 
-[R233] 
-If there is more than one workstep in a workflow, the worksteps in a workflow MUST be causally connected.
-This means that the output of a workstep in a workflow is a required input into one or more subsequent worksteps.
-
-[R235]
+[R235] //TODO enforce sequential execution of worksteps within workflow (in workgroup)
 A workflow with more than one workstep and a given set of inputs MUST be sequentially executed by a BPI.
 This simply means that for a given set of inputs there is only one process path through a given workflow. 
 */
