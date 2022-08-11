539dec061b25bff9943f733ab47b37613180ebc5
ognjenkurtic <kurticognjen@gmail.com>
2022-08-11 15:03:04 +0200

    ...

--- examples/sri-1/components/workgroup/workgroup/workgroupComponent.interface.ts
+++ examples/sri-1/components/workgroup/workgroup/workgroupComponent.interface.ts
@@ -1,15 +1,15 @@
 import { Workstep } from '../workstep';
 import { Workgroup } from './workgroup';
-import { BpiSubject } from '../../identity/bpiSubject/bpiSubject';
+import { IBpiSubject } from '../../identity/bpiSubject/bpiSubject.interface';
 import { Agreement } from '../storage/agreement';
 import { Invitation } from './invitation';
 import { Organization } from "./organization"
 
 export interface IWorkgroupComponent {
     getWorkgroups(): Workgroup[];
-    createWorkgroup(name: string, id: string, owner: BpiSubject, worksteps: Workstep[]): Workgroup;  //[R236] There MUST be at least one BPI Subject role that has the authorization to create a workgroup. [R237] A workgroup MUST consist of at least one BPI Subject participant.
+    createWorkgroup(name: string, id: string, owner: IBpiSubject, worksteps: Workstep[]): Workgroup;  //[R236] There MUST be at least one BPI Subject role that has the authorization to create a workgroup. [R237] A workgroup MUST consist of at least one BPI Subject participant.
     getWorkgroupById(id: string): Workgroup
-    sendInviteToWorkgroup(id: string, name: string, sender: BpiSubject, recipient: string, workgroupId: string, agreement: Agreement): Invitation;
+    sendInviteToWorkgroup(id: string, name: string, sender: IBpiSubject, recipient: string, workgroupId: string, agreement: Agreement): Invitation;
     getInvitationById(id: string): Invitation;
     getReceivedInvitationsByEmail(email: string): Invitation[];
     acceptWorkgroupInvitation(id: string, name: string, recipient: string, workGroupId: string) : {acceptanceStatus: string};
