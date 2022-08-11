afef083820f635650ff014214d2aa19782431f7f
ognjenkurtic <kurticognjen@gmail.com>
2022-08-11 15:15:53 +0200

    Add BpiSubjectAccount interface and class

--- examples/sri-1/components/identity/bpiSubject/bpiSubject.interface.ts
+++ examples/sri-1/components/identity/bpiSubject/bpiSubject.interface.ts
@@ -1,7 +1,7 @@
 import { BpiSubjectType } from "./bpiSubjectType.enum";
 
 export interface IBpiSubject {
-    id: string;
+    id: string; // TODO: Add uuid after #491
     name: string;
     description: string;
     type: BpiSubjectType;
--- examples/sri-1/components/identity/bpiSubject/bpiSubject.ts
+++ examples/sri-1/components/identity/bpiSubject/bpiSubject.ts
@@ -2,14 +2,13 @@ import { IBpiSubject } from "./bpiSubject.interface";
 import { BpiSubjectType } from "./bpiSubjectType.enum";
 
 export class BpiSubject implements IBpiSubject {
-    id: string;
+    id: string; // TODO: Add uuid after #491
     name: string;
     description: string;
     type: BpiSubjectType;
     publicKey: string;
 
-    constructor(id: string, name: string, description: string, type: BpiSubjectType, publicKey: string) {
-        this.id = id;
+    constructor(name: string, description: string, type: BpiSubjectType, publicKey: string) {
         this.name = name;
         this.description = description;
         this.type = type;
