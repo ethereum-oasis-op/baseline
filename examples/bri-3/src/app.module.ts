5fd65a11abeadb8fa14c601cfb1e4541fe940ad6
ognjenkurtic <kurticognjen@gmail.com>
2022-08-20 21:35:07 +0200

    Add simple create bpi subject command with a handler; Add subject controller to trigger the command

--- examples/bri-3/src/app.module.ts
+++ examples/bri-3/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SubjectsModule } from './bri/subjects/subjects.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SubjectsModule],
 })
 export class AppModule {}
---
+++ examples/bri-3/src/bri/subjects/controllers/subjects.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateBpiSubjectCommand } from '../commands/createBpiSubject.command';
+
+@Controller("subjects")
+export class SubjectController {
+  constructor(private commandBus: CommandBus) {}
+
+  @Get()
+  async getTest(): Promise<boolean> {
+    return await this.commandBus.execute(new CreateBpiSubjectCommand("test1", "test2"));
+  }
+}
