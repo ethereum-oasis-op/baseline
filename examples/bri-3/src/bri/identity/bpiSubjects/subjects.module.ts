8af4bbe143bcace8d2b936bb72b9049b32289a33
ognjenkurtic <kurticognjen@gmail.com>
2022-08-22 15:49:48 +0200

    Rename folder that contains dummy business logic

--- examples/bri-3/src/bri/identity/bpiSubjects/subjects.module.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/subjects.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { SubjectController } from './controllers/subjects.controller';
-import { CreateBpiSubjectCommandHandler } from './businessLogic/createBpiSubject/createBpiSubjectCommand.handler';
+import { CreateBpiSubjectCommandHandler } from './capabilities/createBpiSubject/createBpiSubjectCommand.handler';
 
 export const CommandHandlers = [CreateBpiSubjectCommandHandler];
 
