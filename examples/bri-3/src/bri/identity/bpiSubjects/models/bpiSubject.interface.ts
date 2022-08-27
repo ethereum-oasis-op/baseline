f678f166896380bd78d51e31b570ff5765187bec
ognjenkurtic <kurticognjen@gmail.com>
2022-08-27 13:25:21 +0200

    Add happy case test example; Add TODOs for future work

--- examples/bri-3/src/bri/identity/bpiSubjects/models/bpiSubject.interface.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/models/bpiSubject.interface.ts
@@ -1,5 +1,6 @@
 import { BpiSubjectType } from "./bpiSubjectType.enum";
 
+// TODO: Do we actually need interfaces as we are already working with classes?
 export interface IBpiSubject {
     id: string; // TODO: Add uuid after #491
     name: string;
