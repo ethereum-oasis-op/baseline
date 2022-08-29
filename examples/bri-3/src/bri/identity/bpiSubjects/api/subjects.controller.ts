4c24501b1ea076eaaac1d23dbe46b30142695a47
ognjenkurtic <kurticognjen@gmail.com>
2022-08-29 22:56:14 +0200

    Add basic endpoint for bpi subject update with dummy handler WIP

--- examples/bri-3/src/bri/identity/bpiSubjects/api/subjects.controller.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/api/subjects.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateBpiSubjectCommand } from '../capabilities/createBpiSubject/createBpiSubject.command';
 import { BpiSubject } from '../models/bpiSubject';
 import { CreateBpiSubjectDto } from './dtos/request/createBpiSubject.dto';
+import { UpdateBpiSubjectDto } from './dtos/request/updateBpiSubject.dto';
 
 import { uuid } from 'uuidv4';
 
@@ -22,5 +23,10 @@ export class SubjectController {
             requestDto.publicKey
         )
       );
+  }
+
+  @Put()
+  async UpdateBpiSubject(@Body() requestDto: UpdateBpiSubjectDto): Promise<BpiSubject> {
+    return await this.commandBus.execute(new CreateBpiSubjectCommand(requestDto.name, requestDto.desc, requestDto.publicKey));
   }
 }
