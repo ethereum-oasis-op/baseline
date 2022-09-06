ee81a8c8c0a7a05a73a43395e729eff8c2fc37b6
Kasshern <keith.salzman@protonmail.com>
2022-09-06 15:44:23 -0500

    adds delete and update agent method and exception skeletons

--- examples/bri-3/src/bri/workgroup/worksteps/agents/worksteps.agent.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/agents/worksteps.agent.ts
@@ -1,16 +1,16 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { Workstep } from "../models/workstep";
- 
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Workstep } from '../models/workstep';
+
 @Injectable()
 export class WorkstepAgent {
-  // Agent methods have extremely declarative names and perform a single task 
+  // Agent methods have extremely declarative names and perform a single task
   public throwIfCreateWorkstepInputInvalid(
     name: string,
     id: string,
     workgroupId: string,
   ) {
     // This is just an example, these fields will be validated on the DTO validation layer
-    // This validation would check internal business rules (i.e. bpiSubject must have public key in the format defined by the participants..) 
+    // This validation would check internal business rules (i.e. bpiSubject must have public key in the format defined by the participants..)
     if (!name) {
       // We stop execution in case of critical errors by throwing a simple exception
       throw new BadRequestException('Name cannot be empty.');
@@ -23,5 +23,29 @@ export class WorkstepAgent {
     workgroupId: string,
   ): Workstep {
     return new Workstep(name, id, workgroupId);
+  }
+
+  throwIfDeleteWorkstepInputInvalid(
+    name: string,
+    id: string,
+    workgroupId: string,
+  ) {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteWorkstep(name: string, id: string, workgroupId: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  updateWorkstep(name: string, id: string, workgroupId: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  throwIfUpdateWorkstepInputInvalid(
+    name: string,
+    id: string,
+    workgroupId: string,
+  ) {
+    throw new Error('Method not implemented.');
   }
 }
