c23480faeb5c772e25092b7b0dbbea69e2c58889
Kasshern <keith.salzman@protonmail.com>
2022-09-07 16:27:06 -0500

    formats workflow and workflow interface files

--- examples/bri-3/src/bri/workgroup/workflows/models/workflow.interface.ts
+++ examples/bri-3/src/bri/workgroup/workflows/models/workflow.interface.ts
@@ -1,6 +1,6 @@
 import { Workstep } from '../../worksteps/models/workstep';
 
 export interface IWorkflow {
-    id: string; // TODO: Add uuid after #491
-    worksteps: Workstep[]; 
-} 
+  id: string; // TODO: Add uuid after #491
+  worksteps: Workstep[];
+}
--- examples/bri-3/src/bri/workgroup/workflows/models/workflow.ts
+++ examples/bri-3/src/bri/workgroup/workflows/models/workflow.ts
@@ -5,8 +5,7 @@ export class Workflow implements IWorkflow {
   id: string; // TODO: Add uuid after #491
   worksteps: Workstep[] = []; //TODO enforce workstep causal connection through collection order
 
-  constructors(id: string, worksteps: Workstep[]) {
-    this.id = id;
+  constructor(worksteps: Workstep[]) {
     this.worksteps = worksteps;
-  };
+  }
 }
