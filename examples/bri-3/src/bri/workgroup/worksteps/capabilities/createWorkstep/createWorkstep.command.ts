82e9df38442a44bef8cd044c248f76ea9d80c68e
Kasshern <keith.salzman@protonmail.com>
2022-09-09 13:37:19 -0500

    Alters createWOrkstep command to account for all primitive field in workstep class, and creates getters an setters for those fields

--- examples/bri-3/src/bri/workgroup/worksteps/capabilities/createWorkstep/createWorkstep.command.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/capabilities/createWorkstep/createWorkstep.command.ts
@@ -1,7 +1,49 @@
 export class CreateWorkstepCommand {
+  private name: string;
+  private version: string;
+  private status: string;
+  private workgroupId: string;
   constructor(
-    private name: string,
-    private id: string,
-    private workgroupId: string,
-  ) {}
+    name: string,
+    version: string,
+    status: string,
+    workgroupId: string,
+  ) {
+    this.name = name;
+    this.version = version;
+    this.status = status;
+    this.workgroupId = workgroupId;
+  }
+
+  public get _name() {
+    return this.name;
+  }
+
+  public set _name(name: string) {
+    this.name = name;
+  }
+
+  public get _version() {
+    return this.version;
+  }
+
+  public set _version(version: string) {
+    this.version = version;
+  }
+
+  public get _status() {
+    return this.status;
+  }
+
+  public set _status(status: string) {
+    this.status = status;
+  }
+
+  public get _workgroupId() {
+    return this.workgroupId;
+  }
+
+  public set __workgroupId(workgroupId: string) {
+    this.workgroupId = workgroupId;
+  }
 }
