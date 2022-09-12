c4451b6fd5e6723881b7a5f98b36d2f9df2fa0ec
Kasshern <keith.salzman@protonmail.com>
2022-09-12 16:54:47 -0500

    Adds create workgroup dto

---
+++ examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
@@ -0,0 +1,11 @@
+import { BpiSubject } from "src/bri/identity/bpiSubjects/models/bpiSubject";
+import { Workflow } from "src/bri/workgroup/workflows/models/workflow";
+import { Workstep } from "src/bri/workgroup/worksteps/models/workstep";
+
+export interface CreateBpiSubjectDto {
+    name: string;
+    administrator: BpiSubject[];
+    parcitipants: BpiSubject[];
+    worksteps: Workstep[];
+    workflows: Workflow[];
+}
