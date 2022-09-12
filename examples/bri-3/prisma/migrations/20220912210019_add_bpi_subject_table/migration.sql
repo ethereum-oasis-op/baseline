3ee7c788b3ee0b00e44a82ada12dcf3d3223ba44
ognjenkurtic <kurticognjen@gmail.com>
2022-09-12 23:01:05 +0200

    Add migration for bpisubject; execute migration against sqlite db

---
+++ examples/bri-3/prisma/migrations/20220912210019_add_bpi_subject_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "BpiSubject" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "type" INTEGER NOT NULL,
+    "publicKey" TEXT NOT NULL
+);
