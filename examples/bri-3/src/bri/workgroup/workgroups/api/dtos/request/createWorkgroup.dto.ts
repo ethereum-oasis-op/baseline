8263eb8dc20efb174d041d72b679a244c1ee82e6
Kasshern <keith.salzman@protonmail.com>
2022-09-12 17:13:39 -0500

    Corrects name of workgroup dto

--- examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
+++ examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
@@ -2,7 +2,7 @@ import { BpiSubject } from "src/bri/identity/bpiSubjects/models/bpiSubject";
 import { Workflow } from "src/bri/workgroup/workflows/models/workflow";
 import { Workstep } from "src/bri/workgroup/worksteps/models/workstep";
 
-export interface CreateBpiSubjectDto {
+export interface CreateWorkgroupDto {
     name: string;
     administrator: BpiSubject[];
     parcitipants: BpiSubject[];
