ece11d134fd0eb13e2d6a25de5fc7a6f1c2dcdb7
Kasshern <keith.salzman@protonmail.com>
2022-09-13 10:13:31 -0500

    workgroup response dto created with all properties of workgroup

---
+++ examples/bri-3/src/bri/workgroup/workgroups/api/dtos/response/workgroup.dto.ts
@@ -0,0 +1,16 @@
+import { BpiSubject } from 'src/bri/identity/bpiSubjects/models/bpiSubject';
+import { Privacy } from 'src/bri/policy/models/privacy';
+import { Security } from 'src/bri/policy/models/security';
+import { Workflow } from 'src/bri/workgroup/workflows/models/workflow';
+import { Workstep } from 'src/bri/workgroup/worksteps/models/workstep';
+
+export interface WorkgroupDto {
+  id: string;
+  name: string;
+  administrator: BpiSubject[];
+  parcitipants: BpiSubject[];
+  securityPolicy: Security[];
+  privacyPolicy: Privacy[];
+  worksteps: Workstep[];
+  workflows: Workflow[];
+}
