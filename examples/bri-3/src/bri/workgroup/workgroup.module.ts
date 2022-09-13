23a152aa33701498a92dc67171b758e7f0510e1a
Kasshern <keith.salzman@protonmail.com>
2022-09-13 16:01:38 -0500

    moves workgroupsModule to internal workgroups folder and creates workgroupModule on top level of workgroup folder

--- examples/bri-3/src/bri/workgroup/workgroup.module.ts
+++ examples/bri-3/src/bri/workgroup/workgroup.module.ts
@@ -1,21 +1,7 @@
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
-import { WorkgroupAgent } from './workgroups/agents/workgroups.agent';
-import { WorkgroupController } from './workgroups/api/workgroups.controller';
-import { CreateWorkgroupCommandHandler } from './workgroups/capabilities/createWorkgroup/createWorkgroupCommand.handler';
-import { GetWorkgroupByIdQueryHandler } from './workgroups/capabilities/getWorkgroupById/getWorkgroupByIdQuery.handler';
-import { WorkgroupRepository } from './workgroups/persistence/workgroup.repository';
+import { WorkgroupsModule } from './workgroups/workgroups.module';
 
-export const CommandHandlers = [CreateWorkgroupCommandHandler];
-export const QueryHandlers = [GetWorkgroupByIdQueryHandler];
 @Module({
-  imports: [CqrsModule],
-  controllers: [WorkgroupController],
-  providers: [
-    ...CommandHandlers,
-    ...QueryHandlers,
-    WorkgroupAgent,
-    WorkgroupRepository
-  ]
+  imports: [WorkgroupsModule],
 })
 export class WorkgroupModule {}
---
+++ examples/bri-3/src/bri/workgroup/workgroups/workgroups.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { WorkgroupAgent } from './agents/workgroups.agent';
+import { WorkgroupController } from './api/workgroups.controller';
+import { CreateWorkgroupCommandHandler } from './capabilities/createWorkgroup/createWorkgroupCommand.handler';
+import { GetWorkgroupByIdQueryHandler } from './capabilities/getWorkgroupById/getWorkgroupByIdQuery.handler';
+import { WorkgroupRepository } from './persistence/workgroup.repository';
+
+export const CommandHandlers = [CreateWorkgroupCommandHandler];
+export const QueryHandlers = [GetWorkgroupByIdQueryHandler];
+@Module({
+  imports: [CqrsModule],
+  controllers: [WorkgroupController],
+  providers: [
+    ...CommandHandlers,
+    ...QueryHandlers,
+    WorkgroupAgent,
+    WorkgroupRepository
+  ]
+})
+export class WorkgroupsModule {}
