2bb22a187a905b70e2e0ee8cf0a59edaa9207175
Kasshern <keith.salzman@protonmail.com>
2022-09-13 15:55:58 -0500

    updates workgroup module accordingly  with modules, controllers and agents fedin

--- examples/bri-3/src/bri/workgroup/workgroup.module.ts
+++ examples/bri-3/src/bri/workgroup/workgroup.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { WorkgroupAgent } from './workgroups/agents/workgroups.agent';
+import { WorkgroupController } from './workgroups/api/workgroups.controller';
+import { CreateWorkgroupCommandHandler } from './workgroups/capabilities/createWorkgroup/createWorkgroupCommand.handler';
+import { GetWorkgroupByIdQueryHandler } from './workgroups/capabilities/getWorkgroupById/getWorkgroupByIdQuery.handler';
+import { WorkgroupRepository } from './workgroups/persistence/workgroup.repository';
 
+export const CommandHandlers = [CreateWorkgroupCommandHandler];
+export const QueryHandlers = [GetWorkgroupByIdQueryHandler];
 @Module({
-  imports: []
+  imports: [CqrsModule],
+  controllers: [WorkgroupController],
+  providers: [
+    ...CommandHandlers,
+    ...QueryHandlers,
+    WorkgroupAgent,
+    WorkgroupRepository
+  ]
 })
 export class WorkgroupModule {}
