1e46392b63c09a3e69836967eae5bf4b748f4782
Kasshern <keith.salzman@protonmail.com>
2022-09-13 10:03:40 -0500

    createWorkgroup DTO includes all properties except id

--- examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
+++ examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
@@ -1,4 +1,6 @@
 import { BpiSubject } from "src/bri/identity/bpiSubjects/models/bpiSubject";
+import { Privacy } from "src/bri/policy/models/privacy";
+import { Security } from "src/bri/policy/models/security";
 import { Workflow } from "src/bri/workgroup/workflows/models/workflow";
 import { Workstep } from "src/bri/workgroup/worksteps/models/workstep";
 
@@ -6,6 +8,8 @@ export interface CreateWorkgroupDto {
     name: string;
     administrator: BpiSubject[];
     parcitipants: BpiSubject[];
+    securityPolicy: Security[];
+    privacyPolicy: Privacy[];
     worksteps: Workstep[];
     workflows: Workflow[];
 }
