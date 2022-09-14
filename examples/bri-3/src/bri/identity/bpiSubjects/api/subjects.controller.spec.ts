3c706e8f195b00a452df2e15797de6da8871219e
ognjenkurtic <kurticognjen@gmail.com>
2022-09-14 23:17:02 +0200

    Add endpoint to query all bpi subjects with tests

--- examples/bri-3/src/bri/identity/bpiSubjects/api/subjects.controller.spec.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/api/subjects.controller.spec.ts
@@ -4,6 +4,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BpiSubjectAgent } from '../agents/bpiSubjects.agent';
 import { CreateBpiSubjectCommandHandler } from '../capabilities/createBpiSubject/createBpiSubjectCommand.handler';
 import { DeleteBpiSubjectCommandHandler } from '../capabilities/deleteBpiSubject/updateBpiSubjectCommand.handler';
+import { GetAllBpiSubjectsQuery } from '../capabilities/getAllBpiSubjects/getAllBpiSubjects.query';
+import { GetAllBpiSubjectsQueryHandler } from '../capabilities/getAllBpiSubjects/getAllBpiSubjectsQuery.handler';
 import { GetBpiSubjectByIdQueryHandler } from '../capabilities/getBpiSubjectById/getBpiSubjectByIdQuery.handler';
 import { UpdateBpiSubjectCommandHandler } from '../capabilities/updateBpiSubject/updateBpiSubjectCommand.handler';
 import { BpiSubjectRepository } from '../persistence/bpiSubjects.repository';
@@ -25,6 +27,7 @@ describe('SubjectController', () => {
         UpdateBpiSubjectCommandHandler,
         DeleteBpiSubjectCommandHandler,
         GetBpiSubjectByIdQueryHandler,
+        GetAllBpiSubjectsQueryHandler,
         BpiSubjectRepository
       ],
     })
@@ -35,6 +38,84 @@ describe('SubjectController', () => {
     sController = app.get<SubjectController>(SubjectController);
 
     await app.init();
+  });
+
+  describe('getBpiSubjectById', () => {
+    it('should throw NotFound if non existent id passed', () => {
+      // Arrange
+      const nonExistentId = '123';
+
+      // Act and assert
+      expect(async () => {
+        await sController.getBpiSubjectById(nonExistentId);
+      }).rejects.toThrow(new NotFoundException(`Bpi Subject with id: ${nonExistentId} does not exist.`));
+    });
+
+    it('should return the correct bpi subject if proper id passed ', async () => {
+      // Arrange
+      const requestDto = { name: "name", desc: "desc", publicKey: "publicKey"} as CreateBpiSubjectDto;
+
+      const newBpiSubjectId = await sController.createBpiSubject(requestDto);
+
+      // Act
+      const createdBpiSubject = await sController.getBpiSubjectById(newBpiSubjectId);
+
+      // Assert
+      expect(createdBpiSubject.id).toEqual(newBpiSubjectId);
+      expect(createdBpiSubject.name).toEqual(requestDto.name);
+      expect(createdBpiSubject.desc).toEqual(requestDto.desc);
+      expect(createdBpiSubject.publicKey).toEqual(requestDto.publicKey);
+    });
+  });
+
+  describe('getAllBpiSubjects', () => {
+    it('should return emtpy array if not bpi subjects ', async () => {
+      // Act
+      const bpiSubjects = await sController.getAllBpiSubjects();
+
+      // Assert
+      expect(bpiSubjects.length).toEqual(0);
+    });
+
+    it('should return 1 bpi subject if 1 exists ', async () => {
+      // Arrange
+      const requestDto = { name: "name", desc: "desc", publicKey: "publicKey"} as CreateBpiSubjectDto;
+
+      const newBpiSubjectId = await sController.createBpiSubject(requestDto);
+
+      // Act
+      const bpiSubjects = await sController.getAllBpiSubjects();
+
+      // Assert
+      expect(bpiSubjects.length).toEqual(1);
+      expect(bpiSubjects[0].id).toEqual(newBpiSubjectId);
+      expect(bpiSubjects[0].name).toEqual(requestDto.name);
+      expect(bpiSubjects[0].desc).toEqual(requestDto.desc);
+      expect(bpiSubjects[0].publicKey).toEqual(requestDto.publicKey);
+    });
+
+    it('should return 2 bpi subjects if 2 exists ', async () => {
+      // Arrange
+      const requestDto1 = { name: "name1", desc: "desc1", publicKey: "publicKey1"} as CreateBpiSubjectDto;
+      const newBpiSubjectId1 = await sController.createBpiSubject(requestDto1);
+
+      const requestDto2 = { name: "name2", desc: "desc2", publicKey: "publicKey2"} as CreateBpiSubjectDto;
+      const newBpiSubjectId2 = await sController.createBpiSubject(requestDto2);
+
+      // Act
+      const bpiSubjects = await sController.getAllBpiSubjects();
+
+      // Assert
+      expect(bpiSubjects.length).toEqual(2);
+      expect(bpiSubjects[0].id).toEqual(newBpiSubjectId1);
+      expect(bpiSubjects[0].name).toEqual(requestDto1.name);
+      expect(bpiSubjects[0].desc).toEqual(requestDto1.desc);
+      expect(bpiSubjects[0].publicKey).toEqual(requestDto1.publicKey);
+      expect(bpiSubjects[1].id).toEqual(newBpiSubjectId2);
+      expect(bpiSubjects[1].name).toEqual(requestDto2.name);
+      expect(bpiSubjects[1].desc).toEqual(requestDto2.desc);
+      expect(bpiSubjects[1].publicKey).toEqual(requestDto2.publicKey);
+    });
   });
 
   describe('createBpiSubject', () => {
@@ -112,34 +193,6 @@ describe('SubjectController', () => {
       expect(async () => {
         await sController.getBpiSubjectById(newBpiSubjectId);
       }).rejects.toThrow(new NotFoundException(`Bpi Subject with id: ${newBpiSubjectId} does not exist.`));
-    });
-  });
-
-  describe('getBpiSubjectById', () => {
-    it('should throw NotFound if non existent id passed', () => {
-      // Arrange
-      const nonExistentId = '123';
-
-      // Act and assert
-      expect(async () => {
-        await sController.getBpiSubjectById(nonExistentId);
-      }).rejects.toThrow(new NotFoundException(`Bpi Subject with id: ${nonExistentId} does not exist.`));
-    });
-
-    it('should return the correct bpi subject if proper id passed ', async () => {
-      // Arrange
-      const requestDto = { name: "name", desc: "desc", publicKey: "publicKey"} as CreateBpiSubjectDto;
-
-      const newBpiSubjectId = await sController.createBpiSubject(requestDto);
-
-      // Act
-      const createdBpiSubject = await sController.getBpiSubjectById(newBpiSubjectId);
-
-      // Assert
-      expect(createdBpiSubject.id).toEqual(newBpiSubjectId);
-      expect(createdBpiSubject.name).toEqual(requestDto.name);
-      expect(createdBpiSubject.desc).toEqual(requestDto.desc);
-      expect(createdBpiSubject.publicKey).toEqual(requestDto.publicKey);
     });
   });
 });
