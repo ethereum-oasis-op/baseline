e47392f5b59e6665006ac420aa08fc71983208f0
Kasshern <keith.salzman@protonmail.com>
2022-09-14 10:19:36 -0500

    adapts workstep model to new format

--- examples/bri-3/src/bri/workgroup/worksteps/models/workstep.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/models/workstep.ts
@@ -2,13 +2,13 @@ import { Security } from '../../../policy/models/security';
 import { Privacy } from '../../../policy/models/privacy';
 
 export class Workstep {
-  private id: string; // TODO: Add uuid after #491
-  private name: string;
-  private version: string;
-  private status: string;
-  private workgroupId: string;
-  private securityPolicy: Security; // TODO Implement security policy inhereted from workgroup #487
-  private privacyPolicy: Privacy; // TODO Implement simple privacy policy inhereted from workgroup #487
+  private _id: string; // TODO: Add uuid after #491
+  private _name: string;
+  private _version: string;
+  private _status: string;
+  private _workgroupId: string;
+  private _securityPolicy: Security; // TODO Implement security policy inhereted from workgroup #487
+  private _privacyPolicy: Privacy; // TODO Implement simple privacy policy inhereted from workgroup #487
 
   constructor(
     id: string,
@@ -19,12 +19,40 @@ export class Workstep {
     securityPolicy: Security,
     privacyPolicy: Privacy,
   ) {
-    this.id = id;
-    this.name = name;
-    this.version = version;
-    this.status = status;
-    this.workgroupId = workgroupId;
-    this.securityPolicy = securityPolicy;
-    this.privacyPolicy = privacyPolicy;
+    this._id = id;
+    this._name = name;
+    this._version = version;
+    this._status = status;
+    this._workgroupId = workgroupId;
+    this._securityPolicy = securityPolicy;
+    this._privacyPolicy = privacyPolicy;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get version(): string {
+    return this._version;
+  }
+
+  public get status(): string {
+    return this._status;
+  }
+
+  public get workgroupId(): string {
+    return this._workgroupId;
+  }
+
+  public get securityPolicy(): Security {
+    return this._securityPolicy;
+  }
+
+  public get privacyPolicy(): Privacy {
+    return this._privacyPolicy;
   }
 }
