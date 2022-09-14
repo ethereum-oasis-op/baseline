84236a50a86d94159ad6ec4be8e59416affc99b9
Kasshern <keith.salzman@protonmail.com>
2022-09-14 09:57:45 -0500

    Changes command to single constructor with public readonly properties, and prepared command handler for repo

--- examples/bri-3/src/bri/workgroup/worksteps/capabilities/createWorkstep/createWorkstep.command.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/capabilities/createWorkstep/createWorkstep.command.ts
@@ -1,71 +1,8 @@
-import { Privacy } from 'src/bri/policy/models/privacy';
-import { Security } from 'src/bri/policy/models/security';
-
 export class CreateWorkstepCommand {
-  private name: string;
-  private version: string;
-  private status: string;
-  private workgroupId: string;
-  private securityPolicy: Security;
-  private privacyPolicy: Privacy;
-
   constructor(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-  ) {
-    this.name = name;
-    this.version = version;
-    this.status = status;
-    this.workgroupId = workgroupId;
-  }
-
-  public get _name() {
-    return this.name;
-  }
-
-  public set _name(name: string) {
-    this.name = name;
-  }
-
-  public get _version() {
-    return this.version;
-  }
-
-  public set _version(version: string) {
-    this.version = version;
-  }
-
-  public get _status() {
-    return this.status;
-  }
-
-  public set _status(status: string) {
-    this.status = status;
-  }
-
-  public get _workgroupId() {
-    return this.workgroupId;
-  }
-
-  public set __workgroupId(workgroupId: string) {
-    this.workgroupId = workgroupId;
-  }
-
-  public get _securityPolicy() {
-    return this.securityPolicy;
-  }
-
-  public set _securityPolicy(securityPolicy: Security) {
-    this.securityPolicy = securityPolicy;
-  }
-
-  public get _privacyPolicy() {
-    return this.privacyPolicy;
-  }
-
-  public set _privacyPolicy(privacyPolicy: Privacy) {
-    this.privacyPolicy = privacyPolicy;
-  }
+    public readonly name: string,
+    public readonly version: string,
+    public readonly status: string,
+    public readonly workgroupId: string,
+  ) {}
 }
