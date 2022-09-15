d538bf5edc0e0f2440954da3ea149f4847871fa2
Kasshern <keith.salzman@protonmail.com>
2022-09-15 10:18:02 -0500

    Adds policies to Dtos

--- examples/bri-3/src/bri/workgroup/worksteps/api/dtos/response/workstep.dto.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/api/dtos/response/workstep.dto.ts
@@ -1,7 +1,12 @@
+import { Privacy } from "src/bri/policy/models/privacy";
+import { Security } from "src/bri/policy/models/security";
+
 export interface WorkstepDto {
   id: string;
   name: string;
   version: string;
   status: string;
   workgroupId: string;
+  securityPolicy: Security;
+  privacyPolicy: Privacy;
 }
