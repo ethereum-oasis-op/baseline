63c5bae672580ea00d6cae2376c4623f0fb18744
Kasshern <keith.salzman@protonmail.com>
2022-09-15 11:49:38 -0500

    updates worksteps module

--- examples/bri-3/src/bri/workgroup/worksteps/worksteps.module.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/worksteps.module.ts
@@ -3,12 +3,29 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { WorkstepAgent } from './agents/worksteps.agent';
 import { WorkstepController } from './api/worksteps.controller';
 import { CreateWorkstepCommandHandler } from './capabilities/createWorkstep/createWorkstepCommand.handler';
+import { DeleteWorkstepCommandHandler } from './capabilities/deleteWorkstep/deleteWorkstepCommand.handler';
+import { GetAllWorkstepsQueryHandler } from './capabilities/getAllWorksteps/getAllWorkstepsQuery.handler';
+import { GetWorkstepByIdQueryHandler } from './capabilities/getWorkstepById/getWorkstepByIdQuery.handler';
+import { UpdateWorkstepCommandHandler } from './capabilities/updateWorkstep/updateWorkstep.command.handler';
+import { WorkstepStorageAgent } from './persistence/workstepsStorage.agent';
 
-export const CommandHandlers = [CreateWorkstepCommandHandler];
+export const CommandHandlers = [
+  CreateWorkstepCommandHandler,
+  UpdateWorkstepCommandHandler,
+  DeleteWorkstepCommandHandler];
+
+  export const QueryHandlers = [
+    GetWorkstepByIdQueryHandler,
+    GetAllWorkstepsQueryHandler];
 
 @Module({
   imports: [CqrsModule],
   controllers: [WorkstepController],
-  providers: [...CommandHandlers, WorkstepAgent],
+  providers: [
+    ...CommandHandlers,
+    ...QueryHandlers,
+     WorkstepAgent,
+    WorkstepStorageAgent
+  ]
 })
 export class WorkstepsModule {}
