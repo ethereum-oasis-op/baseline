3c9554bc6efba4ebeff15fd45e319c4d5f225b57
Kasshern <keith.salzman@protonmail.com>
2022-09-15 10:09:06 -0500

    final update to workstepAgent - includes error throw for invalid input, create and update functions and fetch/validate for update and delete

--- examples/bri-3/src/bri/workgroup/worksteps/agents/worksteps.agent.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/agents/worksteps.agent.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Privacy } from 'src/bri/policy/models/privacy';
 import { Security } from 'src/bri/policy/models/security';
 import { Workstep } from '../models/workstep';
@@ -7,69 +7,44 @@ import { uuid } from 'uuidv4';
 
 @Injectable()
 export class WorkstepAgent {
-  public throwIfCreateWorkstepInputInvalid(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-  ) {
+  constructor(private repo: WorkstepStorageAgent) {}
+  public throwIfCreateWorkstepInputInvalid(name: string, version: string, status: string, workgroupId: string) {
+    // This is just an example, these fields will be validated on the DTO validation layer
+    // This validation would check internal business rules
     if (!name) {
       throw new BadRequestException('Name cannot be empty.');
     }
   }
 
-  public createNewWorkstep(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-    securityPolicy: Security,
-    privacyPolicy: Privacy,
-  ): Workstep {
-    return new Workstep(
-      uuid(),
-      name,
-      version,
-      status,
-      workgroupId,
-      securityPolicy,
-      privacyPolicy,
-    );
+  public createNewWorkstep( name: string, version: string, status: string, workgroupId: string, securityPolicy: Security, privacyPolicy: Privacy): Workstep {
+    return new Workstep( uuid(), name, version, status, workgroupId, securityPolicy, privacyPolicy);
   }
 
-  throwIfDeleteWorkstepInputInvalid(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-  ) {
-    throw new Error('Method not implemented.');
+  public async fetchUpdateCandidateAndThrowIfUpdateValidationFails(id:string, name: string, version: string, status: string, workgroupId: string): Promise<Workstep> {
+    const workstepToUpdate = await this.repo.getWorkstepById(id);
+
+    if(!workstepToUpdate) {
+      throw new NotFoundException(`Workstep with id: ${id} does not exist.`)
+    }
+
+    return workstepToUpdate;
   }
 
-  deleteWorkstep(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-  ) {
-    throw new Error('Method not implemented.');
+  public updateWorkstep(workstepToUpdate: Workstep, name :string, version: string, status: string, workgroupId: string) {
+    workstepToUpdate.updateName(name);
+    workstepToUpdate.updateVersion(version);
+    workstepToUpdate.updateStatus(status);
+    workstepToUpdate.updateWorkgroupId(workgroupId);
   }
+  
+  public async fetchDeleteCandidateAndThrowIfDeleteValidationFails(id: string): Promise<Workstep> {
 
-  throwIfUpdateWorkstepInputInvalid(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-  ) {
-    throw new Error('Method not implemented.');
-  }
+    const workstepToDelete = await this.repo.getWorkstepById(id);
 
-  updateWorkstep(
-    name: string,
-    version: string,
-    status: string,
-    workgroupId: string,
-  ) {
-    throw new Error('Method not implemented.');
+    if(!workstepToDelete) {
+      throw new NotFoundException(`Workstep with id: ${id} does not exist.`)
+    }
+
+    return workstepToDelete;
   }
 }
