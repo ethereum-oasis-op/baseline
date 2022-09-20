d304b879bc8ed89b86156e1de1e0fffd292714c1
ognjenkurtic <kurticognjen@gmail.com>
2022-09-20 21:49:19 +0200

    Add validation and unit tests for updateWorkstep dto; Fix typos in test naming;

--- examples/bri-3/src/bri/workgroup/worksteps/api/dtos/request/updateWorkstep.dto.spec.ts
+++ examples/bri-3/src/bri/workgroup/worksteps/api/dtos/request/updateWorkstep.dto.spec.ts
@@ -1,15 +1,15 @@
 import { plainToInstance } from "class-transformer";
 import { validate } from "class-validator";
-import { CreateWorkstepDto } from "./createWorkstep.dto";
+import { UpdateWorkstepDto } from "./updateWorkstep.dto";
 
-describe('CreateWorkstepDto', () => {
+describe('UpdateWorkstepDto', () => {
     it('should return error in case name not provided.', async () => {
         // Arrange
         const dto = { workgroupId: '1'}
-        const createWorkstepDto = plainToInstance(CreateWorkstepDto, dto);
+        const updateWorkstepDto = plainToInstance(UpdateWorkstepDto, dto);
 
         // Act
-        const errors = await validate(createWorkstepDto);
+        const errors = await validate(updateWorkstepDto);
 
         // Assert
         expect(errors.length).toBe(1);
@@ -20,10 +20,10 @@ describe('CreateWorkstepDto', () => {
     it('should return error in case workgroupId not provided.', async () => {
         // Arrange
         const dto = { name: 'test'}
-        const createWorkstepDto = plainToInstance(CreateWorkstepDto, dto);
+        const updateWorkstepDto = plainToInstance(UpdateWorkstepDto, dto);
 
         // Act
-        const errors = await validate(createWorkstepDto);
+        const errors = await validate(updateWorkstepDto);
 
         // Assert
         expect(errors.length).toBe(1);
@@ -34,10 +34,10 @@ describe('CreateWorkstepDto', () => {
     it('should return no error if all required properties provided.', async () => {
         // Arrange
         const dto = { name: 'test', workgroupId: '1' }
-        const createWorkstepDto = plainToInstance(CreateWorkstepDto, dto);
+        const updateWorkstepDto = plainToInstance(UpdateWorkstepDto, dto);
 
         // Act
-        const errors = await validate(createWorkstepDto);
+        const errors = await validate(updateWorkstepDto);
 
         // Assert
         expect(errors.length).toBe(0);
