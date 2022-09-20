f1a77231c2fef44cb98c6002c10e29399160d910
ognjenkurtic <kurticognjen@gmail.com>
2022-09-20 21:28:26 +0200

    Add not emtpy validation to all props of CreateBpiSubjectDto; Add unit tests

--- examples/bri-3/src/bri/identity/bpiSubjects/api/dtos/request/createBpiSubject.dto.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/api/dtos/request/createBpiSubject.dto.ts
@@ -1,10 +1,12 @@
 import { IsNotEmpty } from "class-validator";
 
 export class CreateBpiSubjectDto {
+    @IsNotEmpty()
     name: string;
 
     @IsNotEmpty()
     desc: string;
 
+    @IsNotEmpty()
     publicKey: string;
 }
