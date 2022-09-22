c9c9b29d0fa39cc09523fdc610343922cb2b7a5c
Kasshern <keith.salzman@protonmail.com>
2022-09-22 17:05:04 -0500

    updates workflows controller and spec

--- examples/bri-3/src/bri/workgroup/workflows/api/workflows.controller.spec.ts
+++ examples/bri-3/src/bri/workgroup/workflows/api/workflows.controller.spec.ts
@@ -1,62 +1,173 @@
-import { BadRequestException } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
-import { Privacy } from 'src/bri/policy/models/privacy';
-import { Security } from 'src/bri/policy/models/security';
-import { uuid } from 'uuidv4';
-import { Workstep } from '../../worksteps/models/workstep';
+import { NOT_FOUND_ERR_MESSAGE } from '../../workflows/api/err.messages';
 import { WorkflowAgent } from '../agents/workflows.agent';
 import { CreateWorkflowCommandHandler } from '../capabilities/createWorkflow/createWorkflowCommand.handler';
+import { DeleteWorkflowCommandHandler } from '../capabilities/deleteWorkflow/deleteWorkflowCommand.handler';
+import { GetAllWorkflowsQueryHandler } from '../capabilities/getAllWorkflows/getAllWorkflowsQuery.handler';
+import { GetWorkflowByIdQueryHandler } from '../capabilities/getWorkflowById/getWorkflowByIdQuery.handler';
+import { UpdateWorkflowCommandHandler } from '../capabilities/updateWorkflow/updateWorkflowCommand.handler';
 import { CreateWorkflowDto } from './dtos/request/createWorkflow.dto';
 import { WorkflowController } from './workflows.controller';
+import { validate as uuidValidate, version as uuidVersion } from 'uuid';
 
 describe('WorkflowsController', () => {
   let wController: WorkflowController;
+  const requestDto = {
+    workflows: ['workflow1', 'workflow2', 'workflow3'],
+  } as CreateWorkflowDto;
+
+  const createTestWorkflow = async (): Promise<string> => {
+    const workflowId = await wController.createWorkflow(requestDto);
+    return workflowId;
+  };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [CqrsModule],
       controllers: [WorkflowController],
-      providers: [WorkflowAgent, CreateWorkflowCommandHandler],
-    }).compile();
+      providers: [
+        WorkflowAgent,
+        CreateWorkflowCommandHandler,
+        UpdateWorkflowCommandHandler,
+        DeleteWorkflowCommandHandler,
+        GetWorkflowByIdQueryHandler,
+        GetAllWorkflowsQueryHandler,
+        WorkflowStorageAgent,
+      ],
+    })
+      .overrideProvider(WorkflowStorageAgent)
+      .useValue(new MockWorkflowStorageAgent())
+      .compile();
 
     wController = app.get<WorkflowController>(WorkflowController);
 
     await app.init();
   });
 
-  describe('Bpi Workflow creation', () => {
-    it('should throw BadRequest if worksteps not provided', () => {
+  describe('getWorkflowById', () => {
+    it('should throw NotFound if non existent id passed', () => {
       // Arrange
-      const requestDto = { worksteps: [] } as CreateWorkflowDto;
+      const nonExistentId = '123';
 
       // Act and assert
       expect(async () => {
-        await wController.CreateWorkflow(requestDto);
-      }).rejects.toThrow(new BadRequestException('Worksteps cannot be empty.'));
+        await wController.getWorkflowById(nonExistentId);
+      }).rejects.toThrow(new NotFoundException(NOT_FOUND_ERR_MESSAGE));
     });
 
-    it('should return true if all input params provided', async () => {
+    it('should return the correct workflow if proper id passed ', async () => {
       // Arrange
-      const requestDto = {
-        worksteps: [
-          new Workstep(
-            uuid(),
-            'name',
-            'id',
-            'version',
-            'status',
-            new Security(),
-            new Privacy(),
-          ),
-        ],
-      } as CreateWorkflowDto;
+      const workflowId = await createTestWorkflow();
 
       // Act
-      const response = await wController.CreateWorkflow(requestDto);
+      const createdWorkflow = await wController.getWorkflowById(workflowId);
 
       // Assert
-      expect(response).toEqual(true);
+      expect(createdWorkflow.id).toEqual(workflowId);
+      expect(createdWorkflow.workflows).toEqual(requestDto.workflows);
+    });
+  });
+
+  describe('getAllWorkflows', () => {
+    it('should return empty array if no workflows', async () => {
+      // Act
+      const workflows = await wController.getAllWorkflows();
+
+      // Assert
+      expect(workflows).toHaveLength(0);
+    });
+
+    it('should return 2 workflows if 2 exist', async () => {
+      // Arrange
+      const workflowId = await createTestWorkflow();
+      const requestDto2 = {
+        workflows: ['workflow3', 'workflow2', 'workflow1'],
+      } as CreateWorkflowDto;
+      const newWorkflowId2 = await wController.createWorkflow(requestDto2);
+
+      // Act
+      const workflows = await wController.getAllWorkflows();
+
+      // Assert
+      expect(workflows.length).toEqual(2);
+      expect(workflows[0].id).toEqual(workflowId);
+      expect(workflows[0].worksteps).toEqual(requestDto.worksteps);
+      expect(workflows[1].id).toEqual(newWorkflowId2);
+      expect(workflows[1].worksteps).toEqual(requestDto2.worksteps);
+    });
+  });
+
+  describe('createWorkflow', () => {
+    it('should return new uuid from the created workstep when all necessary params provided', async () => {
+      let workflows = await wController.getAllWorkflows();
+      expect(workflows).toEqual([]);
+
+      // Arrange
+      // Act
+      const workflowId = await createTestWorkflow();
+      workflows = await wController.getAllWorkflows();
+
+      // Assert
+      expect(workflows).toHaveLength(1);
+      expect(uuidValidate(workflowId));
+      expect(uuidVersion(workflowId)).toEqual(4);
+    });
+  });
+
+  describe('updateWorkflow', () => {
+    it('should throw NotFound if non existent id passed', () => {
+      // Arrange
+      const nonExistentId = '123';
+      const requestDto: UpdateWorkstepDto = {
+        workflows: ['workflow3', 'workflow2', 'workflow1'],
+      };
+
+      // Act and assert
+      expect(async () => {
+        await wController.updateWorkflow(nonExistentId, requestDto);
+      }).rejects.toThrow(new NotFoundException(NOT_FOUND_ERR_MESSAGE));
+    });
+
+    it('should perform the update if existing id passed', async () => {
+      // Arrange
+      const workflowId = await createTestWorkflow();
+      const updateRequestDto: UpdateWorkstepDto = {
+        workflows: ['workflow3', 'workflow2', 'workflow1'],
+      };
+
+      // Act
+      await wController.updateWorkflow(workflowId, updateRequestDto);
+
+      // Assert
+      const updatedWorkflow = await wController.getWorkflowById(workflowId);
+      expect(updatedWorkflow.id).toEqual(workflowId);
+      expect(updatedWorkflow.workflow).toEqual(updateRequestDto.workflow);
+    });
+  });
+
+  describe('deleteWorkflow', () => {
+    it('should throw NotFound if non existent id passed', () => {
+      // Arrange
+      const nonExistentId = '123';
+      // Act and assert
+      expect(async () => {
+        await wController.deleteWorkflow(nonExistentId);
+      }).rejects.toThrow(new NotFoundException(NOT_FOUND_ERR_MESSAGE));
+    });
+
+    it('should perform the delete if existing id passed', async () => {
+      // Arrange
+      const workflowId = await createTestWorkflow();
+
+      // Act
+      await wController.deleteWorkflow(workflowId);
+
+      // Assert
+      expect(async () => {
+        await wController.getWorkflowById(workflowId);
+      }).rejects.toThrow(new NotFoundException(NOT_FOUND_ERR_MESSAGE));
     });
   });
 });
--- examples/bri-3/src/bri/workgroup/workflows/api/workflows.controller.ts
+++ examples/bri-3/src/bri/workgroup/workflows/api/workflows.controller.ts
@@ -1,22 +1,56 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateWorkflowCommand } from '../capabilities/createWorkflow/createWorkflow.command';
-import { Workflow } from '../models/workflow';
+import { DeleteWorkflowCommand } from '../capabilities/deleteWorkflow/deleteWorkflow.command';
+import { GetAllWorkflowsQuery } from '../capabilities/getAllWorkflows/getAllWorkflows.query';
+import { GetWorkflowByIdQuery } from '../capabilities/getWorkflowById/getWorkflowById.query';
+import { UpdateWorkflowCommand } from '../capabilities/updateWorkflow/updateWorkflow.command';
 import { CreateWorkflowDto } from './dtos/request/createWorkflow.dto';
 
 @Controller('workflows')
 export class WorkflowController {
-  constructor(private commandBus: CommandBus) {}
+  constructor(private commandBus: CommandBus, private queryBus: QueryBus) {}
 
   // TODO: DTO validation
   // TODO: Response DTOs
   // TODO: DTO -> Command mapping
+  @Get('/:id')
+  async getWorkflowById(@Param('id') id: string): Promise<WorkflowDto> {
+    return await this.queryBus.execute(new GetWorkflowByIdQuery(id));
+  }
+
+  @Get()
+  async getAllWorkflows(): Promise<WorkflowDto[]> {
+    return await this.queryBus.execute(new GetAllWorkflowsQuery());
+  }
+
   @Post()
-  async CreateWorkflow(
-    @Body() requestDto: CreateWorkflowDto,
-  ): Promise<Workflow> {
+  async createWorkflow(@Body() requestDto: CreateWorkflowDto): Promise<string> {
     return await this.commandBus.execute(
       new CreateWorkflowCommand(requestDto.worksteps),
     );
+  }
+
+  @Put('/:id')
+  async updateWorkflow(
+    @Param('id') id: string,
+    @Body() requestDto: UpdateWorkflowDto,
+  ): Promise<void> {
+    return await this.commandBus.execute(
+      new UpdateWorkflowCommand(id, requestDto.worksteps),
+    );
+  }
+
+  @Delete('/:id')
+  async deleteWorkflow(@Param('id') id: string): Promise<void> {
+    return await this.commandBus.execute(new DeleteWorkflowCommand(id));
   }
 }
