c3393f74d8e42ef9ea126bbca5e0f48b9db99f02
Kasshern <keith.salzman@protonmail.com>
2022-09-22 11:53:18 -0500

    Adds getters and setters to workflow model

--- examples/bri-3/src/bri/workgroup/workflows/models/workflow.ts
+++ examples/bri-3/src/bri/workgroup/workflows/models/workflow.ts
@@ -1,10 +1,23 @@
 import { Workstep } from '../../worksteps/models/workstep';
 
 export class Workflow {
-  private id: string; // TODO: Add uuid after #491
-  private worksteps: Workstep[]; //TODO enforce workstep causal connection through collection order
+  private _id: string; // TODO: Add uuid after #491
+  private _worksteps: Workstep[]; //TODO enforce workstep causal connection through collection order
 
-  constructor(worksteps: Workstep[]) {
-    this.worksteps = worksteps;
+  constructor(id: string, worksteps: Workstep[]) {
+    this._id = id;
+    this._worksteps = worksteps;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get worksteps(): Workstep[] {
+    return this._worksteps;
+  }
+
+  public updateWorksteps(newWorksteps: Workstep[]): void {
+    this._worksteps = newWorksteps;
   }
 }
