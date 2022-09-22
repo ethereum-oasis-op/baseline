af03a3c39dff796990a509b31da67824acc943bf
Kasshern <keith.salzman@protonmail.com>
2022-09-22 11:53:56 -0500

    adds all expected query and command handlers to workflow module

--- examples/bri-3/src/bri/workgroup/workflows/workflows.module.ts
+++ examples/bri-3/src/bri/workgroup/workflows/workflows.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { WorkstepStorageAgent } from '../worksteps/agents/workstepsStorage.agent';
 import { WorkflowAgent } from './agents/workflows.agent';
 import { WorkflowController } from './api/workflows.controller';
 import { CreateWorkflowCommandHandler } from './capabilities/createWorkflow/createWorkflowCommand.handler';
+import { DeleteWorkflowCommandHandler } from './capabilities/deleteWorkflow/deleteWorkflowCommand.handler';
+import { UpdateWorkflowCommandHandler } from './capabilities/updateWorkflow/updateWorkflowCommand.handler';
 
-export const CommandHandlers = [CreateWorkflowCommandHandler];
+export const CommandHandlers = [
+  CreateWorkflowCommandHandler,
+  UpdateWorkflowCommandHandler,
+  DeleteWorkflowCommandHandler,
+];
+
+export const QueryHandlers = [
+  GetWorkflowByIdQueryHandler,
+  GetAllWorkflowsQueryHandler,
+];
 
 @Module({
   imports: [CqrsModule],
   controllers: [WorkflowController],
-  providers: [...CommandHandlers, WorkflowAgent],
+  providers: [
+    ...CommandHandlers,
+    ...QueryHandlers,
+    WorkflowAgent,
+    WorkstepStorageAgent,
+  ],
 })
 export class WorkflowsModule {}
