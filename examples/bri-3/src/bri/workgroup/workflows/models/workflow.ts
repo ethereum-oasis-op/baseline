f628ba5d7f679082e87c4d3847b371197038545b
Kasshern <keith.salzman@protonmail.com>
2022-09-26 20:31:58 -0500

    adds name and workgroupId property to workflow class

--- examples/bri-3/src/bri/workgroup/workflows/models/workflow.ts
+++ examples/bri-3/src/bri/workgroup/workflows/models/workflow.ts
@@ -4,7 +4,6 @@ export class Workflow {
   private _id: string; // TODO: Add uuid after #491
   private _name: string;
   private _worksteps: Workstep[]; //TODO enforce workstep causal connection through collection order]
-
   private _workgroupId: string;
 
   constructor(
@@ -35,7 +34,15 @@ export class Workflow {
     return this._workgroupId;
   }
 
+  public updateName(newName: string): void {
+    this._name = newName;
+  }
+
   public updateWorksteps(newWorksteps: Workstep[]): void {
     this._worksteps = newWorksteps;
+  }
+
+  public updateWorkgroupId(newWorkgroupId: string): void {
+    this._workgroupId = newWorkgroupId;
   }
 }
