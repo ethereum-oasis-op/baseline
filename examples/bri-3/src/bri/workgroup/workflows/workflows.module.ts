0ef78b93ec0724750e3eac08d936b0844d4d726b
Kasshern <keith.salzman@protonmail.com>
2022-09-26 20:37:04 -0500

    adds missimg import

--- examples/bri-3/src/bri/workgroup/workflows/workflows.module.ts
+++ examples/bri-3/src/bri/workgroup/workflows/workflows.module.ts
@@ -5,6 +5,8 @@ import { WorkflowAgent } from './agents/workflows.agent';
 import { WorkflowController } from './api/workflows.controller';
 import { CreateWorkflowCommandHandler } from './capabilities/createWorkflow/createWorkflowCommand.handler';
 import { DeleteWorkflowCommandHandler } from './capabilities/deleteWorkflow/deleteWorkflowCommand.handler';
+import { GetAllWorkflowsQueryHandler } from './capabilities/getAllWorkflows/getAllWorkflowsQuery.handler';
+import { GetWorkflowByIdQueryHandler } from './capabilities/getWorkflowById/getWorkflowByIdQuery.handler';
 import { UpdateWorkflowCommandHandler } from './capabilities/updateWorkflow/updateWorkflowCommand.handler';
 
 export const CommandHandlers = [
