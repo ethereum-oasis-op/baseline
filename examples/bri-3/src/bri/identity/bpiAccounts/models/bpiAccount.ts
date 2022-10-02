da4ae45cf8163131bf20e4b5627f9683dce4eb45
Stefan Kostic <skostic9242@gmail.com>
2022-10-02 20:47:12 +0200

    Add getters to bpi account

--- examples/bri-3/src/bri/identity/bpiAccounts/models/bpiAccount.ts
+++ examples/bri-3/src/bri/identity/bpiAccounts/models/bpiAccount.ts
@@ -1,11 +1,27 @@
 export class BpiAccount {
-  private id: string; // TODO: Add uuid after #491
-  private nonce: string;
-  private ownerBpiSubjectIds: string[];
+  private _id: string; // TODO: Add uuid after #491
+  private _nonce: string;
+  private _ownerBpiSubjectIds: string[];
 
   constructor(id: string, nonce: string, ownerBpiSubjectIds: string[]) {
-    this.id = id;
-    this.nonce = nonce;
-    this.ownerBpiSubjectIds = ownerBpiSubjectIds;
+    this._id = id;
+    this._nonce = nonce;
+    this._ownerBpiSubjectIds = ownerBpiSubjectIds;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get nonce(): string {
+    return this._nonce;
+  }
+
+  public get ownerBpiSubjectIds(): string[] {
+    return this._ownerBpiSubjectIds;
+  }
+
+  public updateNonce(newNonce: string): void {
+    this._nonce = newNonce;
   }
 }
