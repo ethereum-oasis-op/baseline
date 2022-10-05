258de92a6a9390c2bc3f942cc7ff3bf2855da20f
ognjenkurtic <kurticognjen@gmail.com>
2022-10-05 15:27:08 +0200

    Add updateTransactionDto validation unit tests

---
+++ examples/bri-3/src/bri/transactions/api/dtos/request/updateTransaction.dto.spec.ts
@@ -0,0 +1,49 @@
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { UpdateTransactionDto } from './updateTransaction.dto';
+
+describe('UpdateTransactionDto', () => {
+  it('should return error in case payload not provided.', async () => {
+    // Arrange
+    const dto = { signature: '2323' };
+    const updateTransactionDto = plainToInstance(UpdateTransactionDto, dto);
+
+    // Act
+    const errors = await validate(updateTransactionDto);
+
+    // Assert
+    expect(errors.length).toBe(1);
+    expect(errors[0].property).toEqual('payload');
+    expect(errors[0].constraints.isNotEmpty).toContain(
+      'payload should not be empty',
+    );
+  });
+
+  it('should return error in case signature not provided.', async () => {
+    // Arrange
+    const dto = { payload: 'this is a description' };
+    const updateTransactionDto = plainToInstance(UpdateTransactionDto, dto);
+
+    // Act
+    const errors = await validate(updateTransactionDto);
+
+    // Assert
+    expect(errors.length).toBe(1);
+    expect(errors[0].property).toEqual('signature');
+    expect(errors[0].constraints.isNotEmpty).toContain(
+      'signature should not be empty',
+    );
+  });
+
+  it('should return no error if all required properties provided.', async () => {
+    // Arrange
+    const dto = { payload: 'this is a description', signature: '2323' };
+    const updateTransactionDto = plainToInstance(UpdateTransactionDto, dto);
+
+    // Act
+    const errors = await validate(updateTransactionDto);
+
+    // Assert
+    expect(errors.length).toBe(0);
+  });
+});
