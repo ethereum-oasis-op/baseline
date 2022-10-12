086035bbe403faa52ce9cb2859d6b038fdd9b577
Stefan Kostic <skostic9242@gmail.com>
2022-10-12 20:10:58 +0200

    Fix private bpi subject account fields

--- examples/bri-3/src/bri/identity/bpiSubjectAccounts/models/bpiSubjectAccount.ts
+++ examples/bri-3/src/bri/identity/bpiSubjectAccounts/models/bpiSubjectAccount.ts
@@ -1,9 +1,9 @@
 import { BpiSubject } from '../../bpiSubjects/models/bpiSubject';
 
 export class BpiSubjectAccount {
-  private id: string; // TODO: Add uuid after #491
-  private creatorBpiSubject: BpiSubject;
-  private ownerBpiSubject: BpiSubject;
+  private _id: string; // TODO: Add uuid after #491
+  private _creatorBpiSubject: BpiSubject;
+  private _ownerBpiSubject: BpiSubject;
   private bpiAccounts: string[]; // TODO: Add as part of #489
 
   constructor(
@@ -11,8 +11,20 @@ export class BpiSubjectAccount {
     creatorBpiSubject: BpiSubject,
     ownerBpiSubject: BpiSubject,
   ) {
-    this.id = id;
-    this.creatorBpiSubject = creatorBpiSubject;
-    this.ownerBpiSubject = ownerBpiSubject;
+    this._id = id;
+    this._creatorBpiSubject = creatorBpiSubject;
+    this._ownerBpiSubject = ownerBpiSubject;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get creatorBpiSubjectId(): string {
+    return this._creatorBpiSubject.id;
+  }
+
+  public get ownerBpiSubjectId(): string {
+    return this._ownerBpiSubject.id;
   }
 }
