0a20a402156796a061e72cb556a8835f46106788
Kasshern <keith.salzman@protonmail.com>
2022-10-19 13:27:08 -0500

    updating schema and workgroupdto for testing purposes

--- examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
+++ examples/bri-3/src/bri/workgroup/workgroups/api/dtos/request/createWorkgroup.dto.ts
@@ -4,12 +4,8 @@ export class CreateWorkgroupDto {
   @IsNotEmpty()
   name: string;
 
-  @IsNotEmpty()
-  @ArrayNotEmpty()
   administratorIds: string[];
 
-  @IsNotEmpty()
-  @ArrayNotEmpty()
   parcitipantIds: string[];
 
   @IsNotEmpty()
@@ -18,11 +14,7 @@ export class CreateWorkgroupDto {
   @IsNotEmpty()
   privacyPolicy: string;
 
-  @IsNotEmpty()
-  @ArrayNotEmpty()
   workstepIds: string[];
 
-  @IsNotEmpty()
-  @ArrayNotEmpty()
   workflowIds: string[];
 }
