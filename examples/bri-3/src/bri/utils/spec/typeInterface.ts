b747211ce697321a712f821b6e4c9afea03aa31e
Stefan Kostic <skostic9242@gmail.com>
2022-10-29 15:11:35 +0200

    Remove old mapper

--- examples/bri-3/src/bri/utils/spec/typeInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface TypeInterface {
-  id: string;
-  name: string;
-  description: string;
-  publicKey: string;
-}
--- examples/bri-3/src/bri/utils/types/options.ts
+++
@@ -1,4 +0,0 @@
-export default interface Options {
-  opts?: any;
-  exclude?: any;
-}
