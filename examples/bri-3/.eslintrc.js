2367099fe113baadbdcc561f0f5359156fb74f4f
Manik-Jain <jain.manik@yahoo.co.in>
2022-11-11 18:42:53 -0500

    fixing eslint

--- examples/bri-3/.eslintrc.js
+++ examples/bri-3/.eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'react-app',
   ],
   root: true,
   env: {
