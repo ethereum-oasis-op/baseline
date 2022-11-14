bd67c7f79b5321165927b80ed43ae6fda5306f01
biscuitdey <biswashreedey@gmail.com>
2022-11-14 22:43:36 +0530

    Descriptive error message

--- examples/bri-3/src/bri/zeroKnowledgeProof/api/err.messages.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/api/err.messages.ts
@@ -1,3 +1,4 @@
 export const INVALID_ANCHOR_HASH_INPUT =
   'Input document provided for anchor hash does not exist or is blank';
-export const NOT_FOUND_ERR_MESSAGE = 'Anchor Hash does not exist.';
+export const ANCHOR_HASH_NOT_FOUND_ERR =
+  'Anchor Hash does not exist on the CCSM.';
