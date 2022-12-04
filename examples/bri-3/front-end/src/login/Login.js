cbb0dd08197b0397bf903de34ecf1e6c7b8b3c9a
Stefan Kostic <skostic9242@gmail.com>
2022-12-04 22:42:54 +0100

    Rename and todos in login component

--- examples/bri-3/front-end/src/login/Login.js
+++ examples/bri-3/front-end/src/login/Login.js
@@ -2,25 +2,29 @@ import axios from 'axios';
 import { SRI_BACKEND } from '../constants';
 
 const handleLogin = async () => {
+  // TODO: handle situations like account switch in metamask
   if (window.ethereum) {
     window.ethereum.enable();
     const accounts = await window.ethereum.send('eth_requestAccounts');
-    const mnemonic = await axios.post(`${SRI_BACKEND}/auth/nonce`, {
-      publicKey: accounts.result[0],
-    });
+    const nonceDto = {
+      publicKey: accounts.result[0]
+    };
+    const nonce = await axios.post(`${SRI_BACKEND}/auth/nonce`, nonceDto);
     const signature = await window.ethereum.request({
       method: 'personal_sign',
-      params: [mnemonic.data, accounts.result[0]],
+      params: [nonce.data, accounts.result[0]],
     });
 
-    const input = {
-      message: mnemonic.data,
+    const loginDto = {
+      message: nonce.data,
       signature,
       publicKey: accounts.result[0],
     };
 
-    const tokenRequest = await axios.post(`${SRI_BACKEND}/auth/login`, input);
+    const tokenRequest = await axios.post(`${SRI_BACKEND}/auth/login`, loginDto);
 
+    // TODO: add toast library and show success/failure login with error messages
+    // TODO: add logged in user details on UI
     if (tokenRequest?.data?.access_token) {
       console.log(tokenRequest.data.access_token);
     } else {
