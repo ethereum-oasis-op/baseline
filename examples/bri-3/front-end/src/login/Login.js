353d15c56a9d4b233bcbaae4e29affd3d4dfcfe1
Stefan Kostic <skostic9242@gmail.com>
2022-12-04 22:52:42 +0100

    Use env variables instead of constants

--- examples/bri-3/front-end/src/login/Login.js
+++ examples/bri-3/front-end/src/login/Login.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import { SRI_BACKEND } from '../constants';
 
 const handleLogin = async () => {
   // TODO: handle situations like account switch in metamask
   if (window.ethereum) {
+    console.log('env ', process.env.REACT_APP_SRI_BACKEND);
     window.ethereum.enable();
     const accounts = await window.ethereum.send('eth_requestAccounts');
     const nonceDto = {
       publicKey: accounts.result[0]
     };
-    const nonce = await axios.post(`${SRI_BACKEND}/auth/nonce`, nonceDto);
+    const nonce = await axios.post(`${process.env.REACT_APP_SRI_BACKEND}/auth/nonce`, nonceDto);
     const signature = await window.ethereum.request({
       method: 'personal_sign',
       params: [nonce.data, accounts.result[0]],
@@ -21,7 +21,7 @@ const handleLogin = async () => {
       publicKey: accounts.result[0],
     };
 
-    const tokenRequest = await axios.post(`${SRI_BACKEND}/auth/login`, loginDto);
+    const tokenRequest = await axios.post(`${process.env.REACT_APP_SRI_BACKEND}/auth/login`, loginDto);
 
     // TODO: add toast library and show success/failure login with error messages
     // TODO: add logged in user details on UI
