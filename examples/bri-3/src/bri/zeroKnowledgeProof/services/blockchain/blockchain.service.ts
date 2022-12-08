5715c4c457b13de51f6b4038433c6f18637b5cc6
biscuitdey <biswashreedey@gmail.com>
2022-12-08 18:58:17 +0530

    Renamed anchorHash to ccsmAnchorHash

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/blockchain/blockchain.service.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/blockchain/blockchain.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BlockchainService {
-  async write(anchorHash: string) {
+  async write(ccsmAnchorHash: string) {
     //TODO: store content-addressable hash on contract
   }
 
