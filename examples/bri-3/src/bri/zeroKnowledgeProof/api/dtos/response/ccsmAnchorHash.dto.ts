7a2d48a8c6c43484ab2a325cf0a4cd09b93f54db
biscuitdey <biswashreedey@gmail.com>
2023-01-05 20:24:53 +0530

    Added automapper to CCSMAnchorHash Dto

--- examples/bri-3/src/bri/zeroKnowledgeProof/api/dtos/response/ccsmAnchorHash.dto.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/api/dtos/response/ccsmAnchorHash.dto.ts
@@ -1,6 +1,15 @@
+import { AutoMap } from '@automapper/classes';
+
 export class CCSMAnchorHashDto {
+  @AutoMap()
   id: string;
+
+  @AutoMap()
   ownerBpiSubjectId: string;
+
+  @AutoMap()
   hash: string;
+
+  @AutoMap()
   documentId: string;
 }
--- examples/bri-3/src/bri/zeroKnowledgeProof/api/dtos/response/document.dto.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/api/dtos/response/document.dto.ts
@@ -1,7 +1,13 @@
+import { AutoMap } from '@automapper/classes';
 import { CCSMAnchorHash } from '../../../models/ccsmAnchorHash';
 
 export class DocumentDto {
+  @AutoMap()
   id: number;
+
+  @AutoMap()
   text: string;
+
+  @AutoMap()
   contentAddressableHash?: CCSMAnchorHash;
 }
