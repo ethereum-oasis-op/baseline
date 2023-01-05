70a1cc7860d3bbd9f42c4b5689e2d9ce398feef5
biscuitdey <biswashreedey@gmail.com>
2023-01-05 18:19:56 +0530

    Removed unused folder under zkp component

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/contentAddressableHash/contentAddressableHash.service.ts
+++
@@ -1,9 +0,0 @@
-export class ContentAddressableHash {
-  //Create hash and store it againt document
-  async createContentAddressableHash(document: string) {}
-
-  async storeContentAddressableHash(hash: string) {}
-
-  async getDocumentFromContentAddressableHash(hash: string) {}
-  //Retrieve document for the hash
-}
