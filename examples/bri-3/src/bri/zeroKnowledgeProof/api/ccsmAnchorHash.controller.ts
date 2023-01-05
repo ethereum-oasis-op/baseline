2fe62b4ff201f0d1fb88ca5b0e3cfd331dcd8836
biscuitdey <biswashreedey@gmail.com>
2023-01-05 18:58:35 +0530

    Added GET API for CCSMAnchorHash document

--- examples/bri-3/src/bri/zeroKnowledgeProof/api/ccsmAnchorHash.controller.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/api/ccsmAnchorHash.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateCCSMAnchorHashCommand } from '../capabilities/createCCSMAnchorHash/createCCSMAnchorHash.command';
 import { VerifyCCSMAnchorHashCommand } from '../capabilities/verifyCCSMAnchorHash/verifyCCSMAnchorHash.command';
 import { CreateCCSMAnchorHashDto } from './dtos/request/createCCSMAnchorHash.dto';
 import { VerifyCCSMAnchorHashDto } from './dtos/request/verifyCCSMAnchorHash.dto';
 import { CCSMAnchorHashDto } from './dtos/response/ccsmAnchorHash.dto';
+import { DocumentDto } from './dtos/response/document.dto';
 
 @Controller('ccsmAnchorHash')
 export class CCSMAnchorHashController {
-  constructor(private commandBus: CommandBus) {}
+  constructor(private commandBus: CommandBus, private queryBus: QueryBus) {}
+
+  @Get('document/:hash')
+  async getDocumentByCCSMAnchorHash(
+    @Param('hash') hash: string,
+  ): Promise<DocumentDto> {
+    return await this.queryBus.execute(
+      new GetDocumentByCCSMAnchorHashQuery(hash),
+    );
+  }
 
   @Post('/create')
   async createCCSMAnchorHash(
