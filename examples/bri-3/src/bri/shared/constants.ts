eff3e2aad515bc7254026a2e58af8b22bdcc00cb
Kasshern <keith.salzman@protonmail.com>
2023-01-25 18:01:54 +0000

    Adds check for undefined bearer token, removes bpiSubject agents ability to retrieve multiple subjects, and adds token not found constant in shared folder

--- examples/bri-3/src/bri/shared/constants.ts
+++ examples/bri-3/src/bri/shared/constants.ts
@@ -2,3 +2,5 @@
 export const SHOULD_NOT_BE_EMPTY_VALIDATION_MESSAGE = 'should not be empty';
 export const SHOULD_BE_OF_TYPE_NUMBER =
   'must be a number conforming to the specified constraints';
+
+export const TOKEN_NOT_FOUND = 'access token not found';
