3ce1c52ee7f68b8e6769e7b682e2fa8edfacc2c3
Stefan Kostic <skostic9242@gmail.com>
2023-01-30 01:05:26 +0100

    Add simple ability factory

--- examples/bri-3/src/bri/ability/ability.factory.ts
+++ examples/bri-3/src/bri/ability/ability.factory.ts
@@ -1,21 +1,54 @@
-import { PureAbility, InferSubjects, AbilityBuilder } from '@casl/ability';
-import { Injectable } from '@nestjs/common';
-import { BpiSubject } from '../identity/bpiSubjects/models/bpiSubject';
-import { BpiSubjectType } from '../identity/bpiSubjects/models/bpiSubjectType.enum';
+import {
+  Ability,
+  ForcedSubject,
+  AbilityClass,
+  AbilityBuilder,
+} from '@casl/ability';
+import {
+  BpiSubject,
+  BpiSubjectRoleName,
+} from '../identity/bpiSubjects/models/bpiSubject';
 
-export enum Action {
-  Manage = 'manage',
-  Create = 'create',
-  Read = 'read',
-  Update = 'update',
-  Delete = 'delete',
-}
+const actions = ['manage', 'read'] as const;
+const subjects = ['BpiSubject', 'all'] as const;
+type AppAbilities = [
+  typeof actions[number],
+  (
+    | typeof subjects[number]
+    | ForcedSubject<Exclude<typeof subjects[number], 'all'>>
+  ),
+];
+export type AppAbility = Ability<AppAbilities>;
+export const AppAbility = Ability as AbilityClass<AppAbility>;
 
-export type Subjects = InferSubjects<typeof BpiSubject | 'all'>;
-export type AppAbility = PureAbility<[Action, Subjects]>;
-@Injectable()
-export class AbilityFactory {
-  defineAbility(bpiSubject: BpiSubject) {
-    const builder = new AbilityBuilder(PureAbility);
+type DefinePermissions = (
+  user: BpiSubject,
+  builder: AbilityBuilder<AppAbility>,
+) => void;
+type Roles = 'internal' | 'external';
+
+const rolePermissions: Record<Roles, DefinePermissions> = {
+  external(bpiSubject, { can }) {
+    can('read', 'BpiSubject');
+  },
+  internal(bpiSubject, { can }) {
+    can('manage', 'all');
+  },
+};
+
+export function defineAbilityFor(bpiSubject: BpiSubject): AppAbility {
+  const builder = new AbilityBuilder(Ability as AbilityClass<AppAbility>);
+
+  const role = bpiSubject.roles.find(
+    (r) => r.name === BpiSubjectRoleName.INTERNAL_BPI_SUBJECT,
+  )
+    ? 'internal'
+    : 'external';
+  if (typeof rolePermissions[role] === 'function') {
+    rolePermissions[role](bpiSubject, builder);
+  } else {
+    throw new Error(`Trying to use unknown role "${role}"`);
   }
+
+  return builder.build();
 }
