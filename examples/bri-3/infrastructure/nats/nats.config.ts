4256bae76e5882f11627d4e67e7b68cda7e2ef34
ognjenkurtic <kurticognjen@gmail.com>
2023-02-03 23:42:45 +0100

    Ditch nats config

--- examples/bri-3/infrastructure/nats/nats.config.ts
+++
@@ -1,8 +0,0 @@
-import { NatsOptions, Transport } from '@nestjs/microservices';
-
-export const natsConfig: NatsOptions = {
-  transport: Transport.NATS,
-  options: {
-    url: process.env.NATS_URL || 'nats://localhost:4222',
-  },
-};
