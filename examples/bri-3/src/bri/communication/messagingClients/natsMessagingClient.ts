1b8b57b3f4576e06fd3c5e8b52e19fc5ad6ef8f4
ognjenkurtic <kurticognjen@gmail.com>
2023-02-06 14:33:01 +0100

    Fix formatting

--- examples/bri-3/src/bri/communication/messagingClients/natsMessagingClient.ts
+++ examples/bri-3/src/bri/communication/messagingClients/natsMessagingClient.ts
@@ -4,17 +4,21 @@ import { LoggingService } from '../../../shared/logging/logging.service';
 import { IMessagingClient } from './messagingClient.interface';
 
 @Injectable()
-export class NatsMessagingClient implements IMessagingClient, OnModuleInit, OnModuleDestroy {
+export class NatsMessagingClient
+  implements IMessagingClient, OnModuleInit, OnModuleDestroy
+{
   private nc: NatsConnection;
 
-  constructor(private readonly log: LoggingService) { }
-    
+  constructor(private readonly log: LoggingService) {}
+
   async onModuleInit() {
     try {
       this.nc = await connect({ servers: process.env.BPI_NATS_SERVER_URL });
       this.log.logInfo('Connected to nats server: ' + this.nc.getServer());
     } catch (err) {
-      this.log.logWarn(`Error: ${err} connecting to nats server at: ${process.env.BPI_NATS_SERVER_URL}`);
+      this.log.logWarn(
+        `Error: ${err} connecting to nats server at: ${process.env.BPI_NATS_SERVER_URL}`,
+      );
     }
   }
 
@@ -22,15 +26,15 @@ export class NatsMessagingClient implements IMessagingClient, OnModuleInit, OnMo
     if (this.nc && !this.nc.isClosed()) {
       await this.nc.drain();
     }
-  } 
-  
+  }
+
   async subscribe(
     channelName: string,
     callback: (message: string) => void,
   ): Promise<void> {
     if (!this.nc || this.nc.isClosed()) {
-        this.log.logWarn(`Trying to subscribe without an open nats connection`);
-        return;
+      this.log.logWarn(`Trying to subscribe without an open nats connection`);
+      return;
     }
 
     const sc = StringCodec();
