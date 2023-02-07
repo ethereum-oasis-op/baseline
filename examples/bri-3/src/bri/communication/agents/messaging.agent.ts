e6a325f7dfa877a7ecf20f7a7ce3486f2576accd
ognjenkurtic <kurticognjen@gmail.com>
2023-02-07 17:12:26 +0100

    Introduce basic validation of incoming nats messages for Bpi Message format

--- examples/bri-3/src/bri/communication/agents/messaging.agent.ts
+++ examples/bri-3/src/bri/communication/agents/messaging.agent.ts
@@ -1,8 +1,11 @@
 import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import { isUuid } from 'uuidv4';
 import { LoggingService } from '../../../shared/logging/logging.service';
-import { ProcessInboundMessageCommand } from '../capabilities/processInboundMessage/processInboundMessage.command';
+import { ProcessInboundBpiMessageCommand } from '../capabilities/processInboundMessage/processInboundMessage.command';
 import { IMessagingClient } from '../messagingClients/messagingClient.interface';
+import { BpiMessage } from '../models/bpiMessage';
+import { BpiMessageType } from '../models/bpiMessageType.enum';
 
 @Injectable()
 export class MessagingAgent implements OnApplicationBootstrap {
@@ -26,8 +29,68 @@ export class MessagingAgent implements OnApplicationBootstrap {
 
   private onNewMessageReceived(rawMessage: string): void {
     this.logger.logInfo(
-      `MessagingListenerAgent: New raw message received: ${rawMessage}. Dispatching ProcessInboundMessageCommand`,
+      `MessagingListenerAgent: New raw message received: ${rawMessage}. Trying to parse into a Bpi Message`,
     );
-    this.commandBus.execute(new ProcessInboundMessageCommand(rawMessage));
+
+    const [newBpiMessageCandidate, validationErrors] = this.validateBpiMessageFormat(
+      rawMessage,
+    );
+
+    if (validationErrors.length > 0) {
+      this.logger.logWarn(
+        `MessagingListenerAgent: Failed parsing message into Bpi Message. Errors: ${validationErrors.join(
+          '; ',
+        )}`,
+      );
+
+      return;
+    }
+
+    this.commandBus.execute(
+      new ProcessInboundBpiMessageCommand(newBpiMessageCandidate),
+    );
+  }
+
+  private validateBpiMessageFormat(rawMessage: string): [BpiMessage, string[]] {
+    const errors: string[] = [];
+    let newBpiMessageCandidate: BpiMessage;
+
+    try {
+      newBpiMessageCandidate = JSON.parse(rawMessage) // TODO: Make it more robust (i.e JSON.parse(123) does not throw)
+    } catch (e) {
+      errors.push(`${rawMessage} is not valid JSON. Error: ${e}`);
+      return [newBpiMessageCandidate, errors];
+    }
+    
+    if (!isUuid(newBpiMessageCandidate.id)) {
+      errors.push('id is not a valid UUID');
+    }
+
+    if (!isUuid(newBpiMessageCandidate.fromBpiSubjectId)) {
+      errors.push('fromBpiSubjectId is not a valid UUID');
+    }
+
+    if (!isUuid(newBpiMessageCandidate.toBpiSubjectId)) {
+      errors.push('toBpiSubjectId is not a valid UUID');
+    }
+
+    try {
+      JSON.parse(newBpiMessageCandidate.content); // TODO: Make it more robust (i.e JSON.parse(123) does not throw)
+    } catch (e) {
+      errors.push(`content: ${newBpiMessageCandidate.content} is not valid JSON. Error: ${e}`);
+    }
+
+    if (!newBpiMessageCandidate.signature || newBpiMessageCandidate.signature.length === 0) {
+      errors.push('signature is empty');
+    }
+
+    if (
+      newBpiMessageCandidate.type !== BpiMessageType.MessageType1 &&
+      newBpiMessageCandidate.type !== BpiMessageType.MessageType2
+    ) {
+      errors.push(`type is unknown ${newBpiMessageCandidate.type}`);
+    }
+
+    return [newBpiMessageCandidate, errors];
   }
 }
