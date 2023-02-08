8a2ee39b60bd08b10c99118afff774f7f91db9ab
biscuitdey <biswashreedey@gmail.com>
2023-02-08 16:28:21 +0530

    Storing address of deployed ccsm contract

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/blockchain/ethereum/scripts/deploy.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/blockchain/ethereum/scripts/deploy.ts
@@ -1,17 +1,30 @@
 import { ethers } from 'hardhat';
+import fs from 'fs';
 
-async function main() {
+async function deployCcsmContract() {
   const Ccsm = await ethers.getContractFactory('Ccsm');
   const ccsm = await Ccsm.deploy();
 
   await ccsm.deployed();
 
-  console.log(`Ccsm contact deployed to ${ccsm.address}`);
+  await storeDeployedCcsmContractAddress(ccsm.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+async function storeDeployedCcsmContractAddress(contractAddress: string) {
+  const ccsmAddress = JSON.stringify({
+    contractAddress: contractAddress,
+  });
+
+  fs.appendFile('ccsmContractAddress.json', ccsmAddress, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //file written successfully
+  });
+}
+
+deployCcsmContract().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
