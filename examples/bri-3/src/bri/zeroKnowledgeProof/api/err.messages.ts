0c17048405246ce2f3488334edc2a1c68b1f6ebe
biscuitdey <biswashreedey@gmail.com>
2023-02-09 10:15:07 +0530

    Updated error messages

--- examples/bri-3/src/bri/zeroKnowledgeProof/api/err.messages.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/api/err.messages.ts
@@ -1,16 +1,8 @@
 export const INVALID_ANCHOR_HASH_INPUT =
   'Input document provided for anchor hash does not exist or is blank';
 
-<<<<<<< HEAD
-export const CCSM_ANCHOR_HASH_NOT_FOUND_ERR_MESSAGE =
-  'Anchor Hash does not exist on the CCSM or the local storage.';
-
-export const DOCUMENT_NOT_FOUND_ERR_MESSAGE =
-  'Document does not exist for the CCSM Anchor Hash';
-=======
 export const ANCHOR_HASH_ON_CCSM_NOT_FOUND_ERR_MESSAGE =
   'Anchor Hash does not exist on the CCSM.';
 
 export const ANCHOR_HASH_NOT_FOUND_ERR_MESSAGE =
   'Anchor Hash does not exist in the local storage.';
->>>>>>> dcace9af79f4e81b8c11472bf870f23e6d9d9f0b
