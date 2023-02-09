37fdc24fd271eb2fee9a6121a70ebe1d49eb6cb2
ognjenkurtic <kurticognjen@gmail.com>
2023-02-09 15:15:40 +0100

    WIP - Make sure missing bpi subject do not crash the app in process inbound message cmd handler

--- examples/bri-3/src/bri/communication/capabilities/processInboundMessage/processInboundMessageCommand.handler.ts
+++ examples/bri-3/src/bri/communication/capabilities/processInboundMessage/processInboundMessageCommand.handler.ts
@@ -1,4 +1,5 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BpiSubject } from 'src/bri/identity/bpiSubjects/models/bpiSubject';
 import { BpiMessageAgent } from '../../agents/bpiMessages.agent';
 import { BpiMessageStorageAgent } from '../../agents/bpiMessagesStorage.agent';
 import { ProcessInboundBpiMessageCommand } from './processInboundMessage.command';
@@ -12,16 +13,29 @@ export class ProcessInboundMessageCommandHandler
     private readonly storageAgent: BpiMessageStorageAgent) {}
 
   async execute(command: ProcessInboundBpiMessageCommand) {
-    const { fromBpiSubject, toBpiSubject } =
-      await this.agent.getFromAndToSubjectsAndThrowIfNotExist(
-        command.bpiMessage.fromBpiSubjectId,
-        command.bpiMessage.toBpiSubjectId,
-      );
+    let dbFromBpiSubject: BpiSubject;
+    let dbToBpiSubject: BpiSubject;
+    
+    try {
+      const { fromBpiSubject, toBpiSubject } =
+        await this.agent.getFromAndToSubjectsAndThrowIfNotExist(
+          command.bpiMessage.fromBpiSubjectId,
+          command.bpiMessage.toBpiSubjectId,
+        );
+      
+        // TODO: Better way to map this
+        dbFromBpiSubject = fromBpiSubject;
+        dbToBpiSubject = toBpiSubject;
+    } catch (e) {
+      // TODO: Log and ignore message
+      return;
+    }
+    
 
     const newBpiMessageCandidate = this.agent.createNewBpiMessage(
       command.bpiMessage.id,
-      fromBpiSubject,
-      toBpiSubject,
+      dbFromBpiSubject,
+      dbToBpiSubject,
       command.bpiMessage.content,
       command.bpiMessage.signature,
       command.bpiMessage.type,
