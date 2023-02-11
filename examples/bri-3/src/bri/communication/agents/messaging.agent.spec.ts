b315cf9038ed27996f0b045dff7ba43745f7b639
ognjenkurtic <kurticognjen@gmail.com>
2023-02-11 22:07:22 +0100

    Run prettier

--- examples/bri-3/src/bri/communication/agents/messaging.agent.spec.ts
+++ examples/bri-3/src/bri/communication/agents/messaging.agent.spec.ts
@@ -1,140 +1,157 @@
-import { CreateBpiMessageDto } from "../api/dtos/request/createBpiMessage.dto";
-import { MessagingAgent } from "./messaging.agent";
+import { CreateBpiMessageDto } from '../api/dtos/request/createBpiMessage.dto';
+import { MessagingAgent } from './messaging.agent';
 
 describe('Messaging Agent', () => {
   it('Should return error when validating incorrect JSON raw message', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "test";
+
+    const rawMessage = 'test';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(resultDto).toBeUndefined();
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("test is not valid JSON. Error: SyntaxError: Unexpected token e in JSON at position 1");
+    expect(validationErrors[0]).toEqual(
+      'test is not valid JSON. Error: SyntaxError: Unexpected token e in JSON at position 1',
+    );
   });
 
   it('Should return error when validating correct JSON raw message with invalid GUID in id field', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"pakakoto\", \"from\": \"71302cec-0a38-469a-a4e5-f58bdfc4ab32\", \"to\": \"76cdd901-d87d-4c87-b572-155afe45c128\", \"content\": { \"testProp\":\"testValue\" }, \"signature\": \"xyz\", \"type\": 0}";
+
+    const rawMessage =
+      '{ "id": "pakakoto", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("id pakakoto is not a valid UUID");
+    expect(validationErrors[0]).toEqual('id pakakoto is not a valid UUID');
   });
 
   it('Should return error when validating correct JSON raw message with invalid GUID in from field', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"0a3dd67c-c031-4b50-95df-0bc5fc1c78b5\", \"from\": \"7123\", \"to\": \"76cdd901-d87d-4c87-b572-155afe45c128\", \"content\": { \"testProp\":\"testValue\" }, \"signature\": \"xyz\", \"type\": 0}";
+
+    const rawMessage =
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "7123", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("from 7123 is not a valid UUID");
+    expect(validationErrors[0]).toEqual('from 7123 is not a valid UUID');
   });
 
   it('Should return error when validating correct JSON raw message with invalid GUID in to field', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"0a3dd67c-c031-4b50-95df-0bc5fc1c78b5\", \"from\": \"71302cec-0a38-469a-a4e5-f58bdfc4ab32\", \"to\": \"msm24\", \"content\": { \"testProp\":\"testValue\" }, \"signature\": \"xyz\", \"type\": 0}";
+
+    const rawMessage =
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "msm24", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("to msm24 is not a valid UUID");
+    expect(validationErrors[0]).toEqual('to msm24 is not a valid UUID');
   });
 
   it('Should return error when validating correct JSON raw message with content field not valid JSON', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"0a3dd67c-c031-4b50-95df-0bc5fc1c78b5\", \"from\": \"71302cec-0a38-469a-a4e5-f58bdfc4ab32\", \"to\": \"76cdd901-d87d-4c87-b572-155afe45c128\", \"content\": 123, \"signature\": \"xyz\", \"type\": 0}";
+
+    const rawMessage =
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": 123, "signature": "xyz", "type": 0}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("content: 123 is not valid JSON.");
+    expect(validationErrors[0]).toEqual('content: 123 is not valid JSON.');
   });
 
   it('Should return error when validating correct JSON raw message with signature field empty', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"0a3dd67c-c031-4b50-95df-0bc5fc1c78b5\", \"from\": \"71302cec-0a38-469a-a4e5-f58bdfc4ab32\", \"to\": \"76cdd901-d87d-4c87-b572-155afe45c128\", \"content\": { \"testProp\":\"testValue\" }, \"signature\": \"\", \"type\": 0}";
+
+    const rawMessage =
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "", "type": 0}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("signature is empty");
+    expect(validationErrors[0]).toEqual('signature is empty');
   });
 
   it('Should return error when validating correct JSON raw message with message type not being info', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"0a3dd67c-c031-4b50-95df-0bc5fc1c78b5\", \"from\": \"71302cec-0a38-469a-a4e5-f58bdfc4ab32\", \"to\": \"76cdd901-d87d-4c87-b572-155afe45c128\", \"content\": { \"testProp\":\"testValue\" }, \"signature\": \"xyz\", \"type\": 1}";
+
+    const rawMessage =
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 1}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(1);
-    expect(validationErrors[0]).toEqual("type is unknown 1");
+    expect(validationErrors[0]).toEqual('type is unknown 1');
   });
 
   it('Should return no errors and create message dto when validating correct JSON raw message', () => {
     // Arrange
     let resultDto: CreateBpiMessageDto;
     let validationErrors: string[] = [];
-    
-    const rawMessage = "{ \"id\": \"0a3dd67c-c031-4b50-95df-0bc5fc1c78b5\", \"from\": \"71302cec-0a38-469a-a4e5-f58bdfc4ab32\", \"to\": \"76cdd901-d87d-4c87-b572-155afe45c128\", \"content\": { \"testProp\":\"testValue\" }, \"signature\": \"xyz\", \"type\": 0}";
+
+    const rawMessage =
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const messagingAgent = new MessagingAgent(null, null, null);
-    [resultDto, validationErrors] = messagingAgent.validateBpiMessageFormat(rawMessage);
+    [resultDto, validationErrors] =
+      messagingAgent.validateBpiMessageFormat(rawMessage);
 
     // Assert
     expect(validationErrors.length).toBe(0);
     expect(resultDto).toBeDefined();
-    expect(resultDto.id).toEqual("0a3dd67c-c031-4b50-95df-0bc5fc1c78b5");
-    expect(resultDto.from).toEqual("71302cec-0a38-469a-a4e5-f58bdfc4ab32");
-    expect(resultDto.to).toEqual("76cdd901-d87d-4c87-b572-155afe45c128");
+    expect(resultDto.id).toEqual('0a3dd67c-c031-4b50-95df-0bc5fc1c78b5');
+    expect(resultDto.from).toEqual('71302cec-0a38-469a-a4e5-f58bdfc4ab32');
+    expect(resultDto.to).toEqual('76cdd901-d87d-4c87-b572-155afe45c128');
     // TODO: Find a fix for the escape character
     // expect(resultDto.content).toEqual("{ \"testProp\": \"testValue\" }");
-    expect(resultDto.signature).toEqual("xyz");
+    expect(resultDto.signature).toEqual('xyz');
     expect(resultDto.type).toEqual(0);
   });
 });
