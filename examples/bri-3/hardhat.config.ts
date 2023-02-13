c398b3f2f8897592dbdeef5286058f25420bb2d6
biscuitdey <biswashreedey@gmail.com>
2023-02-13 17:21:40 +0530

    Updated hardhat config to run tests

--- examples/bri-3/hardhat.config.ts
+++ examples/bri-3/hardhat.config.ts
@@ -3,22 +3,35 @@ import '@nomiclabs/hardhat-ethers';
 import '@nomicfoundation/hardhat-toolbox';
 import 'dotenv/config';
 
-const config: HardhatUserConfig = {
-  networks: {
-    goerli: {
-      url: `${process.env.ALCHEMY_URL}`,
-      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
-    },
-  },
-  solidity: {
-    version: '0.8.17',
-  },
-  paths: {
-    sources:
-      './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/contracts',
-    artifacts:
-      './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/artifacts/artifacts',
-  },
-};
+const config: HardhatUserConfig = process.env.CCSM_NETWORK
+  ? {
+      defaultNetwork: process.env.CCSM_NETWORK,
+      networks: {
+        goerli: {
+          url: `${process.env.ALCHEMY_URL}`,
+          accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+        },
+      },
+      solidity: {
+        version: '0.8.17',
+      },
+      paths: {
+        sources:
+          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/contracts',
+        artifacts:
+          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/artifacts/artifacts',
+      },
+    }
+  : {
+      solidity: {
+        version: '0.8.17',
+      },
+      paths: {
+        sources:
+          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/contracts',
+        artifacts:
+          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/artifacts/artifacts',
+      },
+    };
 
 export default config;
