deb3dfc7d60b2d34c93c718c4680a304dc4f32f6
biscuitdey <biswashreedey@gmail.com>
2023-02-13 16:45:35 +0530

    Removed unnecessary hardhat network config

--- examples/bri-3/hardhat.config.ts
+++ examples/bri-3/hardhat.config.ts
@@ -1,19 +1,15 @@
 import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-ganache';
 import '@nomiclabs/hardhat-ethers';
 import '@nomicfoundation/hardhat-toolbox';
 import 'dotenv/config';
 import 'hardhat-change-network';
 
 const config: HardhatUserConfig = {
-  defaultNetwork: process.env.CCSM_NETWORK,
+  defaultNetwork: 'goerli',
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/bvik-_IDWOeJhYuqhc981klKE-r6IRCk',
       accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
-    },
-    ganache: {
-      url: 'http://127.0.0.1:8545',
     },
   },
   solidity: {
