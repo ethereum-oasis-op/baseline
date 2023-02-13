e0e0b12ee15a728142aea1e1452d9c22d752fa62
biscuitdey <biswashreedey@gmail.com>
2023-02-13 17:29:48 +0530

    Removed unnecessary hardhat tests

--- examples/bri-3/hardhat.config.ts
+++ examples/bri-3/hardhat.config.ts
@@ -3,35 +3,23 @@ import '@nomiclabs/hardhat-ethers';
 import '@nomicfoundation/hardhat-toolbox';
 import 'dotenv/config';
 
-const config: HardhatUserConfig = process.env.CCSM_NETWORK
-  ? {
-      defaultNetwork: process.env.CCSM_NETWORK,
-      networks: {
-        goerli: {
-          url: `${process.env.ALCHEMY_URL}`,
-          accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
-        },
-      },
-      solidity: {
-        version: '0.8.17',
-      },
-      paths: {
-        sources:
-          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/contracts',
-        artifacts:
-          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/artifacts/artifacts',
-      },
-    }
-  : {
-      solidity: {
-        version: '0.8.17',
-      },
-      paths: {
-        sources:
-          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/contracts',
-        artifacts:
-          './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/artifacts/artifacts',
-      },
-    };
+const config: HardhatUserConfig = {
+  defaultNetwork: process.env.CCSM_NETWORK,
+  networks: {
+    goerli: {
+      url: `${process.env.ALCHEMY_URL}`,
+      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    version: '0.8.17',
+  },
+  paths: {
+    sources:
+      './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/contracts',
+    artifacts:
+      './src/bri/zeroKnowledgeProof/services/blockchain/ethereum/artifacts/artifacts',
+  },
+};
 
 export default config;
