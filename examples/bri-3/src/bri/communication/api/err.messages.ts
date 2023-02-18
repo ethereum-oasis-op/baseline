0773fd1cc3c86f5656a593ee3a720f3160e98ebb
ognjenkurtic <kurticognjen@gmail.com>
2023-02-18 15:28:25 +0100

    Add existing id check when creating new bpi messages via API

--- examples/bri-3/src/bri/communication/api/err.messages.ts
+++ examples/bri-3/src/bri/communication/api/err.messages.ts
@@ -1,2 +1,4 @@
 export const NOT_FOUND_ERR_MESSAGE = 'Bpi Message does not exist.';
 export const ID_EMPTY_ERR_MESSAGE = 'Id cannot be empty.';
+export const BPI_MESSAGE_ALREADY_EXISTS =
+  'Bpi Message with this Id already exists.';
--- examples/bri-3/src/bri/communication/capabilities/createBpiMessage/createBpiMessageCommand.handler.ts
+++ examples/bri-3/src/bri/communication/capabilities/createBpiMessage/createBpiMessageCommand.handler.ts
@@ -1,7 +1,9 @@
+import { BadRequestException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BpiMessageAgent } from '../../agents/bpiMessages.agent';
 import { BpiMessageStorageAgent } from '../../agents/bpiMessagesStorage.agent';
 import { MessagingAgent } from '../../agents/messaging.agent';
+import { BPI_MESSAGE_ALREADY_EXISTS } from '../../api/err.messages';
 import { CreateBpiMessageCommand } from './createBpiMessage.command';
 
 @CommandHandler(CreateBpiMessageCommand)
@@ -15,7 +17,13 @@ export class CreateBpiMessageCommandHandler
   ) {}
 
   async execute(command: CreateBpiMessageCommand) {
-    // TODO: Validate Bpi Message Id unique
+    const existingBpiMessage = await this.storageAgent.getBpiMessageById(
+      command.id,
+    );
+
+    if (existingBpiMessage) {
+      throw new BadRequestException(BPI_MESSAGE_ALREADY_EXISTS);
+    }
 
     const [fromBpiSubject, toBpiSubject] =
       await this.agent.getFromAndToSubjectsAndThrowIfNotExist(
@@ -39,7 +47,7 @@ export class CreateBpiMessageCommandHandler
     );
 
     // TODO: This is naive as it publishes to a channel anyone who can auth with the NATS server can subscribe to.
-    // Wiil be improved by introducing NATS authz to allow only the recipient Bpi Subject to listen on this channel
+    // Will be improved by introducing NATS authz to allow only the recipient Bpi Subject to listen on this channel (#648)
     await this.messagingAgent.publishMessage(
       toBpiSubject.publicKey,
       this.messagingAgent.serializeBpiMessage(newBpiMessage),
