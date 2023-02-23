b2ad62638aa1c5e21538f936665a87d18657fcf8
ognjenkurtic <kurticognjen@gmail.com>
2023-02-23 17:20:04 +0100

    Add param to bpi message error message

--- examples/bri-3/src/bri/communication/api/err.messages.ts
+++ examples/bri-3/src/bri/communication/api/err.messages.ts
@@ -1,4 +1,4 @@
 export const NOT_FOUND_ERR_MESSAGE = 'Bpi Message does not exist.';
 export const ID_EMPTY_ERR_MESSAGE = 'Id cannot be empty.';
-export const BPI_MESSAGE_ALREADY_EXISTS =
-  'Bpi Message with this Id already exists.';
+export const BPI_MESSAGE_ALREADY_EXISTS = (id: string) =>
+  `Bpi Message with this Id: ${id} already exists.`;
