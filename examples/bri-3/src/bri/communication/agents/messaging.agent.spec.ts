24b95919aa37b15d2fd1d57c9327195e708639c9
ognjenkurtic <kurticognjen@gmail.com>
2023-02-23 17:08:41 +0100

    Tackle  TODO: Switch to BpiMessage instead of DTO in messaging.agent

--- examples/bri-3/src/bri/communication/agents/messaging.agent.spec.ts
+++ examples/bri-3/src/bri/communication/agents/messaging.agent.spec.ts
@@ -26,7 +26,7 @@ describe('Messaging Agent', () => {
   it('Should return error when validating correct JSON raw message with invalid UUID in id field', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "pakakoto", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
+      '{ "id": "pakakoto", "fromBpiSubjectId": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "toBpiSubjectId": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const [, validationErrors] =
@@ -40,7 +40,7 @@ describe('Messaging Agent', () => {
   it('Should return error when validating correct JSON raw message with invalid UUID in from field', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "7123", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "fromBpiSubjectId": "7123", "toBpiSubjectId": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const [, validationErrors] =
@@ -54,7 +54,7 @@ describe('Messaging Agent', () => {
   it('Should return error when validating correct JSON raw message with invalid UUID in to field', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "msm24", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "fromBpiSubjectId": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "toBpiSubjectId": "msm24", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const [, validationErrors] =
@@ -68,7 +68,7 @@ describe('Messaging Agent', () => {
   it('Should return error when validating correct JSON raw message with content field not valid JSON', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": 123, "signature": "xyz", "type": 0}';
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "fromBpiSubjectId": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "toBpiSubjectId": "76cdd901-d87d-4c87-b572-155afe45c128", "content": 123, "signature": "xyz", "type": 0}';
 
     // Act
     const [, validationErrors] =
@@ -82,7 +82,7 @@ describe('Messaging Agent', () => {
   it('Should return error when validating correct JSON raw message with signature field empty', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "", "type": 0}';
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "fromBpiSubjectId": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "toBpiSubjectId": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "", "type": 0}';
 
     // Act
     const [, validationErrors] =
@@ -96,7 +96,7 @@ describe('Messaging Agent', () => {
   it('Should return error when validating correct JSON raw message with message type not being info', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 1}';
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "fromBpiSubjectId": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "toBpiSubjectId": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 1}';
 
     // Act
     const [, validationErrors] =
@@ -110,7 +110,7 @@ describe('Messaging Agent', () => {
   it('Should return no errors and create message dto when validating correct JSON raw message', () => {
     // Arrange
     const rawMessage =
-      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "from": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "to": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
+      '{ "id": "0a3dd67c-c031-4b50-95df-0bc5fc1c78b5", "fromBpiSubjectId": "71302cec-0a38-469a-a4e5-f58bdfc4ab32", "toBpiSubjectId": "76cdd901-d87d-4c87-b572-155afe45c128", "content": { "testProp":"testValue" }, "signature": "xyz", "type": 0}';
 
     // Act
     const [resultDto, validationErrors] =
@@ -120,8 +120,12 @@ describe('Messaging Agent', () => {
     expect(validationErrors.length).toBe(0);
     expect(resultDto).toBeDefined();
     expect(resultDto.id).toEqual('0a3dd67c-c031-4b50-95df-0bc5fc1c78b5');
-    expect(resultDto.from).toEqual('71302cec-0a38-469a-a4e5-f58bdfc4ab32');
-    expect(resultDto.to).toEqual('76cdd901-d87d-4c87-b572-155afe45c128');
+    expect(resultDto.fromBpiSubjectId).toEqual(
+      '71302cec-0a38-469a-a4e5-f58bdfc4ab32',
+    );
+    expect(resultDto.toBpiSubjectId).toEqual(
+      '76cdd901-d87d-4c87-b572-155afe45c128',
+    );
     expect(resultDto.content).toEqual({ testProp: 'testValue' });
     expect(resultDto.signature).toEqual('xyz');
     expect(resultDto.type).toEqual(0);
