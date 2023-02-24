59021650c01171256c01f64bff38149b80d15a96
Ybittan <ybittan@yahoo.com>
2023-02-24 11:40:34 -0600

    createworkflow dto spec values extracted

--- examples/bri-3/src/bri/workgroup/workflows/api/dtos/request/createWorkflow.dto.spec.ts
+++ examples/bri-3/src/bri/workgroup/workflows/api/dtos/request/createWorkflow.dto.spec.ts
@@ -1,11 +1,15 @@
 import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { CreateWorkflowDto } from './createWorkflow.dto';
+import { TEST_VALUES } from 'src/bri/shared/constants';
 
 describe('CreateWorkflowDto', () => {
   it('should return error in case name not provided.', async () => {
     // Arrange
-    const dto = { workstepIds: ['1'], workgroupId: '1' };
+    const dto = {
+      workstepIds: [TEST_VALUES.workstepId],
+      workgroupId: TEST_VALUES.workstepId,
+    };
     const createWorkflowDto = plainToInstance(CreateWorkflowDto, dto);
 
     // Act
@@ -13,7 +17,7 @@ describe('CreateWorkflowDto', () => {
 
     // Assert
     expect(errors.length).toBe(1);
-    expect(errors[0].property).toEqual('name');
+    expect(errors[0].property).toEqual(TEST_VALUES.name);
     expect(errors[0].constraints.isNotEmpty).toContain(
       'name should not be empty',
     );
@@ -21,7 +25,7 @@ describe('CreateWorkflowDto', () => {
 
   it('should return error in case workgroupId not provided.', async () => {
     // Arrange
-    const dto = { name: 'test', workstepIds: ['1'] };
+    const dto = { name: TEST_VALUES.name, workstepIds: ['1'] };
     const createWorkflowDto = plainToInstance(CreateWorkflowDto, dto);
 
     // Act
@@ -37,7 +41,7 @@ describe('CreateWorkflowDto', () => {
 
   it('should return error in case workstepIds not provided.', async () => {
     // Arrange
-    const dto = { name: 'test', workgroupId: '1' };
+    const dto = { name: TEST_VALUES.name, workgroupId: '1' };
     const createWorkflowDto = plainToInstance(CreateWorkflowDto, dto);
 
     // Act
@@ -53,7 +57,7 @@ describe('CreateWorkflowDto', () => {
 
   it('should return error in case an empty workstepIds array is provided.', async () => {
     // Arrange
-    const dto = { name: 'test', workstepIds: [], workgroupId: '1' };
+    const dto = { name: TEST_VALUES.name, workstepIds: [], workgroupId: '1' };
     const createWorkflowDto = plainToInstance(CreateWorkflowDto, dto);
 
     // Act
@@ -69,7 +73,11 @@ describe('CreateWorkflowDto', () => {
 
   it('should return no error if all required properties provided.', async () => {
     // Arrange
-    const dto = { name: 'test', workstepIds: ['1'], workgroupId: '1' };
+    const dto = {
+      name: TEST_VALUES.name,
+      workstepIds: [TEST_VALUES.workstepId],
+      workgroupId: TEST_VALUES.workgroupId,
+    };
     const createWorkflowDto = plainToInstance(CreateWorkflowDto, dto);
 
     // Act
