472eb295801713f9118288bdc602aeae38b51dec
Ybittan <ybittan@yahoo.com>
2023-02-24 11:38:29 -0600

    createbpisubject dto  spec values extracted

--- examples/bri-3/src/bri/identity/bpiSubjects/api/dtos/request/createBpiSubject.dto.spec.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/api/dtos/request/createBpiSubject.dto.spec.ts
@@ -14,7 +14,7 @@ describe('CreateBpiSubjectDto', () => {
 
     // Assert
     expect(errors.length).toBe(1);
-    expect(errors[0].property).toEqual('name');
+    expect(errors[0].property).toEqual(TEST_VALUES.name);
     expect(errors[0].constraints.isNotEmpty).toContain(
       'name ' + SHOULD_NOT_BE_EMPTY_VALIDATION_MESSAGE,
     );
@@ -30,7 +30,7 @@ describe('CreateBpiSubjectDto', () => {
 
     // Assert
     expect(errors.length).toBe(1);
-    expect(errors[0].property).toEqual('desc');
+    expect(errors[0].property).toEqual(TEST_VALUES.description);
     expect(errors[0].constraints.isNotEmpty).toContain(
       'desc ' + SHOULD_NOT_BE_EMPTY_VALIDATION_MESSAGE,
     );
