fda3875ea1d59d9fe6b01787b5996fcd995275f6
ognjenkurtic <kurticognjen@gmail.com>
2023-03-08 15:27:42 +0100

    Add test for encryption service

---
+++ examples/bri-3/src/shared/encryption/encryption.service.spec.ts
@@ -0,0 +1,21 @@
+import { EncryptionService } from './encryption.service';
+
+let encryptionService: EncryptionService;
+
+beforeAll(async () => {
+    encryptionService = new EncryptionService();
+});
+
+describe('Encryption Service', () => {
+  it('Should encrypt and decrypt properly', async () => {
+    // Arrange
+    const testContent = "Liftbois are real";
+    
+    // Act
+    var encryptedContent = await encryptionService.encrypt(testContent);
+    var decrytedContent = await encryptionService.decrypt(encryptedContent);
+    
+    // Assert
+    expect(decrytedContent).toEqual(testContent);
+  })
+});
