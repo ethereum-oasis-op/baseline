f2364271643084896486337f38d90127471146bc
Kasshern <keith.salzman@protonmail.com>
2023-03-22 17:59:44 -0500

    Adds agent ability to fetch merkleTreeCandidate, create and update merkletree

--- examples/bri-3/src/bri/merkleTree/agents/merkleTree.agent.ts
+++ examples/bri-3/src/bri/merkleTree/agents/merkleTree.agent.ts
@@ -1,23 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import MerkleTree from 'merkletreejs';
-import { BinaryHashTree } from '../models/merkleTree';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 } from 'uuid';
+import { NOT_FOUND_ERR_MESSAGE } from '../api/err.messages';
+import { BpiMerkleTree } from '../models/merkleTree';
+import { MerkleTreeStorageAgent } from './merkleTreeStorage.agent';
 
 @Injectable()
 export class MerkleTreeAgent {
+  constructor(private storageAgent: MerkleTreeStorageAgent) {}
   public createNewMerkleTree(
     leaves: string[],
     hashFunction: unknown,
-  ): MerkleTree {
-    return new MerkleTree(leaves, hashFunction);
+  ): BpiMerkleTree {
+    return new BpiMerkleTree(v4(), leaves, hashFunction);
   }
 
   public updateMerkleTree(
-    binaryHashTreeToUpdate: BinaryHashTree,
+    merkleTreeToUpdate: BpiMerkleTree,
     leaves: string[],
     hashFunction: unknown,
   ) {
-    binaryHashTreeToUpdate.updateMerkleTree(
-      new MerkleTree(leaves, hashFunction),
-    );
+    merkleTreeToUpdate.updateLeaves(leaves);
+    merkleTreeToUpdate.updateHashFunction(hashFunction);
+  }
+
+  public async fetchMerkleTreeCandidateByIdAndThrowIfValidationFails(
+    id: string,
+  ): Promise<BpiMerkleTree> {
+    const bpiSubjectToUpdate: BpiMerkleTree =
+      await this.storageAgent.getMerkleTreeById(id);
+
+    if (!bpiSubjectToUpdate) {
+      throw new NotFoundException(NOT_FOUND_ERR_MESSAGE);
+    }
+
+    return bpiSubjectToUpdate;
   }
 }
