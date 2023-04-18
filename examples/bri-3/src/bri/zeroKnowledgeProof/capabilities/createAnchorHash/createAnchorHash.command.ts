10b74aeb9cb480ebc6588b6c01f479fc6165e52e
Siyuan Fan <sf316@duke.edu>
2023-04-17 20:52:45 -0400

    Remove everything related to createAnchorHash command and handler

--- examples/bri-3/src/bri/zeroKnowledgeProof/capabilities/createAnchorHash/createAnchorHash.command.ts
+++
@@ -1,8 +0,0 @@
-import { BpiSubjectAccount } from '../../../identity/bpiSubjectAccounts/models/bpiSubjectAccount';
-
-export class CreateAnchorHashCommand {
-  constructor(
-    public readonly ownerAccount: BpiSubjectAccount,
-    public readonly state: string,
-  ) {}
-}
