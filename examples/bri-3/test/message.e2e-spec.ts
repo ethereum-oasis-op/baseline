f6b58d138863238bab1b7859f8b84211124d59b2
ognjenkurtic <kurticognjen@gmail.com>
2023-04-26 22:36:02 +0200

    Fix jeste2e configuration and introduce a simple messages controller e2e test

--- examples/bri-3/test/message.e2e-spec.ts
+++ examples/bri-3/test/message.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('MessageController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/messages/:id (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/messages/123')
+      .expect(404);
   });
 });
