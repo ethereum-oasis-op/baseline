65a6d76ad9c20a3912ae9f2144db75c909ec47d0
ognjenkurtic <kurticognjen@gmail.com>
2023-04-29 21:37:47 +0200

    Write the full e2e test (with missing JWT)

--- examples/bri-3/test/workgroupAdministration.e2e-spec.ts
+++ examples/bri-3/test/workgroupAdministration.e2e-spec.ts
@@ -18,22 +18,63 @@ describe('Workgroup administration', () => {
 
   it('Logs in an internal Bpi Subject, creates a Workgroup and two Bpi Subjects and adds them as participants to the Workgroup', async () => {
     // login as internal BPI subject
-    var response = await request(app.getHttpServer())
+    var nonceResponse = await request(app.getHttpServer())
       .post('/auth/nonce')
       .send({publicKey: '0x08872e27BC5d78F1FC4590803369492868A1FCCb'})
 
     const signer = new ethers.Wallet("2c95d82bcd8851bd3a813c50afafb025228bf8d237e8fd37ba4adba3a7596d58", null);
 
-    const signature = signer.signMessage(response.text);
+    const signature = await signer.signMessage(nonceResponse.text);
 
+    var loginResponse = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        message: nonceResponse.text,
+        signature: signature,
+        publicKey: '0x08872e27BC5d78F1FC4590803369492868A1FCCb'
+      })
 
     // create two bpi subjects
+
+    const createdBpiSubject1Id = await request(app.getHttpServer())
+      .post('/subjects')
+      .send({
+        name: 'External Bpi Subject 1',
+        desc: 'A test Bpi subject',
+        publicKey: 'todo'
+      })
+    
+    const createdBpiSubject2Id = await request(app.getHttpServer())
+      .post('/subjects')
+      .send({
+        name: 'External Bpi Subject 2',
+        desc: 'Another test Bpi subject',
+        publicKey: 'todo'
+      })
+
     // create workgroup
-    // add both bpi subjects to workgroup
+
+    const createdWorkgroupId = await request(app.getHttpServer())
+      .post('/workgroups')
+      .send({
+        name: 'Test workgroup',
+        securityPolicy: 'secPol',
+        privacyPolicy: 'privPol'
+      })
+    
+     await request(app.getHttpServer())
+      .put(`/workgroups/${createdWorkgroupId}`)
+      .send({
+        participantIds: [createdBpiSubject1Id, createdBpiSubject2Id]
+      })
+      .expect(200);
+  
+    var res = await request(app.getHttpServer())
+      .get('/workgroups/${createdWorkgroupId}`)')
+      .expect(200);
+      
     // verify participants
 
-    return request(app.getHttpServer())
-      .post('/messages/123')
-      .expect(404);
+    return res;
   });
 });
