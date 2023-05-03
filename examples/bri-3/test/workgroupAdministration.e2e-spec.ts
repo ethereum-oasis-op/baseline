155d47f45e26019877741aa1fa58f632e513f5f6
ognjenkurtic <kurticognjen@gmail.com>
2023-05-03 22:25:27 +0200

    Extract login part of the e2e test to a separate method

--- examples/bri-3/test/workgroupAdministration.e2e-spec.ts
+++ examples/bri-3/test/workgroupAdministration.e2e-spec.ts
@@ -18,34 +18,18 @@ describe('Workgroup administration', () => {
     await app.init();
   });
 
-  it('Logs in an internal Bpi Subject, creates a Workgroup and two Bpi Subjects and adds them as participants to the Workgroup', async () => {
-    // login as internal BPI subject
-    var nonceResponse = await request(app.getHttpServer())
-      .post('/auth/nonce')
-      .send({publicKey: '0x08872e27BC5d78F1FC4590803369492868A1FCCb'})
+  it('Logs in an internal Bpi Subject, creates a Workgroup and two Bpi Subjects and adds them as participants to the Workgroup', async () => {    
+    
+    const accessToken = await loginAsInternalBpiSubjectAndReturnAnAccessToken(app);
 
-    const signer = new ethers.Wallet("2c95d82bcd8851bd3a813c50afafb025228bf8d237e8fd37ba4adba3a7596d58", null);
-
-    const signature = await signer.signMessage(nonceResponse.text);
-
-    var loginResponse = await request(app.getHttpServer())
-      .post('/auth/login')
-      .send({
-        message: nonceResponse.text,
-        signature: signature,
-        publicKey: '0x08872e27BC5d78F1FC4590803369492868A1FCCb'
-      })
-
-    var accesstoken = JSON.parse(loginResponse.text)['access_token'];
-    // create two bpi subjects
-
+    // Create two new bpi subjects
     const createdBpiSubject1Response = await request(app.getHttpServer())
       .post('/subjects')
-      .set('Authorization', `Bearer ${accesstoken}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .send({
         name: 'External Bpi Subject 1',
-        desc: 'A test Bpi subject',
-        publicKey: 'todo'
+        desc: 'A test Bpi Subject',
+        publicKey: 'Bpi Subject 1 dummy public key'
       })
       .expect(201);
     
@@ -53,21 +37,21 @@ describe('Workgroup administration', () => {
     
     const createdBpiSubject2Response = await request(app.getHttpServer())
       .post('/subjects')
-      .set('Authorization', `Bearer ${accesstoken}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .send({
         name: 'External Bpi Subject 2',
         desc: 'Another test Bpi subject',
-        publicKey: 'todo'
+        publicKey: 'Bpi Subject 2 dummy public key'
       })
       .expect(201);
     
     const createdBpiSubject2Id = createdBpiSubject2Response.text;
 
-    // create workgroup
+    // Create a workgroup
 
     const createdWorkgroupResponse = await request(app.getHttpServer())
       .post('/workgroups')
-      .set('Authorization', `Bearer ${accesstoken}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .send({
         name: 'Test workgroup',
         securityPolicy: 'secPol',
@@ -77,11 +61,11 @@ describe('Workgroup administration', () => {
     
     const createdWorkgroupId = createdWorkgroupResponse.text;
 
-    // update workgroup with participants
+    // Update workgroup with the bpi subjects created in previous steps
     
      await request(app.getHttpServer())
       .put(`/workgroups/${createdWorkgroupId}`)
-      .set('Authorization', `Bearer ${accesstoken}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .send({
         name: 'Test workgroup',
         administratorIds: [createdBpiSubject1Id],
@@ -90,14 +74,14 @@ describe('Workgroup administration', () => {
         participantIds: [createdBpiSubject1Id, createdBpiSubject2Id]
       })
       .expect(200);
+    
+    // Verify these Bpi subjects are participants of the created workgroup
   
     const getWorkgroupResponse = await request(app.getHttpServer())
       .get(`/workgroups/${createdWorkgroupId}`)
-      .set('Authorization', `Bearer ${accesstoken}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .expect(200);
       
-    // verify participants
-
     const resultWorkgroup = JSON.parse(getWorkgroupResponse.text);
 
     expect(resultWorkgroup.participants.length).toBe(2);
@@ -105,3 +89,28 @@ describe('Workgroup administration', () => {
     expect(resultWorkgroup.participants[1].id).toEqual(createdBpiSubject2Id);
   });
 });
+
+async function loginAsInternalBpiSubjectAndReturnAnAccessToken(app: INestApplication): Promise<string> {
+  // These two values must be inline with the value for the bpiAdmin from seed.ts
+  const internalBpiSubjectPublicKey = '0x08872e27BC5d78F1FC4590803369492868A1FCCb';
+  const internalBpiSubjectPrivateKey = '2c95d82bcd8851bd3a813c50afafb025228bf8d237e8fd37ba4adba3a7596d58';
+
+  const nonceResponse = await request(app.getHttpServer())
+    .post('/auth/nonce')
+    .send({publicKey: internalBpiSubjectPublicKey})
+    .expect(201);
+
+  const signer = new ethers.Wallet(internalBpiSubjectPrivateKey, null);
+  const signature = await signer.signMessage(nonceResponse.text);
+
+  var loginResponse = await request(app.getHttpServer())
+    .post('/auth/login')
+    .send({
+      message: nonceResponse.text,
+      signature: signature,
+      publicKey: internalBpiSubjectPublicKey
+    })
+    .expect(201);
+
+  return JSON.parse(loginResponse.text)['access_token'];
+}
