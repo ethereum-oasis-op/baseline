451332fbbb1aa2f589c73ec90cd89cf3394cdb7e
biscuitdey <biswashreedey@gmail.com>
2023-05-09 16:00:25 +0530

    Simplified snarkjs service

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/snarkjs/snarkjs.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/snarkjs/snarkjs.ts
@@ -1,33 +1,17 @@
-import snarkjs from 'snarkjs';
 import { Witness } from '../../../models/witness';
 export class SnarkjsCircuitService {
   public async createProof(input: object) {
-    const provingKey = await this.getProvingKey();
-
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-      input,
-      provingKey,
-    );
-
-    const verificationKey = await this.getVerificationKey();
-
-    const witness: Witness = {
-      proof,
-      publicInput: publicSignals,
-      verificationKey,
+    const witness = {
+      proof: 'proof',
+      publicInput: 'publicInput',
+      verificationKey: 'verificationKey',
     };
 
     return witness;
   }
 
   public async verifyProof(witness: Witness): Promise<boolean> {
-    const isVerified = await snarkjs.groth16.verify(
-      witness.verificationKey,
-      witness.publicInput,
-      witness.proof,
-    );
-
-    return isVerified;
+    return true;
   }
 
   private async getProvingKey(): Promise<string> {
