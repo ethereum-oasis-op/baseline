dba3d22eb4dc8440c1e4a3779484a6a14aeaf7a6
biscuitdey <biswashreedey@gmail.com>
2023-05-12 10:19:31 +0530

    Update comment on zk proof variable names

--- examples/bri-3/src/bri/zeroKnowledgeProof/models/proof.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/models/proof.ts
@@ -1,3 +1,10 @@
+/**
+ * Proof represents a typical zero knowledge proof.
+ * Groth16 uses pairing-friendly elliptic curves - G1 and G2.
+ * a, c are elements on G1 and b is an element on G2.
+ * The variables are named as per zk convention.
+ */
+
 export class Proof {
   a: string[];
   b: string[][];
