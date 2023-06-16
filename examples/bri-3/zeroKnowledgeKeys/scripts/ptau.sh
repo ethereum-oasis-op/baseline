b221d1112d2b356f3068aed4d519344b2cb2339f
biscuitdey <biswashreedey@gmail.com>
2023-06-16 15:09:04 +0530

    Clean up ptau.sh file

--- examples/bri-3/zeroKnowledgeKeys/scripts/ptau.sh
+++ examples/bri-3/zeroKnowledgeKeys/scripts/ptau.sh
@@ -3,7 +3,7 @@ set -e
 
 # --------------------------------------------------------------------------------
 # Phase 1
-# ... non-circuit-specific stuff
+# Circiut-independent setup
 
 # make ptau folder
 [ -d zeroKnowledgeKeys/ptau ] || mkdir zeroKnowledgeKeys/ptau
@@ -12,7 +12,7 @@ set -e
 # 12 is the power of two of the maximum number of constraints that the ceremony can accept: in this case, the number of constraints is 2 ^ 12 = 4,096.
 snarkjs powersoftau new bn128 12 zeroKnowledgeKeys/ptau/pot12_0000.ptau -v
 
-# Contribute to ceremony a few times...
+# Contribute to ceremony a few times.
 # As we want this to be non-interactive we'll just write something random-ish for entropy
 snarkjs powersoftau contribute zeroKnowledgeKeys/ptau/pot12_0000.ptau zeroKnowledgeKeys/ptau/pot12_0001.ptau \
     --name="First contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
@@ -24,7 +24,7 @@ snarkjs powersoftau contribute zeroKnowledgeKeys/ptau/pot12_0002.ptau zeroKnowle
 # Verify
 snarkjs powersoftau verify zeroKnowledgeKeys/ptau/pot12_0003.ptau
 
-# Apply random beacon to finalised this phase of the setup.
+# Apply random beacon to finalise this phase of the setup.
 # For more information about random beacons see here: https://eprint.iacr.org/2017/1050.pdf
 # For the purposes, the beacon is essentially a delayed hash function evaluated on 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f
 # as given in snarkjs docs.
