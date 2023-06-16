8fdbe9f52f10b4a3af614837eafd8abb8132104c
biscuitdey <biswashreedey@gmail.com>
2023-06-16 15:13:27 +0530

    Add details to circuit.sh

--- examples/bri-3/zeroKnowledgeKeys/scripts/circuit.sh
+++ examples/bri-3/zeroKnowledgeKeys/scripts/circuit.sh
@@ -3,7 +3,7 @@ set -e
 
 # --------------------------------------------------------------------------------
 # Phase 2
-# ... circuit-specific stuff
+# Circuit-specific setup
 
 # if zeroKnowledgeKeys/circuit does not exist, make folder
 [ -d zeroKnowledgeKeys/circuit ] || mkdir zeroKnowledgeKeys/circuit
@@ -14,10 +14,12 @@ circom src/bri/zeroKnowledgeProof/services/circuit/snarkjs/circuit.circom -o zer
 #Setup
 snarkjs groth16 setup zeroKnowledgeKeys/circuit/circuit.r1cs zeroKnowledgeKeys/ptau/pot12_final.ptau zeroKnowledgeKeys/circuit/circuit_final.zkey
 
-# # Generate reference zkey
+## Generate reference zkey -
+## The zkey is a zero-knowledge key that includes both the proving and verification keys as well as phase 2 contributions.
+## Importantly, one can verify whether a zkey belongs to a specific circuit or not.
 snarkjs zkey new zeroKnowledgeKeys/circuit/circuit.r1cs zeroKnowledgeKeys/ptau/pot12_final.ptau zeroKnowledgeKeys/circuit/circuit_0000.zkey
 
-# # Ceremony just like before but for zkey this time
+## Ceremony just like before but for zkey this time
 snarkjs zkey contribute zeroKnowledgeKeys/circuit/circuit_0000.zkey zeroKnowledgeKeys/circuit/circuit_0001.zkey \
     --name="First circuit contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
 snarkjs zkey contribute zeroKnowledgeKeys/circuit/circuit_0001.zkey zeroKnowledgeKeys/circuit/circuit_0002.zkey \
@@ -25,17 +27,17 @@ snarkjs zkey contribute zeroKnowledgeKeys/circuit/circuit_0001.zkey zeroKnowledg
 snarkjs zkey contribute zeroKnowledgeKeys/circuit/circuit_0002.zkey zeroKnowledgeKeys/circuit/circuit_0003.zkey \
     --name="Third circuit contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
 
-# #  Verify zkey
+##  Verify if zkey belongs to our specific circuit
 snarkjs zkey verify zeroKnowledgeKeys/circuit/circuit.r1cs zeroKnowledgeKeys/ptau/pot12_final.ptau zeroKnowledgeKeys/circuit/circuit_0003.zkey
 
-# # Apply random beacon as before
+## Apply random beacon as before
 snarkjs zkey beacon zeroKnowledgeKeys/circuit/circuit_0003.zkey zeroKnowledgeKeys/circuit/circuit_final.zkey \
     0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Circuit FinalBeacon phase2"
 
-# # Optional: verify final zkey
+## Optional: verify final zkey
 snarkjs zkey verify zeroKnowledgeKeys/circuit/circuit.r1cs zeroKnowledgeKeys/ptau/pot12_final.ptau zeroKnowledgeKeys/circuit/circuit_final.zkey
 
-# # Export verification key
+## Export verification key
 snarkjs zkey export verificationkey zeroKnowledgeKeys/circuit/circuit_final.zkey zeroKnowledgeKeys/circuit/circuit_verification_key.json
 
 # Export circuit verifier with updated name and solidity version
