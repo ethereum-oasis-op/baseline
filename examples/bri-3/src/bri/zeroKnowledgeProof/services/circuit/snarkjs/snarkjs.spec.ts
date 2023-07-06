a99f9ca1d42ba71f54bcc0eac8db029d2fe0687b
biscuitdey <biswashreedey@gmail.com>
2023-07-06 16:21:19 +0530

    Add tests for snarkjs circuit

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/snarkjs/snarkjs.spec.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/snarkjs/snarkjs.spec.ts
@@ -6,7 +6,7 @@ describe('Snarkjs circuit service', () => {
     const snarkjsCircuitService = new SnarkjsCircuitService();
     let proverWitness: Witness;
 
-    beforeAll(async () => {
+    beforeEach(async () => {
       proverWitness = await snarkjsCircuitService.createWitness({
         inputValueA: 2,
         inputValueB: 2,
@@ -14,20 +14,6 @@ describe('Snarkjs circuit service', () => {
 
       //Required for running tests or else tests hang
       globalThis.curve_bn128.terminate();
-    });
-
-    it('should return true if verifier uses the same public inputs as prover to verify proof', async () => {
-      let verifierWitness = new Witness();
-
-      verifierWitness = proverWitness;
-
-      const isVerified = await snarkjsCircuitService.verifyProofUsingWitness(
-        verifierWitness,
-      );
-
-      //Required for running tests or else tests hang
-      globalThis.curve_bn128.terminate();
-      expect(isVerified).toEqual(true);
     });
 
     it('should return false if verifier uses different public inputs from prover, even if the values are equal', async () => {
@@ -45,25 +31,6 @@ describe('Snarkjs circuit service', () => {
       expect(isVerified).toEqual(false);
     });
 
-    it('should return true as result of circuit constraints when correct public inputs are used and both input values are equal', async () => {
-      expect(Boolean(parseInt(proverWitness.publicInputs[0]))).toBe(true);
-    });
-  });
-
-  describe('should generate witness (proof, public inputs, verification key) to check for inequality of two values', () => {
-    const snarkjsCircuitService = new SnarkjsCircuitService();
-    let proverWitness: Witness;
-
-    beforeAll(async () => {
-      proverWitness = await snarkjsCircuitService.createWitness({
-        inputValueA: 2,
-        inputValueB: 3,
-      });
-
-      //Required for running tests or else tests hang
-      globalThis.curve_bn128.terminate();
-    });
-
     it('should return true if verifier uses the same public inputs as prover to verify proof', async () => {
       let verifierWitness = new Witness();
 
@@ -76,6 +43,25 @@ describe('Snarkjs circuit service', () => {
       //Required for running tests or else tests hang
       globalThis.curve_bn128.terminate();
       expect(isVerified).toEqual(true);
+    });
+
+    it('should return true as result of circuit constraints when correct public inputs are used and both input values are equal', async () => {
+      expect(Boolean(parseInt(proverWitness.publicInputs[0]))).toBe(true);
+    });
+  });
+
+  describe('should generate witness (proof, public inputs, verification key) to check for inequality of two values', () => {
+    const snarkjsCircuitService = new SnarkjsCircuitService();
+    let proverWitness: Witness;
+
+    beforeEach(async () => {
+      proverWitness = await snarkjsCircuitService.createWitness({
+        inputValueA: 2,
+        inputValueB: 3,
+      });
+
+      //Required for running tests or else tests hang
+      globalThis.curve_bn128.terminate();
     });
 
     it('should return false if verifier uses different public inputs from prover', async () => {
@@ -91,6 +77,20 @@ describe('Snarkjs circuit service', () => {
       //Required for running tests or else tests hang
       globalThis.curve_bn128.terminate();
       expect(isVerified).toEqual(false);
+    });
+
+    it('should return true if verifier uses the same public inputs as prover to verify proof', async () => {
+      let verifierWitness = new Witness();
+
+      verifierWitness = proverWitness;
+
+      const isVerified = await snarkjsCircuitService.verifyProofUsingWitness(
+        verifierWitness,
+      );
+
+      //Required for running tests or else tests hang
+      globalThis.curve_bn128.terminate();
+      expect(isVerified).toEqual(true);
     });
 
     it('should return false as result of circuit constraints when correct public inputs are used and input values are not equal', async () => {
