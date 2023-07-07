07cb5fa2ba00f6a524ec58a86270ad13e9bd61eb
biscuitdey <biswashreedey@gmail.com>
2023-07-07 13:50:50 +0530

    Update circuit interface

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/circuit.interface.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/circuit.interface.ts
@@ -1,9 +1,7 @@
-import { Proof } from '../../models/proof';
 import { Witness } from '../../models/witness';
 
 export interface ICircuitService {
   witness: Witness;
   createWitness(input: object): Promise<Witness>;
-  createProof(input: object): Promise<Proof>;
   verifyProofUsingWitness(witness: Witness): Promise<boolean>;
 }
