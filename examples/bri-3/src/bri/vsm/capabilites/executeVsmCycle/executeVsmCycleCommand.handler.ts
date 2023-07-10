7225e4159e819fc3371a5d551b891f843470bd7d
ognjenkurtic <kurticognjen@gmail.com>
2023-07-10 23:17:45 +0200

    Add todos

--- examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
+++ examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
@@ -14,5 +14,12 @@ export class ExecuteVsmCycleCommandHandler
 
   async execute(command: ExecuteVsmCycleCommand) {
     this.logger.logInfo('I am in a vsm cycle.');
+    // TODO: Initialized transactions are fetched in batches of configurable size
+    // TODO: Fetched transactions are marked as Processing
+    // TODO: Transactions are validated and marked as Invalid in case of errors
+    // TODO: Valid transactions are passed on to Workstep execution, one by one for now
+    // TODO: Dummy workstep outputs are picked up from Workstep execution and transaction is stored as Executed
+    // TODO: Any exception during workstep execution is handled gracefully and logged and transaction is marked as Aborted
+    // TODO: Relevant BPI subjects are informed (Notification is thrown?)
   }
 }
