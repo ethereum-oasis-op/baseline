e1add05df4e496d766c39915f5331ae51677b323
ognjenkurtic <kurticognjen@gmail.com>
2023-07-10 23:13:45 +0200

    Implement a scaffolding for running a vsm cycle periodically

--- examples/bri-3/src/bri/vsm/vsm.module.ts
+++ examples/bri-3/src/bri/vsm/vsm.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ScheduleModule } from '@nestjs/schedule';
+import { VsmTasksSchedulerAgent } from './agents/vsmTaskScheduler.agent';
+import { LoggingModule } from 'src/shared/logging/logging.module';
+import { ExecuteVsmCycleCommandHandler } from './capabilites/executeVsmCycle/executeVsmCycleCommand.handler';
+import { TransactionModule } from '../transactions/transactions.module';
 
 export const CommandHandlers = [
+  ExecuteVsmCycleCommandHandler
 ];
 
 export const QueryHandlers = [
@@ -11,9 +16,12 @@ export const QueryHandlers = [
 @Module({
   imports: [
     CqrsModule,
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
+    TransactionModule,
+    LoggingModule
   ],
   providers: [
+    VsmTasksSchedulerAgent,
     ...CommandHandlers,
     ...QueryHandlers,
   ],
