e03464ce7b87aea511784346783edfa125a8e071
ognjenkurtic <kurticognjen@gmail.com>
2023-07-11 15:55:31 +0200

    Add updating of transaction status to Processing just before validation and execution starts

--- examples/bri-3/src/bri/transactions/models/transaction.ts
+++ examples/bri-3/src/bri/transactions/models/transaction.ts
@@ -1,6 +1,9 @@
 import { TransactionStatus } from './transactionStatus.enum';
 import { AutoMap } from '@automapper/classes';
 import { BpiSubjectAccount } from '../../identity/bpiSubjectAccounts/models/bpiSubjectAccount';
+import {
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export class Transaction {
   @AutoMap()
@@ -64,5 +67,18 @@ export class Transaction {
     // TODO: Verify signature
     this.payload = payload;
     this.signature = signature;
+  }
+
+  public updateStatusToProcessing(): void {
+    if (this.status !== TransactionStatus.Initialized) {
+      // TODO: If we are throwing here we need to make sure
+      // this is properly handled by the caller as this piece
+      // of code will be executed in a VSM cycle
+      throw new InternalServerErrorException(
+        `Error while trying to update tx status to Processing as current status not Initialized. Current tx status: ${this.status}`,
+      );
+    }
+
+    this.status = TransactionStatus.Processing;
   }
 }
--- examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
+++ examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
@@ -3,12 +3,14 @@ import { ExecuteVsmCycleCommand } from './executeVsmCycle.command';
 import { TransactionStorageAgent } from 'src/bri/transactions/agents/transactionStorage.agent';
 import { LoggingService } from 'src/shared/logging/logging.service';
 import { TransactionStatus } from 'src/bri/transactions/models/transactionStatus.enum';
+import { TransactionAgent } from 'src/bri/transactions/agents/transactions.agent';
 
 @CommandHandler(ExecuteVsmCycleCommand)
 export class ExecuteVsmCycleCommandHandler
   implements ICommandHandler<ExecuteVsmCycleCommand>
 {
   constructor(
+    private agent: TransactionAgent,
     private storageAgent: TransactionStorageAgent,
     private readonly logger: LoggingService,
   ) {}
@@ -17,12 +19,17 @@ export class ExecuteVsmCycleCommandHandler
     this.logger.logInfo('I am in a vsm cycle.');
 
     // Initialized transactions are fetched in batches of configurable size
-    const executionCandidates = this.storageAgent.getTopNTransactionsByStatus(
-      Number(process.env.VSM_CYCLE_TX_BATCH_SIZE),
-      TransactionStatus.Initialized,
-    );
+    const executionCandidates =
+      await this.storageAgent.getTopNTransactionsByStatus(
+        Number(process.env.VSM_CYCLE_TX_BATCH_SIZE),
+        TransactionStatus.Initialized,
+      );
 
-    // TODO: Fetched transactions are marked as Processing
+    // Fetched transactions are marked as Processing
+    this.agent.bulkUpdateTransactionStatusToProcessing(executionCandidates);
+
+    await this.storageAgent.bulkUpdateTransactionStatus(executionCandidates);
+
     // TODO: Transactions are validated and marked as Invalid in case of errors
     // TODO: Valid transactions are passed on to Workstep execution, one by one for now
     // TODO: Dummy workstep outputs are picked up from Workstep execution and transaction is stored as Executed
