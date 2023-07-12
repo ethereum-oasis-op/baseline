55d05775d225360a254fd7c18d5953e9ee67b2b2
ognjenkurtic <kurticognjen@gmail.com>
2023-07-12 23:04:52 +0200

    Implement validation of transaction for exection + a dummy execution method

--- examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
+++ examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
@@ -4,6 +4,7 @@ import { TransactionStorageAgent } from 'src/bri/transactions/agents/transaction
 import { LoggingService } from 'src/shared/logging/logging.service';
 import { TransactionStatus } from 'src/bri/transactions/models/transactionStatus.enum';
 import { TransactionAgent } from 'src/bri/transactions/agents/transactions.agent';
+import { WorkstepStorageAgent } from 'src/bri/workgroup/worksteps/agents/workstepsStorage.agent';
 
 @CommandHandler(ExecuteVsmCycleCommand)
 export class ExecuteVsmCycleCommandHandler
@@ -11,41 +12,47 @@ export class ExecuteVsmCycleCommandHandler
 {
   constructor(
     private agent: TransactionAgent,
-    private storageAgent: TransactionStorageAgent,
+    private workstepStorageAgent: WorkstepStorageAgent,
+    private txStorageAgent: TransactionStorageAgent,
     private readonly logger: LoggingService,
   ) {}
 
   async execute(command: ExecuteVsmCycleCommand) {
     const executionCandidates =
-      await this.storageAgent.getTopNTransactionsByStatus(
+      await this.txStorageAgent.getTopNTransactionsByStatus(
         Number(process.env.VSM_CYCLE_TX_BATCH_SIZE),
         TransactionStatus.Initialized,
       );
 
     executionCandidates.forEach(async (tx) => {
       tx.updateStatusToProcessing();
-      await this.storageAgent.updateTransactionStatus(tx);
+      await this.txStorageAgent.updateTransactionStatus(tx);
 
       if (!this.agent.validateTransactionForExecution(tx)) {
         tx.updateStatusToInvalid();
-        await this.storageAgent.updateTransactionStatus(tx);
+        await this.txStorageAgent.updateTransactionStatus(tx);
         return;
       }
 
+      const workstep = await this.workstepStorageAgent.getWorkstepById(
+        tx.workstepInstanceId,
+      );
+
       try {
-        // TODO: Execute the transaction
+        const txResult = this.agent.executeTransaction(tx, workstep);
+        // TODO: When do we update the nonce on the BpiAccount?
+        // TODO: #702 Update relevant Bpi Account state on sucess
+        tx.updateStatusToExecuted();
+        this.txStorageAgent.updateTransactionStatus(tx);
       } catch (error) {
         this.logger.logError(
           `Error executing transaction with id ${tx.id}: ${error}`,
         );
         tx.updateStatusToAborted();
-        this.storageAgent.updateTransactionStatus(tx);
+        this.txStorageAgent.updateTransactionStatus(tx);
+        return;
       }
-
-      tx.updateStatusToExecuted();
-      this.storageAgent.updateTransactionStatus(tx);
     });
-
-    // TODO: Relevant BPI subjects are informed (Notification is thrown?)
+    // TODO: #705 Relevant BPI subjects are informed (Notification is thrown?)
   }
 }
