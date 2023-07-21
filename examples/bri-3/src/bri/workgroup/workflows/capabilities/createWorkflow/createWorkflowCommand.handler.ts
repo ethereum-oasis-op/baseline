ddeb0905cc359ef4016fc818e0c492d291947ba7
ognjenkurtic <kurticognjen@gmail.com>
2023-07-21 22:31:41 +0200

    Add bpiAccount ref to workgroup and start work on initializing a new one during workflow creation

--- examples/bri-3/src/bri/workgroup/workflows/capabilities/createWorkflow/createWorkflowCommand.handler.ts
+++ examples/bri-3/src/bri/workgroup/workflows/capabilities/createWorkflow/createWorkflowCommand.handler.ts
@@ -2,6 +2,9 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { WorkflowAgent } from '../../agents/workflows.agent';
 import { WorkflowStorageAgent } from '../../agents/workflowsStorage.agent';
 import { CreateWorkflowCommand } from './createWorkflow.command';
+import { BpiAccountAgent } from 'src/bri/identity/bpiAccounts/agents/bpiAccounts.agent';
+import { BpiAccountStorageAgent } from 'src/bri/identity/bpiAccounts/agents/bpiAccountsStorage.agent';
+import { WorkgroupStorageAgent } from 'src/bri/workgroup/workgroups/agents/workgroupStorage.agent';
 
 @CommandHandler(CreateWorkflowCommand)
 export class CreateWorkflowCommandHandler
@@ -9,7 +12,10 @@ export class CreateWorkflowCommandHandler
 {
   constructor(
     private agent: WorkflowAgent,
+    private accountAgent: BpiAccountAgent,
     private storageAgent: WorkflowStorageAgent,
+    private workgroupStorageAgent: WorkgroupStorageAgent,
+    private accountStorageAgent: BpiAccountStorageAgent,
   ) {}
 
   async execute(command: CreateWorkflowCommand) {
@@ -18,10 +24,25 @@ export class CreateWorkflowCommandHandler
         command.workstepIds,
       );
 
+    const workgroup = await this.workgroupStorageAgent.getWorkgroupById(command.workgroupId);
+    
+    
+    const newBpiAccountCandidate = this.accountAgent.createNewBpiAccount(
+       [], // TODO:  workgroup.participants, either get all BpiSubjectAccounts bellonging to participatns or have bpisubjectaccounts ids passed as param
+       'sample authorization condition',
+       'sample state object prover system',
+       'sample state object storage',
+    );
+  
+    const newBpiAccount = await this.accountStorageAgent.storeNewBpiAccount(
+      newBpiAccountCandidate,
+    );
+
     const newWorkflowCandidate = this.agent.createNewWorkflow(
       command.name,
       workstepsToConnect,
       command.workgroupId,
+      newBpiAccount
     );
 
     const newWorkflow = await this.storageAgent.storeNewWorkflow(
