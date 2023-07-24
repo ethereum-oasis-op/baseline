c316dd4f4a25295e33320c2f6f7b93c9bae5b0b1
ognjenkurtic <kurticognjen@gmail.com>
2023-07-24 22:44:56 +0200

    Add missing methods to  circuit interface and comment non working import in snarkjs.service

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/circuit.interface.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/circuit/circuit.interface.ts
@@ -1,7 +1,10 @@
+import { Proof } from '../../models/proof';
 import { Witness } from '../../models/witness';
 
 export interface ICircuitService {
   witness: Witness;
   createWitness(input: object): Promise<Witness>;
+  createProof(witness: Witness): Promise<Proof>
+  verifyProof(proof: Proof, witness: Witness): Promise<boolean>;
   verifyProofUsingWitness(witness: Witness): Promise<boolean>;
 }
