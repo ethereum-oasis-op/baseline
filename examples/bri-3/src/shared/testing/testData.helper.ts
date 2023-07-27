dc6407ebe61ef6145834d1e3f4acbf324582d506
ognjenkurtic <kurticognjen@gmail.com>
2023-07-27 22:23:52 +0200

    Fix import in testing module

--- examples/bri-3/src/shared/testing/testData.helper.ts
+++ examples/bri-3/src/shared/testing/testData.helper.ts
@@ -1,11 +1,12 @@
-import { BpiAccount } from 'src/bri/identity/bpiAccounts/models/bpiAccount';
-import { BpiSubject } from 'src/bri/identity/bpiSubjects/models/bpiSubject';
+import { uuid } from 'uuidv4';
+import { BpiAccount } from '../../bri/identity/bpiAccounts/models/bpiAccount';
+import { BpiSubject } from '../../bri/identity/bpiSubjects/models/bpiSubject';
 import {
   BpiSubjectRole,
   BpiSubjectRoleName,
-} from 'src/bri/identity/bpiSubjects/models/bpiSubjectRole';
-import { Workstep } from 'src/bri/workgroup/worksteps/models/workstep';
-import { uuid } from 'uuidv4';
+} from '../../bri/identity/bpiSubjects/models/bpiSubjectRole';
+import { Workflow } from '../../bri/workgroup/workflows/models/workflow';
+import { Workstep } from '../../bri/workgroup/worksteps/models/workstep';
 import {
   BpiSubjectAccountBuilder,
   BpiSubjectBuilder,
@@ -13,7 +14,6 @@ import {
   WorkgroupBuilder,
   WorkstepBuilder,
 } from './builders';
-import { Workflow } from 'src/bri/workgroup/workflows/models/workflow';
 
 // A place to encapsulate  creation of test data objects used for controller testing.
 // These objects will later be used to mock prisma.client calls only once during test bootstrap
