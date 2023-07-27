5fe5b49d12db884dab927e14a034a1700cba990b
biscuitdey <biswashreedey@gmail.com>
2023-07-27 13:15:36 +0530

    Update comments

--- examples/bri-3/zeroKnowledgeKeys/scripts/ptau.sh
+++ examples/bri-3/zeroKnowledgeKeys/scripts/ptau.sh
@@ -10,7 +10,7 @@ set -e
 
 # Starts Powers Of Tau ceremony, creating the file pot14_0000.ptau
 # 14 is the power of two of the maximum number of constraints that the ceremony can accept: in this case, the number of constraints is 2 ^ 14 = 16,384.
-# 14 is required as ecdsaSignatureVerification takes 8,076 constraints.
+# 14 is required as ecdsaSignatureVerification takes 8,076 constraints
 snarkjs powersoftau new bn128 14 zeroKnowledgeKeys/ptau/pot14_0000.ptau -v
 
 # Contribute to ceremony a few times.
