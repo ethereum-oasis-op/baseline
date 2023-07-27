bf58941c4a822353f01044c55b1b8f4d8f2e8a46
ognjenkurtic <kurticognjen@gmail.com>
2023-07-27 22:23:52 +0200

    Fix import in testing module

--- examples/bri-3/src/shared/testing/builders.ts
+++ examples/bri-3/src/shared/testing/builders.ts
@@ -1,10 +1,10 @@
-import { BpiAccount } from 'src/bri/identity/bpiAccounts/models/bpiAccount';
-import { BpiSubjectAccount } from 'src/bri/identity/bpiSubjectAccounts/models/bpiSubjectAccount';
-import { BpiSubject } from 'src/bri/identity/bpiSubjects/models/bpiSubject';
-import { BpiSubjectRole } from 'src/bri/identity/bpiSubjects/models/bpiSubjectRole';
-import { Workflow } from 'src/bri/workgroup/workflows/models/workflow';
-import { Workgroup } from 'src/bri/workgroup/workgroups/models/workgroup';
-import { Workstep } from 'src/bri/workgroup/worksteps/models/workstep';
+import { BpiAccount } from '../../bri/identity/bpiAccounts/models/bpiAccount';
+import { BpiSubjectAccount } from '../../bri/identity/bpiSubjectAccounts/models/bpiSubjectAccount';
+import { BpiSubject } from '../../bri/identity/bpiSubjects/models/bpiSubject';
+import { BpiSubjectRole } from '../../bri/identity/bpiSubjects/models/bpiSubjectRole';
+import { Workflow } from '../../bri/workgroup/workflows/models/workflow';
+import { Workgroup } from '../../bri/workgroup/workgroups/models/workgroup';
+import { Workstep } from '../../bri/workgroup/worksteps/models/workstep';
 
 export class WorkstepBuilder {
   private id: string;
