b7315d0778bf463e368c4f070b36e887de2f90b4
ognjenkurtic <kurticognjen@gmail.com>
2023-08-03 23:24:12 +0200

    Run linter

--- examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
+++ examples/bri-3/src/bri/vsm/capabilites/executeVsmCycle/executeVsmCycleCommand.handler.ts
@@ -39,7 +39,8 @@ export class ExecuteVsmCycleCommandHandler
       );
 
       try {
-        const { merkelizedPayload, witness } = await this.agent.executeTransaction(tx, workstep!);
+        const { merkelizedPayload, witness } =
+          await this.agent.executeTransaction(tx, workstep!);
         // TODO: When do we update the nonce on the BpiAccount?
         // Whenever a transaction is initiated
         // TODO: #702 Update relevant Bpi Account state on sucess
