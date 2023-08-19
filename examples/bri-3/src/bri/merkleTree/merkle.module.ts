673c9b312b36536e5dba6a296510ce08bf8f9975
ognjenkurtic <kurticognjen@gmail.com>
2023-08-19 22:43:18 +0200

    Remove missing handlers from merkle module

--- examples/bri-3/src/bri/merkleTree/merkle.module.ts
+++ examples/bri-3/src/bri/merkleTree/merkle.module.ts
@@ -5,29 +5,18 @@ import { LoggingModule } from '../../shared/logging/logging.module';
 import { AuthModule } from '../auth/auth.module';
 import { MerkleTreeAgent } from './agents/merkleTree.agent';
 import { MerkleTreeStorageAgent } from './agents/merkleTreeStorage.agent';
-import { MerkleTreeController } from './api/merkleTree.controller';
-import { CreateMerkleTreeCommandHandler } from './capabilities/createMerkleTree/createMerkleTreeCommand.handler';
-import { DeleteMerkleTreeCommandHandler } from './capabilities/deleteMerkleTree/deleteMerkleTreeCommand.handler';
-import { GetMerkleTreeByIdQueryHandler } from './capabilities/getMerkleTreeById/getMerkleTreeByIdQuery.handler';
 import { MerkleProfile } from './merkle.profile';
 import { MerkleTreeService } from './services/merkleTree.service';
 
-export const CommandHandlers = [
-  CreateMerkleTreeCommandHandler,
-  DeleteMerkleTreeCommandHandler,
-];
-export const QueryHandlers = [GetMerkleTreeByIdQueryHandler];
-
 @Module({
   imports: [CqrsModule, AuthModule, LoggingModule, EncryptionModule],
-  controllers: [MerkleTreeController],
   providers: [
-    ...CommandHandlers,
-    ...QueryHandlers,
     MerkleTreeAgent,
     MerkleTreeStorageAgent,
     MerkleTreeService,
     MerkleProfile,
   ],
+
+  exports: [MerkleTreeService]
 })
 export class MerkleModule {}
