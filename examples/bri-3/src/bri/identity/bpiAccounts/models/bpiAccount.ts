c753225a5826a11d0caccc583c2e46ccbd176abc
ognjenkurtic <kurticognjen@gmail.com>
2023-08-28 22:56:16 +0200

    Fix import in bpiAccount

--- examples/bri-3/src/bri/identity/bpiAccounts/models/bpiAccount.ts
+++ examples/bri-3/src/bri/identity/bpiAccounts/models/bpiAccount.ts
@@ -1,6 +1,6 @@
 import { AutoMap } from '@automapper/classes';
+import { BpiMerkleTree } from '../../../merkleTree/models/bpiMerkleTree';
 import { BpiSubjectAccount } from '../../bpiSubjectAccounts/models/bpiSubjectAccount';
-import { BpiMerkleTree } from 'src/bri/merkleTree/models/bpiMerkleTree';
 
 export class BpiAccount {
   @AutoMap()
