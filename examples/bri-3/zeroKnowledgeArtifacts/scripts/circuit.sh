a63f553ff6a6108e50775264462a9114e7052f0f
biscuitdey <biswashreedey@gmail.com>
2023-08-30 09:20:32 +0530

    Update ptau.sh & circuit.sh with max constraints

--- examples/bri-3/zeroKnowledgeArtifacts/scripts/circuit.sh
+++ examples/bri-3/zeroKnowledgeArtifacts/scripts/circuit.sh
@@ -12,12 +12,12 @@ set -e
 circom src/bri/zeroKnowledgeProof/services/circuit/snarkjs/$1.circom -o zeroKnowledgeArtifacts/circuits/workstep1 --r1cs --wasm
 
 #Setup
-snarkjs groth16 setup zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot20_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_final.zkey
+snarkjs groth16 setup zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot14_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_final.zkey
 
 ## Generate reference zkey -
 ## The zkey is a zero-knowledge key that includes both the proving and verification keys as well as phase 2 contributions.
 ## Importantly, one can verify whether a zkey belongs to a specific circuit or not.
-snarkjs zkey new zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot20_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_0000.zkey
+snarkjs zkey new zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot14_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_0000.zkey
 
 ## Ceremony just like before but for zkey this time
 snarkjs zkey contribute zeroKnowledgeArtifacts/circuits/workstep1/$1_0000.zkey zeroKnowledgeArtifacts/circuits/workstep1/$1_0001.zkey \
@@ -28,14 +28,14 @@ snarkjs zkey contribute zeroKnowledgeArtifacts/circuits/workstep1/$1_0002.zkey z
     --name="Third circuit contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
 
 ##  Verify if zkey belongs to our specific circuit
-snarkjs zkey verify zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot20_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_0003.zkey
+snarkjs zkey verify zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot14_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_0003.zkey
 
 ## Apply random beacon as before
 snarkjs zkey beacon zeroKnowledgeArtifacts/circuits/workstep1/$1_0003.zkey zeroKnowledgeArtifacts/circuits/workstep1/$1_final.zkey \
     0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Circuit FinalBeacon phase2"
 
 ## Optional: verify final zkey
-snarkjs zkey verify zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot20_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_final.zkey
+snarkjs zkey verify zeroKnowledgeArtifacts/circuits/workstep1/$1.r1cs zeroKnowledgeArtifacts/ptau/pot14_final.ptau zeroKnowledgeArtifacts/circuits/workstep1/$1_final.zkey
 
 ## Export verification key
 snarkjs zkey export verificationkey zeroKnowledgeArtifacts/circuits/workstep1/$1_final.zkey zeroKnowledgeArtifacts/circuits/workstep1/$1_verification_key.json
