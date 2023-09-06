fb3a6cd7ef7d87d95007df46314cac496a7063a1
biscuitdey <biswashreedey@gmail.com>
2023-09-06 13:04:23 +0530

    Remove unnecessary error messages

--- examples/bri-3/src/bri/zeroKnowledgeProof/api/err.messages.ts
+++
@@ -1,8 +0,0 @@
-export const INVALID_ANCHOR_HASH_INPUT =
-  'Input document provided for anchor hash does not exist or is blank';
-
-export const ANCHOR_HASH_ON_CCSM_NOT_FOUND_ERR_MESSAGE =
-  'Anchor Hash does not exist on the CCSM.';
-
-export const ANCHOR_HASH_NOT_FOUND_ERR_MESSAGE =
-  'Anchor Hash does not exist in the local storage.';
