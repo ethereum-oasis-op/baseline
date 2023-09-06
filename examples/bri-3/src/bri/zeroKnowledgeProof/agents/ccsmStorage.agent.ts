ec5e702ec74cdd69b9d76d6092ea44b5bd5fae22
biscuitdey <biswashreedey@gmail.com>
2023-09-06 12:59:23 +0530

    Add ccsm storage agent

---
+++ examples/bri-3/src/bri/zeroKnowledgeProof/agents/ccsmStorage.agent.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { IBlockchainService } from '../services/blockchain/blockchain.interface';
+
+@Injectable()
+export class CcsmStorageAgent {
+  constructor(private readonly ccsmStorageAgent: IBlockchainService) {}
+
+  async storeAnchorHashOnCcsm(anchorHash: string): Promise<void> {
+    await this.ccsmStorageAgent.storeAnchorHash(anchorHash);
+  }
+}
