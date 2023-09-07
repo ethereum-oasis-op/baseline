2ffce8aa20f2c05796baaf0b6fe28f513d9b7464
biscuitdey <biswashreedey@gmail.com>
2023-09-07 15:54:47 +0530

    Add ethereum service unit tests

--- examples/bri-3/src/bri/zeroKnowledgeProof/services/blockchain/blockchain.interface.ts
+++ examples/bri-3/src/bri/zeroKnowledgeProof/services/blockchain/blockchain.interface.ts
@@ -1,4 +1,6 @@
+import { Contract } from 'ethers';
 export interface IBlockchainService {
   deployContract(contractName: string): Promise<void>;
+  connectToContract(contractName: string): Promise<Contract>;
   storeAnchorHash(contractName: string, anchorHash: string): Promise<void>;
 }
