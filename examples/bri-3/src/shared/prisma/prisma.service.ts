6f5ad70d0108493e592743a5efdde3f26b689943
Kasshern <keith.salzman@protonmail.com>
2023-11-28 10:37:25 -0600

    feat: removes onModuleInit in PrismaService - inits lazily

--- examples/bri-3/src/shared/prisma/prisma.service.ts
+++ examples/bri-3/src/shared/prisma/prisma.service.ts
@@ -1,12 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient {
   constructor() {
     super({ log: ['info'] });
-  }
-  async onModuleInit() {
-    await this.$connect();
   }
 }
