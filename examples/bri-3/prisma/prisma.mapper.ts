18d2300df9eb2761015ba90f7dc1a774df96310d
ognjenkurtic <kurticognjen@gmail.com>
2024-01-04 12:14:46 +0100

    Instantiate domain object properly during mapping from prisma model

--- examples/bri-3/prisma/prisma.mapper.ts
+++ examples/bri-3/prisma/prisma.mapper.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { BpiSubject as BpiSubjectPrismaModel, BpiSubjectRole as BpiSubjectRolePrismaModel } from '@prisma/client';
-import { BpiSubject } from 'src/bri/identity/bpiSubjects/models/bpiSubject';
-import { BpiSubjectRole } from 'src/bri/identity/bpiSubjects/models/bpiSubjectRole';
+import { BpiSubject } from '../src/bri/identity/bpiSubjects/models/bpiSubject';
+import { BpiSubjectRole, BpiSubjectRoleName } from '../src/bri/identity/bpiSubjects/models/bpiSubjectRole';
 
+// We do mapping from prisma models to domain objects using simple Object.assing
+// since automapper is not happy working with types, which is how Prisma generates database entities.
+// For these mappings to work, the convention is that the domain objects have the same properties as the
+// prisma models. In case there is a need to do something custom during mapping, it can be implemented 
+// in the appropriate method below. 
 @Injectable()
 export class PrismaMapper {
-    public mapBpiSubjectPrismaModelToDomainObject(bspm: BpiSubjectPrismaModel): BpiSubject {
-        const bsdo: BpiSubject = {} as BpiSubject;
+    public mapBpiSubjectPrismaModelToDomainObject(source: BpiSubjectPrismaModel): BpiSubject {
+        const target: BpiSubject = new BpiSubject("", "", "", "", [])
 
-        Object.assign(bsdo, bspm);
+        Object.assign(target, source);
 
-        return bsdo;
+        return target;
     }
 
-    public mapBpiSubjectRolePrismaModelToDomainObject(bspm: BpiSubjectRolePrismaModel): BpiSubjectRole {
-        const bsdo: BpiSubjectRole = {} as BpiSubjectRole;
+    public mapBpiSubjectRolePrismaModelToDomainObject(source: BpiSubjectRolePrismaModel): BpiSubjectRole {
+        const target: BpiSubjectRole = new BpiSubjectRole("", BpiSubjectRoleName.EXTERNAL_BPI_SUBJECT, "")
 
-        Object.assign(bsdo, bspm);
+        Object.assign(target, source);
 
-        return bsdo;
+        return target;
     }
 }
--- examples/bri-3/src/bri/identity/bpiSubjects/subjects.module.ts
+++ examples/bri-3/src/bri/identity/bpiSubjects/subjects.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { PrismaMapper } from 'prisma/prisma.mapper';
+import { PrismaMapper } from '../../../../prisma/prisma.mapper';
 import { PrismaModule } from '../../../shared/prisma/prisma.module';
 import { AuthzModule } from '../../authz/authz.module';
 import { BpiSubjectAgent } from './agents/bpiSubjects.agent';
