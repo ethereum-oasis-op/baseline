28fc8a0f4e335eef6ef4b2a8bf0de3ead37210ac
ognjenkurtic <kurticognjen@gmail.com>
2024-01-10 22:42:13 +0100

    Implement new mapping approach for prisma models -> domain objects for  BpiAccount and Stateleafvalues

--- examples/bri-3/src/bri/communication/communication.module.ts
+++ examples/bri-3/src/bri/communication/communication.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { PrismaMapper } from '../../../prisma/prisma.mapper';
 import { EncryptionModule } from '../../shared/encryption/encryption.module';
 import { LoggingModule } from '../../shared/logging/logging.module';
+import { PrismaModule } from '../../shared/prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { SubjectModule } from '../identity/bpiSubjects/subjects.module';
 import { SubjectsProfile } from '../identity/bpiSubjects/subjects.profile';
@@ -16,7 +18,6 @@ import { GetBpiMessageByIdQueryHandler } from './capabilities/getBpiMessageById/
 import { ProcessInboundMessageCommandHandler } from './capabilities/processInboundMessage/processInboundMessageCommand.handler';
 import { UpdateBpiMessageCommandHandler } from './capabilities/updateBpiMessage/updateBpiMessageCommand.handler';
 import { CommunicationProfile } from './communicaton.profile';
-import { PrismaModule } from '../../shared/prisma/prisma.module';
 
 export const CommandHandlers = [
   CreateBpiMessageCommandHandler,
@@ -44,6 +45,7 @@ export const QueryHandlers = [GetBpiMessageByIdQueryHandler];
     MessagingAgent,
     SubjectsProfile,
     CommunicationProfile,
+    PrismaMapper,
     {
       provide: 'IMessagingClient',
       useClass: NatsMessagingClient,
