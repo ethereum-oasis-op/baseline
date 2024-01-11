12d3517218ea91ce8996cc1a06497d6c9246aba0
biscuitdey <biswashreedey@gmail.com>
2024-01-11 18:39:05 +0530

    Add hashing env variable

---
+++ examples/bri-3/front-end/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+	'@typescript-eslint/interface-name-prefix': 'off',
+	'@typescript-eslint/explicit-function-return-type': 'off',
+	'@typescript-eslint/explicit-module-boundary-types': 'off',
+	'@typescript-eslint/no-explicit-any': 'off',
+      },,
+};
