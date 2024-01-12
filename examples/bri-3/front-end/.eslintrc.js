645fb73121225f24489cbce4eaebb5ef5af2c521
biscuitdey <biswashreedey@gmail.com>
2024-01-12 15:16:01 +0530

    Fix error in .eslintrc

--- examples/bri-3/front-end/.eslintrc.js
+++ examples/bri-3/front-end/.eslintrc.js
@@ -21,9 +21,9 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-	'@typescript-eslint/interface-name-prefix': 'off',
-	'@typescript-eslint/explicit-function-return-type': 'off',
-	'@typescript-eslint/explicit-module-boundary-types': 'off',
-	'@typescript-eslint/no-explicit-any': 'off',
-      },,
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
