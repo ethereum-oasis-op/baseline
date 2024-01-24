5dedf2dd92248cc2364b3ea7ca416f66086c05eb
biscuitdey <biswashreedey@gmail.com>
2024-01-24 16:40:31 +0530

    Rename key to ecdsa key

--- examples/bri-3/test/sriUseCase.e2e-spec.ts
+++ examples/bri-3/test/sriUseCase.e2e-spec.ts
@@ -188,7 +188,7 @@ async function loginAsInternalBpiSubjectAndReturnAnAccessToken(): Promise<string
   // These values are used for testing purposes only
   const internalBpiSubjectEcdsaPublicKey =
     '0x044e851fa6118d0d33f11ebf8d4cae2a25dca959f06c1ab87b8fec9ccbf0ca0021b7efc27c786f9480f9f11cfe8df1ae991329654308611148a35a2277ba5909fe';
-  const internalBpiSubjectPrivateKey =
+  const internalBpiSubjectEcdsaPrivateKey =
     '0x0fbdb56ab0fecb2f406fa807d9e6558baedacc1c15c0e2703b77d4c08441e4fe';
 
   const nonceResponse = await request(server)
@@ -196,7 +196,10 @@ async function loginAsInternalBpiSubjectAndReturnAnAccessToken(): Promise<string
     .send({ publicKey: internalBpiSubjectEcdsaPublicKey })
     .expect(201);
 
-  const signer = new ethers.Wallet(internalBpiSubjectPrivateKey, undefined);
+  const signer = new ethers.Wallet(
+    internalBpiSubjectEcdsaPrivateKey,
+    undefined,
+  );
   const signature = await signer.signMessage(nonceResponse.text);
 
   const loginResponse = await request(server)
