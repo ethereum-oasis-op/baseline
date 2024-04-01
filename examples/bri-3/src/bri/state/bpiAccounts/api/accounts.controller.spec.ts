0e3c61b84e85f1df64ca319bad562102c45ea0cd
biscuitdey <biswashreedey@gmail.com>
2024-04-01 17:57:25 +0530

    Use publicKeys in bpiAccounts tests

--- examples/bri-3/src/bri/state/bpiAccounts/api/accounts.controller.spec.ts
+++ examples/bri-3/src/bri/state/bpiAccounts/api/accounts.controller.spec.ts
@@ -28,6 +28,10 @@ import { GetAllBpiAccountsQueryHandler } from '../capabilities/getAllBpiAccounts
 import { GetBpiAccountByIdQueryHandler } from '../capabilities/getBpiAccountById/getBpiAccountByIdQuery.handler';
 import { UpdateBpiAccountCommandHandler } from '../capabilities/updateBpiAccount/updateBpiAccountCommand.handler';
 import { BpiAccount } from '../models/bpiAccount';
+import {
+  PublicKey,
+  PublicKeyType,
+} from '../../../identity/bpiSubjects/models/publicKey';
 import { AccountController } from './accounts.controller';
 import { CreateBpiAccountDto } from './dtos/request/createBpiAccount.dto';
 import { NOT_FOUND_ERR_MESSAGE } from './err.messages';
@@ -91,18 +95,23 @@ describe('AccountController', () => {
   });
 
   const createBpiSubjectAccount = () => {
+    const publicKeys = [
+      new PublicKey('111', PublicKeyType.ECDSA, 'ecdsaPk', '123'),
+      new PublicKey('112', PublicKeyType.EDDSA, 'eddsaPk', '123'),
+    ];
+
     const ownerBpiSubject = new BpiSubject(
       '123',
       'owner',
       'desc',
-      'publicKey',
+      publicKeys,
       [],
     );
     const creatorBpiSubject = new BpiSubject(
       '321',
       'creator',
       'desc',
-      'publicKey',
+      publicKeys,
       [],
     );
     return new BpiSubjectAccount(
