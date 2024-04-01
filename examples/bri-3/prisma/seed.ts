3668f47072f3b9074bd64c8e2108a86d0304df98
biscuitdey <biswashreedey@gmail.com>
2024-04-01 18:07:42 +0530

    Use publicKeys in shared test builder data

--- examples/bri-3/prisma/seed.ts
+++ examples/bri-3/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, PublicKeyType } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -21,19 +21,35 @@ async function main() {
     },
   });
 
-  await prisma.bpiSubject.create({
+  const internalBpiSubject = await prisma.bpiSubject.create({
     data: {
-        name: 'BpiAdmin',
-        description: 'Internal Bpi Subject of this Bpi',
-        publicKey: '0x044e851fa6118d0d33f11ebf8d4cae2a25dca959f06c1ab87b8fec9ccbf0ca0021b7efc27c786f9480f9f11cfe8df1ae991329654308611148a35a2277ba5909fe',
-        // private key 0x0fbdb56ab0fecb2f406fa807d9e6558baedacc1c15c0e2703b77d4c08441e4fe, used for testing purposes only
-        loginNonce: '',
-        roles: {
-          connect: {
-            id: internalBpiSubjectRole.id
-          },
+      name: 'BpiAdmin',
+      description: 'Internal Bpi Subject of this Bpi',
+      // private key 0x0fbdb56ab0fecb2f406fa807d9e6558baedacc1c15c0e2703b77d4c08441e4fe, used for testing purposes only
+      loginNonce: '',
+      roles: {
+        connect: {
+          id: internalBpiSubjectRole.id,
         },
-    }      
+      },
+    },
+  });
+
+  await prisma.publicKey.createMany({
+    data: [
+      {
+        type: PublicKeyType.ECDSA,
+        value:
+          '0x044e851fa6118d0d33f11ebf8d4cae2a25dca959f06c1ab87b8fec9ccbf0ca0021b7efc27c786f9480f9f11cfe8df1ae991329654308611148a35a2277ba5909fe',
+        bpiSubjectId: internalBpiSubject.id,
+      },
+      {
+        type: PublicKeyType.EDDSA,
+        value:
+          '0x044e851fa6118d0d33f11ebf8d4cae2a25dca959f06c1ab87b8fec9ccbf0ca0021b7efc27c786f9480f9f11cfe8df1ae991329654308611148a35a2277ba5909fe',
+        bpiSubjectId: internalBpiSubject.id,
+      },
+    ],
   });
 }
 main()
