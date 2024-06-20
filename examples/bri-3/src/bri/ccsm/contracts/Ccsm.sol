3306c5349e23f59f5d64c550590368f1b128080b
biscuitdey <biswashreedey@gmail.com>
2024-06-20 13:58:48 +0530

    Create Ccsm.sol contract

---
+++ examples/bri-3/src/bri/ccsm/contracts/Ccsm.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+contract Ccsm {
+  mapping(string => string) public anchorHashStore;
+  event AnchorHashSet(string indexed workgroupId, string anchorHash);
+
+  
+}
