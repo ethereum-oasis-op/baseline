e2176bde35f09eaf8dc2dd002576dc2fd690a17d
biscuitdey <biswashreedey@gmail.com>
2024-06-20 13:59:08 +0530

    Add setAnchorHash function

--- examples/bri-3/src/bri/ccsm/contracts/Ccsm.sol
+++ examples/bri-3/src/bri/ccsm/contracts/Ccsm.sol
@@ -5,5 +5,14 @@ contract Ccsm {
   mapping(string => string) public anchorHashStore;
   event AnchorHashSet(string indexed workgroupId, string anchorHash);
 
+ function setAnchorHash(string calldata _workgroupId, string calldata _anchorHash) external {
+	require(bytes(_workgroupId).length > 0, "WorkgroupId cannot be empty");
+	require(bytes(_anchorHash).length > 0, "AnchorHash cannot be empty");
+
+    	anchorHashStore[_workgroupId] = _anchorHash;
+
+	emit AnchorHashSet(_workgroupId, _anchorHash);
+  }
+
   
 }
