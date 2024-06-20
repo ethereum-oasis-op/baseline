fbb9cf79b31c5cf469eb54b35419014bacdfd491
biscuitdey <biswashreedey@gmail.com>
2024-06-20 14:00:03 +0530

    Fix formatting

--- examples/bri-3/src/bri/ccsm/contracts/Ccsm.sol
+++ examples/bri-3/src/bri/ccsm/contracts/Ccsm.sol
@@ -5,18 +5,21 @@ contract Ccsm {
   mapping(string => string) public anchorHashStore;
   event AnchorHashSet(string indexed workgroupId, string anchorHash);
 
- function setAnchorHash(string calldata _workgroupId, string calldata _anchorHash) external {
-	require(bytes(_workgroupId).length > 0, "WorkgroupId cannot be empty");
-	require(bytes(_anchorHash).length > 0, "AnchorHash cannot be empty");
+  function setAnchorHash(
+    string calldata _workgroupId,
+    string calldata _anchorHash
+  ) external {
+    require(bytes(_workgroupId).length > 0, 'WorkgroupId cannot be empty');
+    require(bytes(_anchorHash).length > 0, 'AnchorHash cannot be empty');
 
-    	anchorHashStore[_workgroupId] = _anchorHash;
+    anchorHashStore[_workgroupId] = _anchorHash;
 
-	emit AnchorHashSet(_workgroupId, _anchorHash);
+    emit AnchorHashSet(_workgroupId, _anchorHash);
   }
 
- function getAnchorHash(
+  function getAnchorHash(
     string calldata _workgroupId
   ) external view returns (string memory) {
     return anchorHashStore[_workgroupId];
-  }  
+  }
 }
