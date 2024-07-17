74cd1c4def940833f61c301e0d6c0ca75e0940b2
ognjenkurtic <kurticognjen@gmail.com>
2024-07-17 22:51:51 +0200

    Change DID_NETWORK env var to CCSM_NETWORK as it is used for connecting to BPI contract as well

--- examples/bri-3/ccsm/hardhat.config.ts
+++ examples/bri-3/ccsm/hardhat.config.ts
@@ -3,9 +3,16 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  }
 };
 
-// TODO: add local hardhat node config
 // TODO: add sepolia network config with infura or alchemy URL
 // TODO: Compile the contract and add the ABI to a folder ccsmArtifacts so that it can be used by the BPI to talk to the contract
 
