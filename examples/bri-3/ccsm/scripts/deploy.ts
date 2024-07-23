1e8866e3024cd191a14281f80038a43a80387856
ognjenkurtic <kurticognjen@gmail.com>
2024-07-23 16:16:19 +0200

    Deploy bpi smart contract with local internal bpi subject as owner

--- examples/bri-3/ccsm/scripts/deploy.ts
+++ examples/bri-3/ccsm/scripts/deploy.ts
@@ -1,13 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
-  const [owner, adminAccount] = await hre.ethers.getSigners();
+  const [originalOwner] = await hre.ethers.getSigners();
+
+  const ownerAndAdminPrivateKey = "2c95d82bcd8851bd3a813c50afafb025228bf8d237e8fd37ba4adba3a7596d58";
+  const ownerAndAdmin = new hre.ethers.Wallet(ownerAndAdminPrivateKey, hre.ethers.provider);
+
+  await originalOwner.sendTransaction({
+    to: ownerAndAdmin.address,
+    value: hre.ethers.parseEther("1.0")
+  });
 
   const CcsmBpiStateAnchor = await hre.ethers.getContractFactory("CcsmBpiStateAnchor");
 
-  const ccsmBpiStateAnchor = await CcsmBpiStateAnchor.deploy([
-    await owner.getAddress(),
-    await adminAccount.getAddress(),
+  const ccsmBpiStateAnchor = await CcsmBpiStateAnchor.connect(ownerAndAdmin).deploy([
+    ownerAndAdmin.address,
+    ownerAndAdmin.address,
   ]);
 
   console.log("CcsmBpiStateAnchor deployed to:", await ccsmBpiStateAnchor.getAddress());
